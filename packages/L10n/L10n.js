c9fd2290b979e5dbabd7c6ecb7fec38f1e6930d7
Jamie Kupka <jamie_kupka@acl.com>
2019-02-21 16:50:46 -0800

    UX-438 Add prop requirements

--- packages/L10n/L10n.js
+++ packages/L10n/L10n.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { node, string } from "prop-types";
 import { i18n } from ".";
 import Locales from "./locales";
 
 export const L10nContext = React.createContext();
+
+const propTypes = {
+  locale: string,
+  children: node.isRequired,
+};
+
+const defaultProps = {
+  locale: "en",
+};
 
 export default function L10n(props) {
   const _i18n = i18n(Locales);
   _i18n.changeLanguage(props.locale);
   return <L10nContext.Provider value={_i18n}>{props.children}</L10nContext.Provider>;
 }
+
+L10n.propTypes = propTypes;
+L10n.defaultProps = defaultProps;
