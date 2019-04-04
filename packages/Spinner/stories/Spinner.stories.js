5bff4138cb7c8d1b6578e971f07364a759c07940
Azmarie Wang <azmarie_wang@acl.com>
2019-04-04 20:47:24 +0000

    UX-456 Fix lint

--- packages/Spinner/stories/Spinner.stories.js
+++ packages/Spinner/stories/Spinner.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import Basic from "./examples/Basic";
 import { withKnobs } from "@storybook/addon-knobs";
+import Basic from "./examples/Basic";
 
 storiesOf("Spinner", module)
   .addDecorator(withKnobs)
