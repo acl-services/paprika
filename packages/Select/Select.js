553e7a1885e2dd02431d221b62427eb450314023
Tristan Jasper <tjasper9@gmail.com>
2019-04-25 15:53:22 -0700

    intiial setup

---
+++ packages/Select/Select.js
@@ -0,0 +1,19 @@
+/* eslint-disable react/button-has-type */
+
+import React from "react";
+import PropTypes from "prop-types";
+import selectStyles from "./Select.styles";
+
+const propTypes = {};
+
+const defaultProps = {};
+
+const Button = props => {
+  return <div />;
+};
+
+Button.displayName = "Select";
+Button.propTypes = propTypes;
+Button.defaultProps = defaultProps;
+
+export default Button;
