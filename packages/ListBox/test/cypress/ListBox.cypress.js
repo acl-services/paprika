0f0aa3585164238e5900487c305ffd07528bf718
Patrick Moreno <patrick_moreno@LT-C02W42AJHTDF.local>
2019-05-07 11:23:38 -0700

    UX-434 Cypress test

--- packages/ListBox/test/cypress/ListBox.cypress.js
+++ packages/ListBox/test/cypress/ListBox.cypress.js
@@ -2,6 +2,14 @@
 //   cy.get("[data-qa-anchor='trigger']");
 // }
 
+const getFilterInput = () => cy.get("[data-qa-anchor='list-filter-input']");
+
+const getPopoverContent = () => cy.get("[data-qa-anchor='popover-content']");
+
+const getPopoverList = () => cy.get("[data-qa-anchor='styled-list']");
+
+const getClearButton = () => cy.get("[data-qa-anchor='clear-button']");
+
 const getListBoxTrigger = () => cy.get("[data-qa-anchor='trigger']");
 
 function toggleDropdown() {
@@ -10,17 +18,47 @@ function toggleDropdown() {
 
 describe("ListBox single select", () => {
   beforeEach(() => {
-    cy.visitStorybook("ListBox%20%2F%20single", "Filter");
+    cy.visitStorybook("ListBox / single", "Basic");
+    toggleDropdown();
   });
 
-  it("Filters and selects option", () => {
+  it("should selected option and clear it", () => {
+    cy.contains("Punisher").click();
+    getListBoxTrigger().should("contain", "Punisher");
+    getClearButton().click();
+    getListBoxTrigger().should("contain", "Select one of the options");
+    getClearButton().should("not.be.visible");
+  });
+});
+
+describe("ListBox single select filter", () => {
+  beforeEach(() => {
+    cy.visitStorybook("ListBox / single", "Filter");
     toggleDropdown();
-    cy.get("[data-qa-anchor='list-filter-input']").type("Ven");
-    cy.get("[data-qa-anchor='popover-content']")
+  });
+
+  it("should show correct amount of filter options", () => {
+    getFilterInput().type("wo");
+    getPopoverList()
+      .children()
+      .should("have.length", 2);
+  });
+
+  it("filters and selects option", () => {
+    getFilterInput().type("Ven");
+    getPopoverContent()
       .contains(/venom/i)
       .click();
     getListBoxTrigger().should("contain", "Venom");
   });
+
+  // it.only("should show all options after erasing filtered input", () => {
+  //   getFilterInput().type("wo");
+  //   getFilterInput().type("{backspace}", "{backspace}");
+  //   getPopoverList()
+  //     .children()
+  //     .should("have.length", 10);
+  // });
 });
 
 // describe("ListBox single select with getScrollContainer prop", () => {
