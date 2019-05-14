8f18fa459f9c74e42a74d80f210ff26eb0037b48
Patrick Moreno <patrick_moreno@LT-C02W42AJHTDF.local>
2019-05-13 17:08:27 -0700

    UX-434 🐙📝 Test - added lazy tests and qa anchors

--- packages/ListBox/test/cypress/Lazy.ListBox.cypress.js
+++ packages/ListBox/test/cypress/Lazy.ListBox.cypress.js
@@ -1,10 +1,40 @@
+import selectors from "../helpers/selectors";
+
 function toggleDropDown() {
   cy.contains("Marvel API").click();
 }
 
 describe("Lazy ListBox", () => {
-  it("should show marvel characters starting iwth 'n','z', and 's'", () => {
+  beforeEach(() => {
     cy.visitStorybook("ListBox / more examples", "Lazy ListBox");
     toggleDropDown();
+  });
+
+  it("should select three options and see three images", () => {
+    cy.get("#root")
+      .children()
+      .should("have.length", 1);
+    cy.contains("Namora").click();
+    cy.get(selectors.popoverList).scrollTo("bottom");
+    cy.contains("Satana").click();
+    cy.contains("Sauron").click();
+    cy.contains("Accept").click();
+    cy.get(selectors.popover).should("not.be.visible");
+    cy.get("#root div:first")
+      .children()
+      .should("have.length", 3)
+      .and("contain", "Namora")
+      .and("contain", "Satana")
+      .and("contain", "Sauron");
+  });
+
+  it.only("should select options and clear selections", () => {
+    cy.contains("Namora").click();
+    // cy.contains("Nebula")
+    //   .should("have.css", "font-weight")
+    //   .and("match", 400);
+    cy.contains("Nebula").click();
+    // cy.contains("Nebula").should("have.css", "font-weight");
+    cy.contains("Clear").click();
   });
 });
