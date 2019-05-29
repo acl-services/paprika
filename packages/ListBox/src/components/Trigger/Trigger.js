e6ae6716b82d90d041790e17a9ac2dd60e7a9746
nahumzs <nahum_zsilva@acl.com>
2019-05-29 13:31:31 -0500

    UX-434 🐛 - Fixing - rendering Trigger incorrectly  / 🦄.

--- packages/ListBox/src/components/Trigger/Trigger.js
+++ packages/ListBox/src/components/Trigger/Trigger.js
@@ -85,7 +85,6 @@ export default function Trigger(props) {
         options={state.options}
         placeholder={placeholder}
         selectedOptions={state.selectedOptions}
-        data-qa-anchor="trigger"
       />
     ) : (
       <RawButton
