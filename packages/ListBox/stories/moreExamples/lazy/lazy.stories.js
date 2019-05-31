46a3f6ee96156da47a6c5b037413cdc884b1d924
nahumzs <nahum_zsilva@acl.com>
2019-05-31 12:17:42 -0500

    UX-434 🙅‍♀️  - moving - Docs/Stories/Test into a separated branch  / 🦄.

--- packages/ListBox/stories/moreExamples/lazy/lazy.stories.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import LazyListBox from "./Lazy";
-
-storiesOf("ListBox / more examples", module).add("Lazy ListBox", () => <LazyListBox />);
--- packages/ListBox/stories/moreExamples/onChange/onChange.stories.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import OnChange from "./OnChange";
-
-storiesOf("ListBox / more examples", module).add("OnChange ListBox", () => <OnChange />);
