1d8ec5a3d6814e8129a80176d34931873b73e4af
nahumzs <nahum_zsilva@acl.com>
2019-06-13 11:30:31 -0700

    UX-434  - dry - divider.styles  / 🦄.

--- packages/ListBox/src/components/Divider/Divider.styles.js
+++ packages/ListBox/src/components/Divider/Divider.styles.js
@@ -1,31 +1,30 @@
 import { css } from "styled-components";
 
+const pseudoElement = `
+  background: #d7d7d7;
+  content: "";
+  display: inline-block;
+  flex-grow: 1;
+  height: 1px;
+`;
+
 // css let the IDE recognize css highlight
 export const dividerCSS = css`
   align-items: center;
   display: flex;
   color: #717171;
-  font-size: 14px;
   justify-content: center;
   width: 100%;
   font-size: 12px;
   padding: 4px;
 
   &:before {
-    background: #d7d7d7;
-    content: "";
-    display: inline-block;
-    flex-grow: 1;
-    height: 1px;
+    ${pseudoElement}
     margin-right: 8px;
   }
 
   &:after {
-    background: #d7d7d7;
-    content: "";
-    display: inline-block;
-    flex-grow: 1;
-    height: 1px;
+    ${pseudoElement}
     margin-left: 8px;
   }
 `;
