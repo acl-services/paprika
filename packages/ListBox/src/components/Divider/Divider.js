33c64cef05f60fa49cf817e6ff2282199bd35c0f
nahumzs <nahum_zsilva@acl.com>
2019-06-14 09:43:07 -0700

    UX-434 ✨ - improving - keyup keydown and hook for children length - linter  / 🦄.

--- packages/ListBox/src/components/Divider/Divider.js
+++ packages/ListBox/src/components/Divider/Divider.js
@@ -3,7 +3,11 @@ import PropTypes from "prop-types";
 import { dividerCSS } from "./Divider.styles";
 
 const propTypes = {
-  isDisabled: PropTypes.bool,
+  // isDisable is use internally as a default prop
+  // the prop is read by the option/helpers/optionState.js which assigned in the store
+  // later can be ignore by the keyup keydown event seens is consider disable see options/helpers/options.js
+  // function getNextOptionActiveIndex
+  isDisabled: PropTypes.bool, // eslint-disable-line
   children: PropTypes.node.isRequired,
 };
 
