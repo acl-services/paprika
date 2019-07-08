af6627ae4f17bcd6f1c065f535e9440402d07481
nahumzs <nahum_zsilva@acl.com>
2019-07-08 16:06:35 -0700

    UX-498 👩🏻‍💼 - secret - printing token  / 🦄.

--- scripts/npmRelease.js
+++ scripts/npmRelease.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
-const { NPM_TOKEN, SEMAPHORE_GIT_BRANCH } = process.env;
+const { PAPRIKA_NPM_TOKEN, SEMAPHORE_GIT_BRANCH } = process.env;
 const baseBranch = "master"; // this should be the one that we want to operate.
 
 if (SEMAPHORE_GIT_BRANCH === "UX-498-automate-release-process") {
   console.log("BRANCH:", "👍");
 }
 
-console.log("TOKEN:", NPM_TOKEN);
+console.log("TOKEN:", PAPRIKA_NPM_TOKEN);
+
+process.exit(0);
