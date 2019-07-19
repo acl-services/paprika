12626a40f1d91eb9a5c91f005ba78ad35ade9baf
Raul Souza <raul_souza@acl.com>
2019-07-18 17:21:31 -0700

    Refactor header styles

--- packages/Modal/src/Header/Header.styles.js
+++ packages/Modal/src/Header/Header.styles.js
@@ -2,28 +2,27 @@ import tokens from "@paprika/tokens";
 import stylers from "@paprika/stylers";
 import { css } from "styled-components";
 
+const headerDarkStyles = css`
+  background-color: ${tokens.color.black};
+  padding-bottom: ${stylers.spacer(2)};
+  &::before {
+    background-color: #323030;
+    border-radius: 0 ${tokens.card.borderRadius} 0 0;
+    content: "";
+    display: block;
+    height: ${stylers.spacer(7)};
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: ${stylers.spacer(10)};
+  }
+`;
+
 export const headerStyles = css`
   border-radius: ${tokens.card.borderRadius} ${tokens.card.borderRadius} 0 0;
   padding: ${stylers.spacer(2)} ${stylers.spacer(10)} 0 ${stylers.spacer(3)};
   position: relative;
-
-  ${props =>
-    props.isDark &&
-    css`
-      background-color: ${tokens.color.black};
-      padding-bottom: ${stylers.spacer(2)};
-      &::before {
-        background-color: #323030;
-        border-radius: 0 ${tokens.card.borderRadius} 0 0;
-        content: "";
-        display: block;
-        height: ${stylers.spacer(7)};
-        position: absolute;
-        right: 0;
-        top: 0;
-        width: ${stylers.spacer(10)};
-      }
-    `}
+  ${props => props.isDark && headerDarkStyles}
 `;
 
 export const headingStyle = css`
@@ -39,24 +38,4 @@ export const closeButtonStyle = css`
   position: absolute;
   right: ${stylers.spacer(2)};
   top: ${stylers.spacer(2)};
-
-  ${props =>
-    props.isDark &&
-    css`
-      .icon::before {
-        color: ${tokens.color.blackLighten50};
-      }
-      :hover {
-        background-color: rgba(255, 255, 255, 0.1);
-        .icon::before {
-          color: ${tokens.color.white};
-        }
-      }
-      :active {
-        background-color: rgba(255, 255, 255, 0.2);
-        .icon::before {
-          color: ${tokens.color.white};
-        }
-      }
-    `}
 `;
