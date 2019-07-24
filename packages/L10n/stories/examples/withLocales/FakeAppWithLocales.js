779123b3ad1b2781fb52dafc5ce1657efbffdb8c
Jamie Kupka <jamie_kupka@acl.com>
2019-07-24 08:35:58 -0700

    AN-7742 Fix capitalization

--- packages/L10n/stories/examples/withLocales/FakeAppWithLocales.js
+++ packages/L10n/stories/examples/withLocales/FakeAppWithLocales.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Story } from "storybook/assets/styles/common.styles";
-import L10n from "@paprika/L10n";
+import L10n from "@paprika/l10n";
 import Locales from "./Locales";
 import Greeting from "./Greeting";
 
