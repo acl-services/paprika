7063f6b7e704a145b88d0a80015323d625fa2829
Tristan Jasper <tjasper9@gmail.com>
2019-08-09 09:26:41 -0700

    enhance showcase

--- packages/Confirmation/stories/examples/Showcase.js
+++ packages/Confirmation/stories/examples/Showcase.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Rule, Tagline } from "storybook/assets/styles/common.styles";
-import { select } from "@storybook/addon-knobs";
+import { select, action, text } from "@storybook/addon-knobs";
 import L10n from "@paprika/l10n";
 import Heading from "@paprika/heading";
 import Confirmation from "../../src";
 
 const handleConfirm = () => {
-  console.log("confirmed");
+  action("Clicked the confirmation button")();
 };
 
 const handleCancel = () => {
-  console.log("canceled");
+  action("Clicked the cancel button")();
 };
 
 const ExampleStory = () => (
@@ -24,13 +24,14 @@ const ExampleStory = () => (
       <Confirmation
         isOpen
         buttonSize={select("Confirmation Button Size", ["x-small", "small", "medium", "large"], "medium")}
-        confirmLabel="Delete filter"
-        description={
-          <p>Lorem ipsum dolor amet vexillologist tacos selvage narwhal butcher twee ethical hot chicken.</p>
-        }
+        confirmLabel={text("Confirm button text", "Delete filter")}
+        description={text(
+          "Description text",
+          "Lorem ipsum dolor amet vexillologist tacos selvage narwhal butcher twee ethical hot chicken."
+        )}
         onConfirm={() => handleConfirm()}
         onCancel={() => handleCancel()}
-        heading="Delete filter?"
+        heading={text("Heading text", "Delete filter?")}
       />
     </L10n>
   </React.Fragment>
