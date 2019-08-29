5aa861af1fe4f7a5d36f91fab49980d79b97cf20
Mike Polowick <mike_polowick@acl.com>
2019-08-29 16:59:56 -0700

    Adjust background color of CollapsibleChecklists Heading

--- packages/CollapsibleChecklists/src/components/Heading/Heading.styles.js
+++ packages/CollapsibleChecklists/src/components/Heading/Heading.styles.js
@@ -4,6 +4,7 @@ import stylers from "@paprika/stylers";
 
 const headingStyles = css`
   background-color: ${tokens.table.header.backgroundColor};
+  background-color: ${tokens.color.creme};
   border-bottom: 2px solid ${tokens.border.color};
   font-weight: bold;
   padding: ${tokens.space} ${tokens.space} ${tokens.space} ${stylers.spacer(2)};
