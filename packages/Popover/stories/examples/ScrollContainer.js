a820b37c25ce8af049b86d4c7f2174da4a85009b
Mike Polowick <mike_polowick@acl.com>
2019-09-09 01:30:45 -0700

    Experiments with Popover ScrollContainer story

--- packages/Popover/stories/examples/ScrollContainer.js
+++ packages/Popover/stories/examples/ScrollContainer.js
@@ -1,6 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { addParameters } from "@storybook/react";
+
 import { select } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+
 import styled from "styled-components";
 import { CenteredStory } from "storybook/assets/styles/common.styles";
 import Button from "@paprika/button";
@@ -8,15 +12,14 @@ import { popoverProps as basicProps } from "./Basic";
 import Popover from "../../src";
 
 const PopoverContainer = styled.div`
-  width: 240%;
-  height: 980px;
-  // padding: 380px 230px 0 0;
-  padding: 450px 0 0 0;
+  height: 1024px;
+  padding: 512px 0 0 0;
   text-align: center;
+  width: 3072px;
 `;
 
 const ScrollBox = styled.div`
-  border: 3px solid #333;
+  border: 3px solid #999;
   border-radius: 5px;
   height: 420px;
   margin: 16px 32px;
@@ -62,15 +65,52 @@ PopoverBox.propTypes = {
   getScrollContainer: PropTypes.func.isRequired,
 };
 
-const ExampleStory = props => (
-  <React.Fragment>
-    <CenteredStory>
-      <ScrollBox id="scroll-container">
-        <PopoverBox {...props} getScrollContainer={() => document.querySelector("#scroll-container")} />
-      </ScrollBox>
-    </CenteredStory>
-    <TrailerBox>Fin.</TrailerBox>
-  </React.Fragment>
-);
+function setKnobsTab() {
+  setTimeout(() => {
+    // eslint-disable-next-line no-restricted-globals
+    parent.document.querySelector("#storybook-panel-root .simplebar-content button").click();
+  });
+}
+
+function togglePanelPosition() {
+  setTimeout(() => {
+    // eslint-disable-next-line no-restricted-globals
+    const buttons = parent.document.querySelectorAll("#storybook-panel-root .simplebar-content button");
+    buttons[buttons.length - 2].click();
+  });
+}
+
+function actionsObseyo() {
+  setTimeout(() => {
+    action("There are 0 actions for this story.")();
+    setKnobsTab();
+  });
+}
+
+const ExampleStory = props => {
+  actionsObseyo();
+  togglePanelPosition();
+
+  setTimeout(() => {
+    document.getElementById("scroll-container").scrollTo(1080, 320);
+  });
+
+  addParameters({
+    options: {
+      panelPosition: "bottom",
+    },
+  });
+
+  return (
+    <React.Fragment>
+      <CenteredStory>
+        <ScrollBox id="scroll-container">
+          <PopoverBox {...props} getScrollContainer={() => document.querySelector("#scroll-container")} />
+        </ScrollBox>
+      </CenteredStory>
+      <TrailerBox>Fin.</TrailerBox>
+    </React.Fragment>
+  );
+};
 
 export default () => <ExampleStory {...popoverProps()} />;
