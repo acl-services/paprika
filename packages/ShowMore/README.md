21b15ac1de747762f56d8c80fe583ebb7021c6bc
Mike Polowick <mike_polowick@acl.com>
2019-09-13 18:07:12 -0700

    Add new ShowMore component

---
+++ packages/ShowMore/README.md
@@ -0,0 +1,25 @@
+## ShowMore
+
+An uncontrolled component that ...
+
+### Installation
+
+`> npm install --save @paprika/show-more`
+or
+`> yarn add @paprika/show-more`
+
+### Usage
+
+```js
+import ShowMore from "@paprika/show-more";
+
+const yourComponent = () => {
+  return <ShowMore />;
+};
+
+export default yourComponent;
+```
+
+### Props
+
+[More detail about these props](https://github.com/acl-services/paprika/blob/master/packages/ShowMore/src/ShowMore.js)
---
+++ packages/ShowMore/src/ShowMore.js
@@ -0,0 +1,88 @@
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import PropTypes from "prop-types";
+import truncate from "lodash/truncate";
+import uuid from "uuid/v4";
+import useI18n from "@paprika/l10n/lib/useI18n";
+import Button from "@paprika/button";
+import { toggleStyles } from "./ShowMore.styles";
+
+const propTypes = {
+  a11yText: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  collapsedLength: PropTypes.number,
+};
+
+const defaultProps = {
+  a11yText: null,
+  collapsedLength: 255,
+};
+
+function getTruncatedVersion(content, length) {
+  return truncate(content, {
+    length,
+    separator: " ",
+  });
+}
+
+const ShowMore = props => {
+  const I18n = useI18n();
+  const { a11yText, children, collapsedLength, ...moreProps } = props;
+  const [isCollapsed, setIsCollapsed] = React.useState(true);
+  const [collapsedContent, setCollapsedContent] = React.useState();
+  const contentId = React.useRef(uuid()).current;
+
+  React.useEffect(() => {
+    setCollapsedContent(`${getTruncatedVersion(ReactDOMServer.renderToStaticMarkup(children), collapsedLength)} `);
+  }, [children, collapsedLength]);
+
+  function getToggleLabel() {
+    return isCollapsed ? I18n.t("showMore.more") : I18n.t("showMore.less");
+  }
+
+  function getA11yText() {
+    if (a11yText) {
+      return isCollapsed
+        ? `${I18n.t("showMore.more_about")}: ${a11yText}`
+        : `${I18n.t("showMore.less_about")}: ${a11yText}`;
+    }
+    return getToggleLabel();
+  }
+
+  const handleToggle = () => {
+    setIsCollapsed(prevCollapsed => !prevCollapsed);
+  };
+
+  return (
+    <div data-pka-anchor="show-more" {...moreProps}>
+      {isCollapsed ? (
+        <span
+          data-pka-anchor="show-more.content"
+          id={contentId}
+          dangerouslySetInnerHTML={{ __html: collapsedContent }}
+        />
+      ) : (
+        <span data-pka-anchor="show-more.content" id={contentId}>
+          {children}
+        </span>
+      )}
+      <span isCollapsed={isCollapsed} css={toggleStyles}>
+        <Button
+          a11yText={getA11yText()}
+          aria-controls={contentId}
+          aria-expanded={!isCollapsed}
+          kind="link"
+          onClick={handleToggle}
+        >
+          {getToggleLabel()}
+        </Button>
+      </span>
+    </div>
+  );
+};
+
+ShowMore.displayName = "ShowMore";
+ShowMore.propTypes = propTypes;
+ShowMore.defaultProps = defaultProps;
+
+export default ShowMore;
