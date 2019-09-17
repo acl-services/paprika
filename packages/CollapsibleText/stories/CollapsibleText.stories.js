ac31d3c143c2a2bf672a4d2b340e4aea55cc213a
Mike Polowick <mike_polowick@acl.com>
2019-09-16 17:55:43 -0700

    Add Screener story

--- packages/CollapsibleText/stories/CollapsibleText.stories.js
+++ packages/CollapsibleText/stories/CollapsibleText.stories.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
-
 import Variations from "./examples/Variations";
+import Screener from "./examples/Screener";
 
 storiesOf("CollapsibleText", module)
   .addDecorator(withKnobs)
   .add("Variations", () => <Variations />);
+
+storiesOf("CollapsibleText/Automation Tests", module).add("Screener", () => <Screener />);
