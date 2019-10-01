85565827bd433af9e2ea907a2a0ad6617834f07b
nahumzs <nahum_zsilva@acl.com>
2019-10-01 15:02:51 -0700

    chore: fixing css styles to match better

--- packages/ListBoxBrowser/stories/ListBoxBrowser.stories.js
+++ packages/ListBoxBrowser/stories/ListBoxBrowser.stories.js
@@ -4,13 +4,25 @@ import { storiesOf } from "@storybook/react";
 import ListBoxBrowser from "../src";
 import data from "../test/fixtures";
 
-storiesOf("ListBoxBrowser", module).add("Showcase", () => (
-  <Story>
-    <ListBoxBrowser
-      data={data}
-      onChange={selectedOptions => {
-        console.log("selected options:", selectedOptions);
-      }}
-    />
-  </Story>
-));
+storiesOf("ListBoxBrowser", module)
+  .add("Multi / basic", () => (
+    <Story>
+      <ListBoxBrowser
+        data={data}
+        onChange={selectedOptions => {
+          console.log("selected options:", selectedOptions);
+        }}
+      />
+    </Story>
+  ))
+  .add("Multi / Allowing select parent", () => (
+    <Story>
+      <ListBoxBrowser
+        data={data}
+        isParentSelectable
+        onChange={selectedOptions => {
+          console.log("selected options:", selectedOptions);
+        }}
+      />
+    </Story>
+  ));
