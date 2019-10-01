8eae48097873468cf4b65fcf1c86dba410a20cb0
nahumzs <nahum_zsilva@acl.com>
2019-10-01 11:25:34 -0700

    feature: wip letting user select parents

--- packages/ListBoxBrowser/src/components/CustomListBox/CustomListBox.js
+++ packages/ListBoxBrowser/src/components/CustomListBox/CustomListBox.js
@@ -14,14 +14,17 @@ const propTypes = {
   onUp: PropTypes.func,
   options: PropTypes.arrayOf(PropTypes.object).isRequired,
   onClickNavigate: PropTypes.func.isRequired,
-  selectedOptions: PropTypes.arrayOf(PropTypes.object).isRequired,
+  selectedOptions: PropTypes.object, // eslint-disable-line
   rootKey: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  browserKey: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  isParentSelectable: PropTypes.bool,
 };
 const defaultProps = {
   id: "root",
   hasOnUp: false,
   onUp: () => {},
   rootKey: null,
+  isParentSelectable: null,
 };
 
 function isSelected({ $$key, selectedOptions, browserKey }) {
@@ -32,31 +35,67 @@ function isSelected({ $$key, selectedOptions, browserKey }) {
   return false;
 }
 
+function hasPreventDefaultOnSelect({ hasOptions, isParentSelectable }) {
+  if (isParentSelectable !== null) {
+    return !isParentSelectable;
+  }
+
+  return hasOptions;
+}
+
 export default function CustomListBox(props) {
-  const { id, height, isMulti, onChange, hasOnUp, onUp, options, onClickNavigate, selectedOptions, rootKey } = props;
+  const {
+    browserKey,
+    hasOnUp,
+    height,
+    id,
+    isMulti,
+    onChange,
+    onClickNavigate,
+    onUp,
+    options,
+    rootKey,
+    selectedOptions,
+    isParentSelectable,
+  } = props;
   return (
     <ListBox key={id} height={height} isMulti={isMulti} isInline onChange={onChange}>
       <ListBox.Trigger isHidden></ListBox.Trigger>
       {hasOnUp ? <ListBox.Option onClick={onUp}>../</ListBox.Option> : null}
       {options.map(({ $$key, attributes, hasOptions }) => (
         <ListBox.Option
-          preventDefaultOnSelect={hasOptions}
-          isSelected={isSelected({ $$key, id, selectedOptions })}
+          preventDefaultOnSelect={hasPreventDefaultOnSelect({ hasOptions, isParentSelectable })}
+          isSelected={isSelected({ browserKey, $$key, selectedOptions })}
           key={$$key}
           label={attributes.label}
           value={{ $$key, attributes, hasOptions }}
           data-ppk-is-root-selected={rootKey === $$key}
         >
-          <div css={label}>
-            <div>{attributes.label}</div>
-            <div>
-              {hasOptions ? (
-                <RawButton css={navigateButton} onClick={onClickNavigate($$key, hasOptions)}>
-                  <ArrowRight />
-                </RawButton>
-              ) : null}
-            </div>
-          </div>
+          {({ isSelected }) => {
+            return (
+              <div css={label}>
+                <div>
+                  {!hasOptions || isParentSelectable ? (
+                    <span>
+                      <input tabIndex={-1} aria-hidden type="checkbox" checked={isSelected} onChange={() => {}} />
+                    </span>
+                  ) : null}
+                  <span>{attributes.label}</span>
+                </div>
+                <div>
+                  {hasOptions ? (
+                    <RawButton
+                      a11yText="Browse content (i18n)"
+                      css={navigateButton}
+                      onClick={onClickNavigate($$key, hasOptions)}
+                    >
+                      <ArrowRight />
+                    </RawButton>
+                  ) : null}
+                </div>
+              </div>
+            );
+          }}
         </ListBox.Option>
       ))}
     </ListBox>
