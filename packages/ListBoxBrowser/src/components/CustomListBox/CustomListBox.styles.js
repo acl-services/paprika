7a20016a529f1b0a5b2952a3d0cc0dd13981d1f5
nahumzs <nahum_zsilva@acl.com>
2019-10-02 08:33:40 -0700

    chore: styling and repositioning checkbox

--- packages/ListBoxBrowser/src/components/CustomListBox/CustomListBox.styles.js
+++ packages/ListBoxBrowser/src/components/CustomListBox/CustomListBox.styles.js
@@ -7,18 +7,25 @@ export const navigateButton = css`
   border: 1px solid ${tokens.border.color};
   border-radius: ${tokens.border.radius};
   color: ${tokens.color.black};
-  display: flex;
+  display: inline-flex;
   height: 20px;
   justify-content: center;
   position: relative;
   width: 20px;
+
   ${({ isParentSelectable }) => {
     return isParentSelectable
-      ? ""
+      ? css`
+          margin-right: 4px;
+          top: 1px;
+        `
       : css`
           background: transparent;
           border: 1px solid transparent;
           border-radius: ${tokens.border.radius};
+          margin-left: -6px;
+          top: 2px;
+          width: 18px;
         `;
   }}
 `;
@@ -26,20 +33,21 @@ export const navigateButton = css`
 export const label = css`
   align-items: center;
   display: flex;
+  font-size: 16px;
   justify-content: flex-start;
   position: relative;
-  & > div {
-    flex-grow: 1;
-  }
+`;
 
-  & > div + div {
-    color: ${tokens.color.white};
-    display: flex;
-    justify-content: flex-end;
+export const labelContent = css`
+  flex-grow: 1;
+`;
 
-    svg {
-      left: 1px;
-      position: relative;
-    }
-  }
+export const action = css`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const checkbox = css`
+  position: relative;
+  top: -2px;
 `;
