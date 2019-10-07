78609a41ac4c0caac07ef2c5a6284473a37a05fa
nahumzs <nahum_zsilva@acl.com>
2019-10-07 12:14:21 -0700

    feature: adding context to the component and extracting OptionSelected component

--- packages/ListBoxBrowser/src/components/CustomListBox/CustomListBox.js
+++ packages/ListBoxBrowser/src/components/CustomListBox/CustomListBox.js
@@ -4,29 +4,22 @@ import ListBox from "@paprika/listbox";
 import RawButton from "@paprika/raw-button";
 import ArrowRight from "@paprika/icon/lib/ArrowRight";
 import ArrowLeft from "@paprika/icon/lib/ArrowLeft";
+import { ListBoxBrowserContext } from "../../ListBoxBrowser";
 import { label, navigateButton, action, labelContent, checkbox, backButton } from "./CustomListBox.styles";
 
 const noop = () => () => {};
 const propTypes = {
   id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  height: PropTypes.number.isRequired,
-  isMulti: PropTypes.bool.isRequired,
   onChange: PropTypes.func.isRequired,
   hasOnUp: PropTypes.bool,
   onUp: PropTypes.func,
   options: PropTypes.arrayOf(PropTypes.object).isRequired,
   onClickNavigate: PropTypes.func.isRequired,
-  selectedOptions: PropTypes.object, // eslint-disable-line
-  rootKey: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  browserKey: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  isParentSelectable: PropTypes.bool,
 };
 const defaultProps = {
   id: "root",
   hasOnUp: false,
   onUp: () => {},
-  rootKey: null,
-  isParentSelectable: null,
 };
 
 function isSelected({ $$key, selectedOptions, browserKey }) {
@@ -46,20 +39,12 @@ function isSelectable({ hasOptions, isParentSelectable }) {
 }
 
 export default function CustomListBox(props) {
-  const {
-    browserKey,
-    hasOnUp,
-    height,
-    id,
-    isMulti,
-    onChange,
-    onClickNavigate,
-    onUp,
-    options,
-    rootKey,
-    selectedOptions,
-    isParentSelectable,
-  } = props;
+  const { browserKey, height, isMulti, isParentSelectable, rootKey, selectedOptions } = React.useContext(
+    ListBoxBrowserContext
+  );
+
+  const { id, onChange, options, onClickNavigate, onUp, hasOnUp } = props;
+
   return (
     <ListBox key={id} height={height} isMulti={isMulti} isInline onChange={onChange}>
       <ListBox.Trigger isHidden></ListBox.Trigger>
--- packages/ListBoxBrowser/stories/ListBoxBrowser.single.stories.js
+++ packages/ListBoxBrowser/stories/ListBoxBrowser.single.stories.js
@@ -2,35 +2,10 @@ import React from "react";
 import { Story } from "storybook/assets/styles/common.styles";
 import { storiesOf } from "@storybook/react";
 import ListBoxBrowser from "../src";
-import dataMultiple from "../test/fixtures/multiple";
+
 import dataSingle from "../test/fixtures/single";
 
 storiesOf("ListBoxBrowser", module)
-  .add("Multi / basic", () => (
-    <Story>
-      <ListBoxBrowser
-        data={dataMultiple}
-        rootTitle="Universes"
-        browserTitle="Heroes"
-        onChange={selectedOptions => {
-          console.log("selected options:", selectedOptions);
-        }}
-      />
-    </Story>
-  ))
-  .add("Multi / Allowing select parent", () => (
-    <Story>
-      <ListBoxBrowser
-        data={dataMultiple}
-        isParentSelectable
-        rootTitle="Universes"
-        browserTitle="Heroes"
-        onChange={selectedOptions => {
-          console.log("selected options:", selectedOptions);
-        }}
-      />
-    </Story>
-  ))
   .add("Single / basic", () => (
     <Story>
       <ListBoxBrowser
@@ -42,5 +17,20 @@ storiesOf("ListBoxBrowser", module)
           console.log("selected options:", selectedOptions);
         }}
       />
+    </Story>
+  ))
+  .add("Single / With OptionSelected component", () => (
+    <Story>
+      <ListBoxBrowser
+        data={dataSingle}
+        isMulti={false}
+        rootTitle="Universes"
+        browserTitle="Heroes"
+        onChange={selectedOptions => {
+          console.log("selected options:", selectedOptions);
+        }}
+      >
+        <ListBoxBrowser.OptionsSelected />
+      </ListBoxBrowser>
     </Story>
   ));
