7ca723837f25ba5b6d72490f48ee75cc7344270f
Andrei Chernykh <andrei_chernykh@acl.com>
2019-10-23 11:10:55 -0700

    add useMountedRef hook

---
+++ packages/helpers/test/useMountedRef.spec.js
@@ -0,0 +1,29 @@
+import { renderHook } from "@testing-library/react-hooks";
+
+import useMountedRef from "../src/hooks/useMountedRef";
+
+describe("useMountedRef", () => {
+  test("should increment counter", () => {
+    const {
+      result: { current: mountedRef },
+      rerender,
+      unmount,
+    } = renderHook(() => {
+      const ref = useMountedRef();
+      // Check initial state
+      expect(ref.current).toBe(false);
+      return ref;
+    });
+
+    // After render
+    expect(mountedRef.current).toBe(true);
+
+    rerender();
+    // After re-render
+    expect(mountedRef.current).toBe(true);
+
+    unmount();
+    // After after unmount
+    expect(mountedRef.current).toBe(false);
+  });
+});
