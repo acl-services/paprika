4ff7d7ae64452c390ae9f5ca39055560c684a15d
nahumzs <nahum_zsilva@acl.com>
2019-10-30 13:33:21 -0700

    chore: fixing stylers import

--- packages/ListBoxBrowser/src/components/OptionsSelected/OptionsSelected.styles.js
+++ packages/ListBoxBrowser/src/components/OptionsSelected/OptionsSelected.styles.js
@@ -1,6 +1,6 @@
 import { css } from "styled-components";
 import tokens from "@paprika/tokens/lib/tokens";
-import { spacer } from "@paprika/stylers/lib/helpers";
+import stylers from "@paprika/stylers";
 
 export const container = css`
   display: flex;
