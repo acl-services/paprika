5332a851ba8d1b70fe89e986727625f78ece0d4c
Alice Lee <alicesmlee91@gmail.com>
2019-11-01 10:02:01 -0700

    no divider yet

--- packages/Heading/src/Heading.styles.js
+++ packages/Heading/src/Heading.styles.js
@@ -1,7 +1,9 @@
 import tokens from "@paprika/tokens";
 import stylers from "@paprika/stylers";
 
-const headingStyles = () => `
+// default heading styles
+
+const defaultHeadingStyles = `
   align-items: center;
   color: inherit;
   display: flex;
@@ -14,64 +16,75 @@ const headingStyles = () => `
     outline: none;
   }
 
-  &.heading--level-1 {
-    ${stylers.fontSize(7)}
-    ${stylers.lineHeight(-2)}
-    font-weight: 400;
-  }
-
-  &.heading--level-2 {
-    ${stylers.fontSize(5)}
-    ${stylers.lineHeight(-2)}
-  }
-
-  &.heading--level-3 {
-    ${stylers.fontSize(3)}
-  }
-
-  &.heading--level-4 {
-    ${stylers.fontSize(1)}
-  }
-
-  &.heading--level-5 {
-    ${stylers.fontSize()}
-    margin: ${tokens.space} 0;
-  }
-
-  &.heading--level-6 {
-    ${stylers.fontSize(-1)}
-    margin: ${tokens.space} 0;
-  }
-
-  &.heading--has-underline {
-    border-bottom: 1px solid ${tokens.border.color};
-    &.heading--level-1 {
-      padding-bottom: ${tokens.space};
-    }
-    &.heading--level-2,
-    &.heading--level-3,
-    &.heading--level-4 {
-      padding-bottom: ${tokens.spaceSm};
-    }
-    &.heading--level-5,
-    &.heading--level-6 {
-      padding-bottom: 2px;
-    }
-  }
-
-  &.heading--is-hidden {
-    ${stylers.visuallyHidden}
-  }
-
-  &.heading--is-light {
-    font-weight: 400;
-  }
-
   .heading__divider {
     border-bottom: 2px solid ${tokens.color.blackLighten70};
     flex: 1;
     margin-left: ${tokens.space};
   }
+`;
+
+// heading styles
+
+const heading = {
+  HEADING1: `
+    ${stylers.fontSize(7)}
+    ${stylers.lineHeight(-2)}
+    font-weight: 400;`,
+
+  HEADING2: `
+    ${stylers.fontSize(5)}
+    ${stylers.lineHeight(-2)}`,
+
+  HEADING3: `
+     ${stylers.fontSize(3)}`,
+
+  HEADING4: `
+     ${stylers.fontSize(1)}`,
+
+  HEADING5: `
+    ${stylers.fontSize()}
+    margin: ${tokens.space} 0;`,
+
+  HEADING6: `
+    ${stylers.fontSize(-1)}
+    margin: ${tokens.space} 0;
+  }`,
+};
+
+// underline
+const underline = `
+    border-bottom: 1px solid ${tokens.border.color};
+    h1 {
+      padding-bottom: ${tokens.space};
+    }
+    h2,
+    h3,
+    h4 {
+      padding-bottom: ${tokens.spaceSm};
+    }
+    h5,
+    h6 {
+      padding-bottom: 2px;
+    }`;
+
+// hidden
+
+const hidden = `
+${stylers.visuallyHidden}
+`;
+
+// light
+
+const light = `
+font-weight: 400;
+`;
+
+const headingStyles = props => `
+${defaultHeadingStyles}
+${heading[`HEADING${props.level}`]}
+${props.isHidden ? hidden : ""}
+${props.hasUnderline ? underline : ""}
+${props.isLight ? light : ""}
 `;
 
 export default headingStyles;
