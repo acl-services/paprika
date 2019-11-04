4a842d8aab8aa077404bc0cf1f58dd5324256757
Alice Lee <alicesmlee91@gmail.com>
2019-11-04 13:03:47 -0800

    heading styled component

--- packages/Heading/src/Heading.styles.js
+++ packages/Heading/src/Heading.styles.js
@@ -1,8 +1,6 @@
 import tokens from "@paprika/tokens";
 import stylers from "@paprika/stylers";
 
-// default heading styles
-
 const defaultHeadingStyles = `
   align-items: center;
   color: inherit;
@@ -16,75 +14,64 @@ const defaultHeadingStyles = `
     outline: none;
   }
 
-  .heading__divider {
-    border-bottom: 2px solid ${tokens.color.blackLighten70};
-    flex: 1;
-    margin-left: ${tokens.space};
-  }
 `;
 
-// heading styles
-
 const heading = {
-  HEADING1: `
+  HEADING_LEVEL_1: `
     ${stylers.fontSize(7)}
     ${stylers.lineHeight(-2)}
     font-weight: 400;`,
 
-  HEADING2: `
+  HEADING_LEVEL_2: `
     ${stylers.fontSize(5)}
     ${stylers.lineHeight(-2)}`,
 
-  HEADING3: `
+  HEADING_LEVEL_3: `
      ${stylers.fontSize(3)}`,
 
-  HEADING4: `
+  HEADING_LEVEL_4: `
      ${stylers.fontSize(1)}`,
 
-  HEADING5: `
+  HEADING_LEVEL_5: `
     ${stylers.fontSize()}
     margin: ${tokens.space} 0;`,
 
-  HEADING6: `
+  HEADING_LEVEL_6: `
     ${stylers.fontSize(-1)}
     margin: ${tokens.space} 0;
   }`,
 };
 
-// underline
-const underline = `
-    border-bottom: 1px solid ${tokens.border.color};
-    h1 {
-      padding-bottom: ${tokens.space};
-    }
-    h2,
-    h3,
-    h4 {
-      padding-bottom: ${tokens.spaceSm};
-    }
-    h5,
-    h6 {
-      padding-bottom: 2px;
-    }`;
-
-// hidden
+const underline = level => {
+  let result = `border-bottom: 1px solid ${tokens.border.color};`;
+  if (level === 1) {
+    result += `padding-bottom: ${tokens.space};`;
+  } else if (level > 1 && level < 5) {
+    result += `padding-bottom: ${tokens.spaceSm};`;
+  } else {
+    result += `padding-bottom: 2px;`;
+  }
+  return result;
+};
 
 const hidden = `
 ${stylers.visuallyHidden}
 `;
 
-// light
-
 const light = `
 font-weight: 400;
 `;
 
-const headingStyles = props => `
-${defaultHeadingStyles}
-${heading[`HEADING${props.level}`]}
-${props.isHidden ? hidden : ""}
-${props.hasUnderline ? underline : ""}
-${props.isLight ? light : ""}
+export const dividerStyles = `
+  border-bottom: 2px solid ${tokens.color.blackLighten70};
+  flex: 1;
+  margin-left: ${tokens.space};
 `;
 
-export default headingStyles;
+export const headingStyles = props => `
+${defaultHeadingStyles}
+${heading[`HEADING_LEVEL_${props.safeDisplayLevel || props.safeLevel}`]}
+${props.isHidden ? hidden : ""}
+${props.hasUnderline ? underline(props.safeDisplayLevel || props.safeLevel) : ""}
+${props.isLight ? light : ""}
+`;
