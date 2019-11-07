9abf0d6c426ac520555ed46abd2f8b371ec7b13f
Alice Lee <alicesmlee91@gmail.com>
2019-11-07 09:09:32 -0800

    first testing

--- packages/Select/tests/Select.spec.js
+++ packages/Select/tests/Select.spec.js
@@ -22,4 +22,34 @@ describe("Select", () => {
     );
     expect(getByText(/placeholder value/i)).toBeInTheDocument();
   });
+
+  it("should not display the options", () => {
+    const { queryByText } = render(
+      <Select placeholder="placeholder value">
+        <option value="Select value">Select value</option>
+        <option value="Another option">Another option</option>
+      </Select>
+    );
+    expect(queryByText(/place holder value/i)).not.toBeInTheDocument();
+  });
+
+  it("should be read only", () => {
+    const { container } = render(
+      <Select placeholder="placeholder value" isReadOnly>
+        <option value="Select value">Select value</option>
+        <option value="Another option">Another option</option>
+      </Select>
+    );
+    expect(container.querySelector(".form-select--is-readonly")).toBeInTheDocument();
+  });
+
+  it("should be an error if clicks the wrong option", () => {
+    const { container } = render(
+      <Select placeholder="placeholder value" hasError>
+        <option value="Select value">Select value</option>
+        <option value="Another option">Another option</option>
+      </Select>
+    );
+    expect(container.querySelector(".form-select--has-error")).toBeInTheDocument();
+  });
 });
