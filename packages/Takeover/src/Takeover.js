5dffdab681fd98f28017d93b8c949a656114ed7a
Danil Agafonov <danil.agafonov@flatstack.com>
2019-11-08 14:30:31 +0100

    UX-565 Ignore ESLint warning

--- packages/Takeover/src/Takeover.js
+++ packages/Takeover/src/Takeover.js
@@ -78,6 +78,7 @@ const Takeover = props => {
         >
           {state => (
             <FocusTrapLibrary focusTrapOptions={focusTrapOptions}>
+              {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
               <div
                 css={styles.wrapper}
                 state={state}
