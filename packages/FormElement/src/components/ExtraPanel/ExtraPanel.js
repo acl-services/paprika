495a4a1e09a5f241078a27e05fb30c93ca1a58ac
Tristan Jasper <tjasper9@gmail.com>
2019-11-12 08:54:03 -0800

    Remove extra panel id

--- packages/FormElement/src/components/ExtraPanel/ExtraPanel.js
+++ packages/FormElement/src/components/ExtraPanel/ExtraPanel.js
@@ -4,19 +4,14 @@ import PropTypes from "prop-types";
 import extraPanelStyles from "./ExtraPanel.styles";
 
 const propTypes = {
-  ariaExtraPanelId: PropTypes.string,
   children: PropTypes.node.isRequired,
 };
 
-const defaultProps = {
-  ariaExtraPanelId: null,
-};
-
 function ExtraPanel(props) {
-  const { children, ariaExtraPanelId, ...moreProps } = props;
+  const { children, ...moreProps } = props;
 
   return (
-    <div id={ariaExtraPanelId} css={extraPanelStyles} data-pka-anchor="formElement.extrapanel" {...moreProps}>
+    <div css={extraPanelStyles} data-pka-anchor="formElement.extrapanel" {...moreProps}>
       {children}
     </div>
   );
@@ -25,6 +20,5 @@ function ExtraPanel(props) {
 ExtraPanel.displayName = "FormElement.ExtraPanel";
 
 ExtraPanel.propTypes = propTypes;
-ExtraPanel.defaultProps = defaultProps;
 
 export default ExtraPanel;
