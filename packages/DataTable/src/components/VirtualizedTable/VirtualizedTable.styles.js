c73250cefb9d94fd0f122d13ed26f99da1c8eacc
nahumzs <nahum_zsilva@acl.com>
2019-11-26 11:43:54 -0800

    chore(DataTable): deleting useless code

--- packages/DataTable/src/components/VirtualizedTable/VirtualizedTable.styles.js
+++ packages/DataTable/src/components/VirtualizedTable/VirtualizedTable.styles.js
@@ -83,33 +83,3 @@ export const Expand = styled.div`
   }
   /* Ignore this will be replace with an svg icon */
 `;
-
-export const Cell = styled.div.attrs(({ cellIndex, activeCellIndex, $height, $width, isHeaderCell = false }) => {
-  const style = {};
-  if (cellIndex && activeCellIndex && cellIndex === activeCellIndex) {
-    style.outline = "3px solid #2c7ff9";
-  }
-
-  if ($height) {
-    style.height = isHeaderCell ? "32px" : `${$height}px`;
-    style.alignItems = "center";
-    style.display = "flex";
-  }
-
-  if ($width) {
-    style.flexBasis = `${$width}px`;
-  }
-
-  return {
-    style,
-  };
-})`
-  border-left: 1px solid #dde1e3;
-  overflow: hidden;
-  flex-grow: 0;
-  padding-left: 8px;
-  position: relative;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  width: 100%;
-`;
