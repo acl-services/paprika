92ca2e102d3f4c8f239678df01f34db1c3c80cc1
Danil Agafonov <danil.agafonov@flatstack.com>
2019-11-29 14:19:55 +0100

    feat(takeover): [UX-663] Design tweaks

--- packages/Takeover/src/components/Header.styles.js
+++ packages/Takeover/src/components/Header.styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import { fontSize, spacer } from "@paprika/stylers/lib/helpers";
+import OriginalHeading from "@paprika/heading";
+import { spacer } from "@paprika/stylers/lib/helpers";
 import tokens from "@paprika/tokens/lib/tokens";
 
 const kind = {
@@ -9,29 +10,26 @@ const kind = {
 
 export const Wrapper = styled.div`
   align-items: center;
-  border-bottom: 1px solid ${tokens.border.color};
   box-shadow: ${tokens.shadow};
   box-sizing: border-box;
   display: flex;
-  height: ${spacer(6)};
   justify-content: space-between;
   min-height: ${spacer(6)};
-  padding: 0 ${spacer(2)};
   width: 100%;
+  z-index: 1;
 
   &:focus {
     outline: 0;
   }
 
-  .heading--level-1,
-  .heading--level-2,
-  .heading--level-3,
-  .heading--level-4,
-  .heading--level-5 {
-    ${fontSize()};
-    font-weight: 400;
-    margin: 0;
-  }
-
   ${props => kind[props.kind]}
+`;
+
+export const Heading = styled(OriginalHeading)`
+  margin: 0 0 0 ${spacer(2)};
+`;
+
+export const CloseButtonWrapper = styled.div`
+  border-left: 1px solid ${tokens.border.color};
+  padding: ${spacer(1)};
 `;
