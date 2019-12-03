787e863833d2db8264f95c9f7710da0d19c87313
Danil Agafonov <danil.agafonov@flatstack.com>
2019-12-03 14:10:43 +0100

    refactor(takeover): [UX-663] Use `card.borderRadius` token

--- packages/Takeover/src/components/Content.js
+++ packages/Takeover/src/components/Content.js
@@ -4,7 +4,7 @@ import tokens from "@paprika/tokens/lib/tokens";
 
 const Content = styled.div`
   background-color: ${tokens.color.white};
-  border-radius: ${tokens.border.radius};
+  border-radius: ${tokens.card.borderRadius};
   box-shadow: ${tokens.card.shadow};
   margin: ${spacer(2)};
   padding: ${spacer(2)};
