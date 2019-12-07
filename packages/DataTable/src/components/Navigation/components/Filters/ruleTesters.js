c4fe3c1192e04a8e35460ee4d3d3d5b801c88125
Allison Chen <allison_chen@acl.com>
2019-12-06 16:53:59 -0800

    fix(DataTable): is_blank tester

--- packages/DataTable/src/components/Navigation/components/Filters/ruleTesters.js
+++ packages/DataTable/src/components/Navigation/components/Filters/ruleTesters.js
@@ -5,6 +5,6 @@ export default {
   [rules.IS_NOT]: (value, testValue) => (testValue === "" ? true : value !== testValue),
   [rules.CONTAINS]: (value, testValue) => (testValue === "" ? true : value.includes(testValue)),
   [rules.DOES_NOT_CONTAIN]: (value, testValue) => (testValue === "" ? true : !value.includes(testValue)),
-  [rules.IS_BLANK]: value => value !== null || value !== undefined || value !== "",
+  [rules.IS_BLANK]: value => value === null || value === undefined || value === "",
   [rules.IS_NOT_BLANK]: value => value !== null && value !== undefined && value !== "",
 };
