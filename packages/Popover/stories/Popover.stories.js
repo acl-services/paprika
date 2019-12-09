266c34e2923859d05c8b198e5ce7a3c61a1d41b5
Mike Polowick <mike_polowick@acl.com>
2019-12-08 23:49:26 -0800

    Popover story fixes / new experiments

--- packages/Popover/stories/Popover.stories.js
+++ packages/Popover/stories/Popover.stories.js
@@ -13,14 +13,30 @@ import Cypress, { propHandles } from "./examples/Cypress";
 import Screener from "./examples/Screener";
 import DynamicContent from "./examples/DynamicContent";
 
+const experimentalParameters = {
+  docs: {
+    page: null,
+  },
+  options: {
+    isToolshown: true,
+    showPanel: true,
+    selectedPanel: "storybook/knobs/panel",
+    panelPosition: "bottom",
+  },
+};
+
 storiesOf("Popover", module)
   .addDecorator(withKnobs)
   .add("Basic", Basic)
   .add("Controlled", () => <Controlled />)
   .add("With Trigger Components", () => <WithTriggers />)
   .add("With Positioning Element", PositioningElement)
-  .add("With Scroll Container", ScrollContainer)
   .add("With Dynamic Content", DynamicContent);
+
+storiesOf("Popover", module)
+  .addParameters(experimentalParameters)
+  .addDecorator(withKnobs)
+  .add("With Scroll Container", ScrollContainer);
 
 storiesOf("Popover/Dev", module)
   .add("Has container with a CSS transform", () => <Transformed />)
