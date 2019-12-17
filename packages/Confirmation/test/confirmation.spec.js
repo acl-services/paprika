2e2716a19302024577bfad75fec8c1c1ff4770e5
Tristan Jasper <tjasper9@gmail.com>
2019-12-17 15:10:48 -0800

    add additionaly check for on close

--- packages/Confirmation/test/confirmation.spec.js
+++ packages/Confirmation/test/confirmation.spec.js
@@ -22,14 +22,19 @@ describe("Confirmation", () => {
     expect(getByText(/Confirm Delete/)).toBeVisible();
   });
 
-  it("should trigger the confirm callback when confirm button is clicked", () => {
+  it("should trigger the confirm callback when confirm button is clicked and on Close to not have been called", async () => {
+    const onCloseFunc = jest.fn();
     const onConfirmFunc = jest.fn();
     const { getByText } = renderComponent({
       onConfirm: onConfirmFunc,
+      onClose: onCloseFunc,
     });
     fireEvent.click(getByText(/trigger/i));
     fireEvent.click(getByText(/Confirm Delete/i));
     expect(onConfirmFunc).toHaveBeenCalled();
+    await wait(() => {
+      expect(onCloseFunc).not.toHaveBeenCalled();
+    });
   });
 
   it("should close the confirmation when clicking cancel", async () => {
