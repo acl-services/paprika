ceba8857a063a9b8994d534affa2128017a02fe3
Mike Polowick <mike_polowick@acl.com>
2019-12-30 11:03:23 -0800

    Try the secret API Key

--- screener.config.js
+++ screener.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  apiKey: "809f59e2-e260-4303-a661-7c69d72486ef",
+  apiKey: process.env.SCREENER_API_KEY,
   baseBranch: "master",
   branch: process.env.SEMAPHORE_GIT_BRANCH,
-  // build: process.env.SEMAPHORE_WORKFLOW_ID,
-  // commit: process.env.SEMAPHORE_GIT_SHA,
   initialBaselineBranch: "master",
   projectRepo: "acl-services/paprika",
   resolution: "1280x1024",
   storybookConfigDir: ".storybook",
   storybookStaticDir: "./.storybook/assets",
   failureExitCode: 0, // Don't fail CI builds
-  includeRules: [/Screener/, /^Stylers/, /^Button: Basic/],
+  includeRules: [/Screener/, /^Stylers/, /^Button\/Examples: Basic/],
 };
