2ab2764580f8f2c34e5b874afa034d60bcee8a36
Tristan Jasper <tjasper9@gmail.com>
2020-01-08 14:40:21 -0800

    change to pka anchor

--- packages/Confirmation/src/Confirmation.js
+++ packages/Confirmation/src/Confirmation.js
@@ -121,7 +121,7 @@ const Confirmation = props => {
               kind={confirmButtonType}
               size={buttonSize}
               onClick={handleOnConfirm}
-              data-qa-anchor="confirmation__confirm-button"
+              data-pka-anchor="confirmation__confirm-button"
             >
               {confirmLabel}
             </Button>
@@ -131,7 +131,7 @@ const Confirmation = props => {
               kind={Button.Kinds.MINOR}
               size={buttonSize}
               onClick={handleCloseConfirm}
-              data-qa-anchor="confirmation__cancel-button"
+              data-pka-anchor="confirmation__cancel-button"
             >
               {I18n.t("actions.cancel")}
             </Button>
