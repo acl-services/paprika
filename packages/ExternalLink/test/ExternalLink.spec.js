8bd715eb64ba66cc1741d71d4aa7bdb5eafadee8
Tristan Jasper <tjasper9@gmail.com>
2020-01-23 10:44:53 -0800

    cleanup

--- packages/ExternalLink/test/ExternalLink.spec.js
+++ packages/ExternalLink/test/ExternalLink.spec.js
@@ -2,19 +2,21 @@ import React from "react";
 import { render } from "@testing-library/react";
 import ExternalLink from "../src";
 
+const mockUrl = "http://wegalvanize.com";
+
 describe("ExternalLink", () => {
   it("should render default link word of view", () => {
-    const { getByText } = render(<ExternalLink href="http://wegalvanize.com" />);
+    const { getByText } = render(<ExternalLink href={mockUrl} />);
     expect(getByText(/view/i)).toBeVisible();
   });
 
   it("should render children if provided", () => {
-    const { getByText } = render(<ExternalLink href="http://wegalvanize.com">example text</ExternalLink>);
+    const { getByText } = render(<ExternalLink href={mockUrl}>example text</ExternalLink>);
     expect(getByText(/example text/i)).toBeVisible();
   });
 
   it("should render href on a tag", () => {
-    render(<ExternalLink href="http://wegalvanize.com">example text</ExternalLink>);
-    expect(document.querySelector("a").getAttribute("href")).toBe("http://wegalvanize.com");
+    render(<ExternalLink href={mockUrl}>example text</ExternalLink>);
+    expect(document.querySelector("a").getAttribute("href")).toBe(mockUrl);
   });
 });
