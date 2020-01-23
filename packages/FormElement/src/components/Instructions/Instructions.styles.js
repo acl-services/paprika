5c0d188c02ec0c22fbdb0a5e3f472fea4d80980e
Mike Polowick <mike_polowick@acl.com>
2020-01-23 14:22:01 -0800

    Code style update to FormElement styled components

--- packages/FormElement/src/components/Instructions/Instructions.styles.js
+++ packages/FormElement/src/components/Instructions/Instructions.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import tokens from "@paprika/tokens";
 import stylers from "@paprika/stylers";
 
-export const InstructionsStyled = styled.div`
+export const Instructions = styled.div`
   ${stylers.lineHeight(-1)}
   color: ${tokens.color.blackLighten20};
   margin: ${tokens.spaceSm} 0 0 0;
