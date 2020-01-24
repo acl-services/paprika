9fda7e0b8f592f8afd8d3e024f00d58a27a5d916
Tristan Jasper <tjasper9@gmail.com>
2020-01-23 16:43:03 -0800

    Star 106 Adding External link & NewTab Icon components (#399)
    * Adding ExternalLink comp and new-tab icon
    
    * ensure content truncated
    
    * addeding trucated example
    
    * add width style in storybook
    
    * remove space
    
    * use with heading component and create story
    
    * add unit test
    
    * cleanup
    
    * added changelog
    
    * cleanup
    
    * icon color through style
    
    * change to use hasNoUnderline prop
    
    * spelling
    
    * make children required
    
    * show component in the flow of a block of text
    
    * cleanup
    
    * add npmignore
    
    * add more missing npmignores

---
+++ packages/ExternalLink/src/ExternalLink.styles.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import tokens from "@paprika/tokens";
+import { truncateText } from "@paprika/stylers/lib/includes";
+import { toInt } from "@paprika/stylers/lib/helpers";
+
+export const ExternalLink = styled.a`
+  border-radius: ${tokens.border.radius};
+  color: ${tokens.textColor.link};
+  display: inline-block;
+  max-width: 100%;
+  position: relative;
+
+  ${({ iconFontSize }) => `padding: 1px ${iconFontSize + toInt(tokens.spaceLg)}px 1px ${tokens.spaceSm};`}
+
+  &:focus,
+  &:active {
+    box-shadow: ${tokens.highlight.active.boxShadow};
+    outline: none;
+  }
+`;
+
+const contentHoverFocusStyles = `
+  text-decoration: none;
+  &:hover,
+  &:focus {
+    text-decoration: underline;
+  }
+`;
+
+export const ExternalLinkContent = styled.span`
+  display: inline-block;
+  text-decoration: underline;
+  ${truncateText}
+  vertical-align: bottom;
+  width: 100%;
+
+  ${props => {
+    return props.hasNoUnderline ? contentHoverFocusStyles : "";
+  }};
+`;
+
+export const ExternalLinkIconStyles = `
+  color: ${tokens.textColor.icon};
+  display: inline-block;
+  margin-left: ${tokens.spaceSm};
+  position: absolute;
+  top: 0;
+`;
