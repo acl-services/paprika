655b991180ff9081623c6bfa030841195a064b2c
Danil Agafonov <danil.agafonov@flatstack.com>
2020-02-07 15:01:51 +0100

    feat(icon): [STAR-108] Add data-pka-anchor="icon" for icons

--- packages/Icon/src/ArrowUp.js
+++ packages/Icon/src/ArrowUp.js
@@ -11,6 +11,7 @@ const SvgArrowUp = ({ title, ...props }) => (
       height: ${props.size};
       vertical-align: text-top;
     `}
+    data-pka-anchor="icon"
     focusable={false}
     {...props}
   >
