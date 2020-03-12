59444290638d992897d2d7677e0466cc9b6d1a70
nahumzs <nahum_zsilva@acl.com>
2020-03-12 11:57:09 -0700

    feature(DataGrid): adding expanded column

--- packages/DataGrid/stories/AAA/AAA.js
+++ packages/DataGrid/stories/AAA/AAA.js
@@ -2,7 +2,7 @@ import React from "react";
 import Spinner from "@paprika/spinner";
 import ActionBar, { Sort } from "@paprika/action-bar";
 import ColumnsArrangement from "./components/ColumnsArrangement";
-import DataGrid from "../../src";
+import DataGrid, { renderColumnExpand } from "../../src";
 import useData from "./hooks/useData";
 import useColumnsArragment from "./hooks/useColumnsArragment";
 import useSort from "./hooks/useSort";
@@ -86,6 +86,7 @@ export default function AAA() {
             </ColumnsArrangement>
           </ActionBar>
           <DataGrid hasAutofocus={false} data={sortedData} width={size.width || 640} height={size.height || 400}>
+            {renderColumnExpand()}
             {orderedColumns.map(column => {
               return column.isHidden ? null : (
                 <DataGrid.ColumnDefinition key={column.key} header={column.label} cell={column.key} />
