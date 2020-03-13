a5dcf2aa69b8eb3798a12f3659cc3a04990c93b9
Allison Chen <allison_chen@acl.com>
2020-03-13 12:41:16 -0700

    PD-14332 Adding icon in link button

--- packages/Button/src/LinkButton.js
+++ packages/Button/src/LinkButton.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { ShirtSizes } from "@paprika/helpers/lib/customPropTypes";
+import NewTabIcon from "@paprika/icon/lib/NewTab";
 import Button from "./Button";
 import buttonStyles from "./Button.styles";
 
@@ -11,13 +12,15 @@ const LinkPropTypes = {
   isOpenNewTab: PropTypes.bool,
   kind: PropTypes.oneOf(Button.Kinds.ALL),
   size: PropTypes.oneOf(ShirtSizes.DEFAULT),
+  suffixIcon: PropTypes.node,
 };
 
 const LinkDefaultProps = {
   a11yText: null,
   isOpenNewTab: true,
-  kind: Button.Kinds.DEFAULT,
+  kind: Button.Kinds.LINK,
   size: ShirtSizes.MEDIUM,
+  suffixIcon: <NewTabIcon />,
 };
 
 const isOpenNewTabProps = {
@@ -25,22 +28,25 @@ const isOpenNewTabProps = {
   rel: "noopener noreferrer",
 };
 
-const LinkButton = React.forwardRef(({ a11yText, children, href, isOpenNewTab, kind, size, ...moreProps }, ref) => {
-  return (
-    <a
-      css={buttonStyles}
-      ref={ref}
-      href={href}
-      aria-label={a11yText}
-      kind={kind}
-      size={size}
-      {...(isOpenNewTab ? isOpenNewTabProps : {})}
-      {...moreProps}
-    >
-      {children}
-    </a>
-  );
-});
+const LinkButton = React.forwardRef(
+  ({ a11yText, children, href, isOpenNewTab, kind, size, suffixIcon, ...moreProps }, ref) => {
+    return (
+      <a
+        css={buttonStyles}
+        ref={ref}
+        href={href}
+        aria-label={a11yText}
+        kind={kind}
+        size={size}
+        {...(isOpenNewTab ? isOpenNewTabProps : {})}
+        {...moreProps}
+      >
+        {children}
+        {isOpenNewTab ? suffixIcon : null}
+      </a>
+    );
+  }
+);
 
 LinkButton.Kinds = Button.Kinds;
 LinkButton.displayName = "LinkButton";
