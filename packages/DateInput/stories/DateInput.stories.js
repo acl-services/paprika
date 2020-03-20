bdefbb920b463f4ffb7f431d8f323cf5d29f6029
Danil Agafonov <danil.agafonov@flatstack.com>
2020-03-20 14:39:22 +0100

    docs(date-input): [STAR-156] Add storybook stories

--- packages/DateInput/stories/DateInput.stories.js
+++ packages/DateInput/stories/DateInput.stories.js
@@ -1,7 +1,26 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { select, text, withKnobs } from "@storybook/addon-knobs";
+import { boolean, select, text, withKnobs } from "@storybook/addon-knobs";
 import DateInput from "../src";
+
+const DateInputExample = props => {
+  const [date, setDate] = React.useState(null);
+  const [possibleDate, setPossibleDate] = React.useState(null);
+
+  return (
+    <React.Fragment>
+      <DateInput
+        date={date}
+        possibleDate={possibleDate}
+        onChange={setDate}
+        onChangePossibleDate={setPossibleDate}
+        {...props}
+      />
+      <p>Date: {date ? date.format(props.humanFormat) : <i>empty</i>}</p>
+      <p>Possible date: {possibleDate ? possibleDate.format(props.humanFormat) : <i>empty</i>}</p>
+    </React.Fragment>
+  );
+};
 
 storiesOf("DateInput", module)
   .addDecorator(withKnobs)
@@ -9,7 +28,12 @@ storiesOf("DateInput", module)
     const inputProps = {
       size: select("size", ["small", "medium", "large"], "medium"),
       placeholder: text("placeholder", ""),
+      dateFormat: select("dateFormat", ["MM/DD/YYYY", "DD/MM/YYYY", "YYYY-MM-DD"], "MM/DD/YYYY"),
+      humanFormat: select("humanFormat", ["MMMM DD, YYYY", "YYYY-MM-DD"], "MMMM DD, YYYY"),
+      isDisabled: boolean("isDisabled", false),
+      isReadOnly: boolean("isReadOnly", false),
+      hasError: boolean("hasError", false),
     };
 
-    return <DateInput {...inputProps} />;
+    return <DateInputExample {...inputProps} />;
   });
