eef411aec141f7c502ddec7eeff11f5ca85ca67d
nahumzs <nahum_zsilva@acl.com>
2020-04-15 19:17:22 -0700

    feature STAR-190 Proccessed Readme files

--- packages/ListBox/src/components/Divider/Divider.js
+++ packages/ListBox/src/components/Divider/Divider.js
@@ -3,10 +3,10 @@ import PropTypes from "prop-types";
 import { dividerCSS } from "./Divider.styles";
 
 const propTypes = {
-  // isDisable is use internally as a default prop
-  // the prop is read by the option/helpers/optionState.js which is assigned in the store
-  // it helps to ignore the divider while using the keyboard.
-  // see: options/helpers/options.js
+  /**  isDisable is use internally as a default prop
+  the prop is read by the option/helpers/optionState.js which is assigned in the store
+  it helps to ignore the divider while using the keyboard.
+  see: options/helpers/options.js */
   isDisabled: PropTypes.bool, // eslint-disable-line
   children: PropTypes.node,
 };
