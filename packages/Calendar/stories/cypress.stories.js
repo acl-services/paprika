f094bfcc532773990ee93e1857e75c2470ba8f86
Danil Agafonov <danil.agafonov@flatstack.com>
2020-04-24 18:03:04 +0200

    docs(calendar): [STAR-156] Add showcase for SingleDateCalendar

--- packages/Calendar/stories/cypress.stories.js
+++ packages/Calendar/stories/cypress.stories.js
@@ -1,33 +1,22 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-
 import moment from "moment";
 import L10n from "@paprika/l10n";
-
 import SingleDateCalendar from "../src/SingleDateCalendar";
 
 const noop = () => {};
 
-function Example(props) {
-  const { initialDate, locale } = props;
-  const [date, setDate] = React.useState(initialDate || null);
-
-  moment.locale(locale === "zh" ? "zh-cn" : locale || "en");
-
-  function handleChange(newDate) {
-    setDate(newDate);
-  }
-
-  return (
-    <L10n locale={props.locale}>
-      <SingleDateCalendar date={date} onSelect={handleChange} resetPossibleDate={noop} />
-    </L10n>
-  );
-}
-
 storiesOf("Calendar / cypress", module)
-  .add("SingleDateCalendar test", () => (
-    <Example />
-  ));
+  .add("SingleDateCalendar test", () => {
+    const [date, setDate] = React.useState(null);
+
+    moment.locale("en");
+
+    return (
+      <L10n locale="en">
+        <SingleDateCalendar date={date} onSelect={setDate} resetPossibleDate={noop} />
+      </L10n>
+    );
+  });
 
 
