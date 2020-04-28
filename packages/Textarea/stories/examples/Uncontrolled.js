90fa33ff17c7d6ac6e7ead767f5cb13c95d8c515
Tristan Jasper <tjasper9@gmail.com>
2020-04-28 14:18:34 -0700

    remove button from story

--- packages/Textarea/stories/examples/Uncontrolled.js
+++ packages/Textarea/stories/examples/Uncontrolled.js
@@ -1,21 +1,16 @@
 import React from "react";
 import { Story } from "storybook/assets/styles/common.styles";
-import Button from "@paprika/button/lib/Button";
 import Textarea from "../../src";
 
 const UncontrolledExample = () => {
-  const [defaultState, setDefaultState] = React.useState("hello world");
   return (
     <Story>
       <Textarea
-        defaultValue={defaultState}
+        defaultValue="hello world"
         onChange={e => {
           console.log("the textareas new value is: ", e.target.value);
         }}
       />
-      <Button onClick={() => setDefaultState("updated default state")}>
-        Change default state value (should have no effect)
-      </Button>
     </Story>
   );
 };
