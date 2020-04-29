fbd71221133a6ecb09ee2d5009685fea29966f69
Allison Chen <allison_chen@acl.com>
2020-04-29 14:46:44 -0700

    SNP-131 Fix useOnScrolled issue and call onCancel when the content on blue with footor

--- packages/ListBox/src/components/Content/Content.js
+++ packages/ListBox/src/components/Content/Content.js
@@ -9,10 +9,13 @@ import { ContentStyled } from "./Content.styles";
 
 const propTypes = {
   children: PropTypes.node.isRequired,
+  onCancel: PropTypes.func,
 };
-const defaultProps = {};
+const defaultProps = {
+  onCancel: null,
+};
 
-const handleBlur = (state, dispatch) => () => {
+const handleBlur = (state, dispatch, onCancel) => () => {
   const { refListBoxContainer } = state;
 
   if (state.isDisabled) {
@@ -39,6 +42,7 @@ const handleBlur = (state, dispatch) => () => {
 
       if (state.hasFooter) {
         dispatch({ type: useListBox.types.cancel });
+        if (onCancel) onCancel();
       }
     }
   });
@@ -74,7 +78,7 @@ export default function Content(props) {
 
   return (
     <Popover.Content
-      onBlur={handleBlur(state, dispatch)}
+      onBlur={handleBlur(state, dispatch, props.onCancel)}
       ref={refListBoxContainer}
       {...getDOMAttributesForListBoxContainer()}
       onKeyDown={handleKeyboardKeys({ state, dispatch, onChangeContext })}
