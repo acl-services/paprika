daf094cdd54818a1ee7580e49f7256b97add8573
Tristan Jasper <tjasper9@gmail.com>
2020-05-01 11:48:37 -0700

    expand input tests

--- packages/Input/tests/input.spec.js
+++ packages/Input/tests/input.spec.js
@@ -3,11 +3,36 @@ import { render } from "@testing-library/react";
 import { ShirtSizes } from "@paprika/helpers/lib/customPropTypes";
 import Input from "../src";
 
+function renderComponent(props = {}) {
+  const handleChange = props.onChange || (() => {});
+  const rendered = render(<Input {...props} onChange={() => {}} />);
+  const { rerender } = rendered;
+
+  return {
+    ...rendered,
+    rerender: updatedProps => {
+      rerender(<Input {...updatedProps} onChange={handleChange} />);
+    },
+  };
+}
+
 describe("Input", () => {
-  it("should be visible", () => {
-    const { container } = render(<Input onChange={() => {}} placeholder="First Name" size={ShirtSizes.LARGE} />);
+  it("should be visible and render large text", () => {
+    const { container } = renderComponent({ size: ShirtSizes.LARGE });
     expect(container.firstChild.classList.contains("form-input--large")).toBe(true);
   });
-});
 
-// TODO: add more tests
+  it("should change value when value changes", () => {
+    const { getByTestId, rerender } = renderComponent({ value: "testing value" });
+    expect(getByTestId("input").value).toBe("testing value");
+    rerender({ value: "testing value changed" });
+    expect(getByTestId("input").value).toBe("testing value changed");
+  });
+
+  it("should not change value when defaultValue changes", () => {
+    const { getByTestId, rerender } = renderComponent({ defaultValue: "testing default value" });
+    expect(getByTestId("input").value).toBe("testing default value");
+    rerender({ defaultValue: "testing default value changed" });
+    expect(getByTestId("input").value).not.toBe("testing default value changed");
+  });
+});
