66083ee3b5d53dcc97bfe02f60bc7c086e5e28b5
Tristan Jasper <tjasper9@gmail.com>
2020-05-08 15:43:54 -0700

    update input test

--- packages/Input/tests/input.spec.js
+++ packages/Input/tests/input.spec.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { ShirtSizes } from "@paprika/helpers/lib/customPropTypes";
 import Input from "../src";
 
 function renderComponent(props = {}) {
-  const handleChange = props.onChange || (() => {});
-  const rendered = render(<Input {...props} onChange={() => {}} />);
+  const rendered = render(<Input {...props} />);
   const { rerender } = rendered;
 
   return {
     ...rendered,
     rerender: updatedProps => {
-      rerender(<Input {...updatedProps} onChange={handleChange} />);
+      rerender(<Input {...updatedProps} />);
     },
   };
 }
@@ -22,17 +21,31 @@ describe("Input", () => {
     expect(container.firstChild.classList.contains("form-input--large")).toBe(true);
   });
 
-  it("should change value when value changes", () => {
-    const { getByTestId, rerender } = renderComponent({ value: "testing value" });
-    expect(getByTestId("input").value).toBe("testing value");
-    rerender({ value: "testing value changed" });
-    expect(getByTestId("input").value).toBe("testing value changed");
+  it("should change value when value changes", async () => {
+    let changeValue = null;
+    const onChangeFunc = e => {
+      changeValue = e.target.value;
+    };
+    const { getByTestId, rerender } = renderComponent({ value: "init value", onChange: onChangeFunc });
+    expect(getByTestId("input").value).toBe("init value");
+    fireEvent.change(getByTestId("input"), { target: { value: "input change" } });
+    expect(changeValue).toBe("input change");
+    expect(getByTestId("input").value).toBe("init value");
+    rerender({ value: "input value controlled change" });
+    expect(getByTestId("input").value).toBe("input value controlled change");
   });
 
-  it("should not change value when defaultValue changes", () => {
-    const { getByTestId, rerender } = renderComponent({ defaultValue: "testing default value" });
-    expect(getByTestId("input").value).toBe("testing default value");
-    rerender({ defaultValue: "testing default value changed" });
-    expect(getByTestId("input").value).not.toBe("testing default value changed");
+  it("should set input value with defaultValue and call the inputRef function passing node when input changes", async () => {
+    let node = null;
+    const inputRefFunc = element => {
+      node = element;
+    };
+    const { getByTestId } = renderComponent({
+      defaultValue: "init default value",
+      inputRef: inputRefFunc,
+    });
+    expect(getByTestId("input").value).toBe("init default value");
+    fireEvent.change(getByTestId("input"), { target: { value: "input change" } });
+    expect(node.value).toBe("input change");
   });
 });
