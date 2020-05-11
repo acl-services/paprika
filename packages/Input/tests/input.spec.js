a2bd06e3b11bf62ada9a086508cf1ec3f65a8a51
Tristan Jasper <tjasper9@gmail.com>
2020-05-11 11:24:38 -0700

    cleanup

--- packages/Input/tests/input.spec.js
+++ packages/Input/tests/input.spec.js
@@ -4,16 +4,11 @@ import { ShirtSizes } from "@paprika/helpers/lib/customPropTypes";
 import Input from "../src";
 
 function renderComponent(props = {}) {
-  const rendered = render(<Input {...props} />);
-  const { rerender } = rendered;
-
-  return {
-    ...rendered,
-    rerender: updatedProps => {
-      rerender(<Input {...updatedProps} />);
-    },
-  };
+  return render(<Input {...props} />);
 }
+
+const initialValue = "initial value";
+const updatedValue = "changed value";
 
 describe("Input", () => {
   it("should be visible and render large text", () => {
@@ -21,31 +16,29 @@ describe("Input", () => {
     expect(container.firstChild.classList.contains("form-input--large")).toBe(true);
   });
 
-  it("should change value when value changes", async () => {
+  it("should change value controlled when value changes", async () => {
     let changeValue = null;
     const onChangeFunc = e => {
       changeValue = e.target.value;
     };
-    const { getByTestId, rerender } = renderComponent({ value: "init value", onChange: onChangeFunc });
-    expect(getByTestId("input").value).toBe("init value");
-    fireEvent.change(getByTestId("input"), { target: { value: "input change" } });
-    expect(changeValue).toBe("input change");
-    expect(getByTestId("input").value).toBe("init value");
-    rerender({ value: "input value controlled change" });
-    expect(getByTestId("input").value).toBe("input value controlled change");
+    const { getByTestId } = renderComponent({ value: initialValue, onChange: onChangeFunc });
+    expect(getByTestId("input").value).toBe(initialValue);
+    fireEvent.change(getByTestId("input"), { target: { value: updatedValue } });
+    expect(changeValue).toBe(updatedValue);
+    expect(getByTestId("input").value).toBe(initialValue);
   });
 
-  it("should set input value with defaultValue and call the inputRef function passing node when input changes", async () => {
+  it("should set value uncontrolled using defaultValue and call the inputRef function with node when input changes", async () => {
     let node = null;
     const inputRefFunc = element => {
       node = element;
     };
     const { getByTestId } = renderComponent({
-      defaultValue: "init default value",
+      defaultValue: initialValue,
       inputRef: inputRefFunc,
     });
-    expect(getByTestId("input").value).toBe("init default value");
-    fireEvent.change(getByTestId("input"), { target: { value: "input change" } });
-    expect(node.value).toBe("input change");
+    expect(getByTestId("input").value).toBe(initialValue);
+    fireEvent.change(getByTestId("input"), { target: { value: updatedValue } });
+    expect(node.value).toBe(updatedValue);
   });
 });
