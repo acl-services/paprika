01f2b8f2e5bade47257dd5c8d7c7da8e0465ea11
kcumlat <karen.cumlat@wegalvanize.com>
2020-07-09 13:42:03 -0700

    fix: adjust padding

--- packages/Card/src/components/Content/Content.styles.js
+++ packages/Card/src/components/Content/Content.styles.js
@@ -1,13 +1,9 @@
 import styled from "styled-components";
-import tokens from "@paprika/tokens";
+import stylers from "@paprika/stylers";
 
 export const Content = styled.div`
+  box-sizing: border-box;
   height: 100%;
-  padding: ${tokens.spaceLg};
+  padding: ${stylers.spacer(1)} ${stylers.spacer(2)} ${stylers.spacer(2)} ${stylers.spacer(2)};
   width: 100%;
-
-  &,
-  * {
-    box-sizing: border-box;
-  }
 `;
