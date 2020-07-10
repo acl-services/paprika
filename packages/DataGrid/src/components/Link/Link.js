9c78a9806c739956f898e50f6ca9c684b02f4955
nahumzs <nahum_zsilva@acl.com>
2020-07-10 10:43:49 -0700

    UXD-39 chore(DataGrid) small fix  / 🦄.

--- packages/DataGrid/src/components/Link/Link.js
+++ packages/DataGrid/src/components/Link/Link.js
@@ -2,16 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import * as sc from "./Link.styles";
 
-/**
- * Different scenarios
- * 1.- when the intention is to the link to act as a button but look like a link onClick
- * 2.- when you wants to actually have the look like link act as a real link
- * 3.- when the user has a look like link acting as a link and want to modify the target of the link
- * 4.- when the user wants to fire the look like link contextual menu
- * 5.- when the consumer wants the user to specifically click the link to initiate an action but ignore if the cell was clicked
- * 6.- when the consumer wants the user to click any part of the link or the cell to initiate an action
- */
-
 // https://stackoverflow.com/questions/4907843/open-a-url-in-a-new-tab-and-not-a-new-window
 // https://stackoverflow.com/questions/2405771/is-right-click-a-javascript-event
 // https://stackoverflow.com/questions/7914684/trigger-right-click-using-pure-javascript
