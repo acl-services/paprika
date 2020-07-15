7fe4ee5ebc18f1e00f65b3fb238e73744fb2c6dc
Kaan Darcey <kaan.darcey@wegalvanize.com>
2020-07-15 14:21:19 -0700

    Fix: removed interactive tests

--- packages/Uploader/tests/Uploader.cypress.js
+++ packages/Uploader/tests/Uploader.cypress.js
@@ -16,18 +16,6 @@ describe("Uploader", () => {
     cy.getByText("example.json").should("be.visible");
   });
 
-  //   it("Should show error when failing to upload", () => {
-  //     cy.visitStorybook("forms-uploader-examples--failed-upload-error");
-
-  //     const fileName = "example.json";
-  //     cy.getByTestId("uploader").attachFile(fileName, { subjectType: "drag-n-drop" });
-
-  //     cy.getByText("Internal Server Error").should("be.visible");
-  //     cy.getByTestId("uploader-file-progressBar")
-  //       .its("background")
-  //       .should("eq", "#de4d33");
-  //   });
-
   it("Should show error for file size", () => {
     cy.visitStorybook("forms-uploader-examples--file-too-big-error");
 
@@ -37,70 +25,24 @@ describe("Uploader", () => {
     cy.getByText("File must be smaller than 1B").should("be.visible");
   });
 
-  //   it("Should not auto upload", () => {
-  //     cy.visitStorybook("forms-uploader-examples--no-auto-upload");
+  it("Should display differant types of status", () => {
+    cy.visitStorybook("forms-uploader-examples--file-statuses");
 
-  //     const fileName = "example.json";
-  //     cy.getByTestId("uploader").attachFile(fileName, { subjectType: "drag-n-drop" });
+    cy.getByText("Idle").should("be.visible");
 
-  //     cy.getByText("Idle").should("be.visible");
+    cy.getByText("Uploading 4.4MiB of 11.8MiB").should("be.visible");
+    cy.getByTestId("uploader-file-progressBar")
+      .eq(1)
+      .should("have.css", "background-color", "rgb(120, 92, 186)");
 
-  //     cy.getByText("Start upload").click();
+    cy.getByText("Complete").should("be.visible");
+    cy.getByTestId("uploader-file-progressBar")
+      .eq(2)
+      .should("have.css", "background-color", "rgb(66, 153, 109)");
 
-  //     cy.getByText("Complete").should("be.visible");
-
-  //     cy.getByTestId("uploader-file-progressBar")
-  //       .its("background")
-  //       .should("eq", "#42996d");
-  //   });
-
-  //   it("Should only accept files dropped on the DropZone", () => {
-  //     cy.visitStorybook("forms-uploader-examples--only-accept-files-dropped-on-the-dropzone");
-
-  //     const fileName = "example.json";
-  //     cy.getByTestId("body").attachFile(fileName, { subjectType: "drag-n-drop" });
-
-  //     cy.getByText("example.json").should("not.be.visible");
-
-  //     cy.getByText("Complete").should("not.be.visible");
-  //   });
-
-  //   it("Should only allow one file per upload", () => {
-  //     cy.visitStorybook("forms-uploader-examples--allow-only-one-file-per-upload");
-
-  //     const fileName = "example.json";
-  //     const otherFile = "example2.json";
-  //     cy.getByTestId("uploader")
-  //       .attachFile(fileName, { subjectType: "drag-n-drop" })
-  //       .attachFile(otherFile, { subjectType: "drag-n-drop" });
-
-  //     cy.getByText("example.json").should("not.be.visible");
-  //     cy.getByText("example2.json").should("be.visible");
-
-  //     cy.getByText("Complete").should("be.visible");
-  //   });
-
-  //   it("Should display differant types of status", () => {
-  //     cy.visitStorybook("forms-uploader-examples--file-statuses");
-
-  //     cy.getByText("Idle").should("be.visible");
-
-  //     cy.getByText("Uploading 4.4MiB of 11.8MiB").should("be.visible");
-  //     cy.getByTestId("uploader-file-progressBar")
-  //       .eq(0)
-  //       .its("background")
-  //       .should("eq", "##785cba");
-
-  //     cy.getByText("Complete").should("be.visible");
-  //     cy.getByTestId("uploader-file-progressBar")
-  //       .eq(1)
-  //       .its("background")
-  //       .should("eq", "#42996d");
-
-  //     cy.getByText("Something went wrong").should("be.visible");
-  //     cy.getByTestId("uploader-file-progressBar")
-  //       .eq(2)
-  //       .its("background")
-  //       .should("eq", "#de4d33");
-  //   });
+    cy.getByText("Something went wrong").should("be.visible");
+    cy.getByTestId("uploader-file-progressBar")
+      .eq(3)
+      .should("have.css", "background-color", "rgb(222, 77, 51)");
+  });
 });
