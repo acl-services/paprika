3e9c0b8a62c5e64bb716ce0ceb4ec477dad68066
nahumzs <nahum_zsilva@acl.com>
2020-07-21 15:54:23 -0700

    UXD-51 feature(TimePicker) adding timepicker component  / 🦄.

---
+++ packages/TimePicker/src/TimePicker.styles.js
@@ -0,0 +1,18 @@
+// @import "~Utils/RobotsHelpers";
+import styled from "styled-components";
+import tokens from "@paprika/tokens";
+
+export const CSSHolder = styled.div`
+  .timeinput {
+    position: relative;
+
+    .timeinput-picker {
+      position: absolute;
+    }
+
+    input[type="text"][disabled] {
+      background: ${tokens.color.blackLighten70};
+      cursor: not-allowed;
+    }
+  }
+`;
---
+++ packages/TimePicker/stories/TimePicker.stories.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import TimePicker from "../src";
+
+function Container({ children }) {
+  return <div style={{ padding: "32px", maxWidth: "366px" }}>{children}</div>;
+}
+
+function ScreenerContainer({ children }) {
+  return <div style={{ height: "256px", width: "366px", float: "left" }}>{children}</div>;
+}
+
+storiesOf("TimePicker", module)
+  .add("Basic", () => (
+    <Container>
+      <TimePicker defaultIsVisible />
+    </Container>
+  ))
+  .add("Format 0000", () => (
+    <Container>
+      <TimePicker defaultValue="0000" defaultIsVisible />
+    </Container>
+  ))
+  .add("Format 03:33am", () => (
+    <Container>
+      <TimePicker defaultValue="03:33am" defaultIsVisible />
+    </Container>
+  ))
+  .add("Format 10....22pm2", () => (
+    <Container>
+      <TimePicker defaultValue="10....22pm2" defaultIsVisible />
+    </Container>
+  ))
+  .add("Format 1344", () => (
+    <Container>
+      <TimePicker defaultValue="1344" defaultIsVisible />
+    </Container>
+  ))
+  .add("Format 1pm", () => (
+    <Container>
+      <TimePicker defaultValue="1pm" defaultIsVisible />
+    </Container>
+  ))
+  .add("Format 3333", () => (
+    <Container>
+      <TimePicker defaultValue="3333" />
+    </Container>
+  ))
+  .add("Format 959", () => (
+    <Container>
+      <TimePicker defaultValue="959" defaultIsVisible />
+    </Container>
+  ))
+  .add("Format 6am", () => (
+    <Container>
+      <TimePicker defaultValue="6am" defaultIsVisible />
+    </Container>
+  ))
+  .add("Format 730am  defaultIsVisible=false", () => (
+    <Container>
+      <TimePicker defaultValue="730am" />
+    </Container>
+  ))
+  .add("Screener", () => (
+    <>
+      <ScreenerContainer>
+        <TimePicker defaultValue="730am" />
+      </ScreenerContainer>
+      <ScreenerContainer>
+        <TimePicker defaultValue="6am" defaultIsVisible />
+      </ScreenerContainer>
+      <ScreenerContainer>
+        <TimePicker defaultIsVisible />
+      </ScreenerContainer>
+      <ScreenerContainer>
+        <TimePicker defaultValue="959" defaultIsVisible />
+      </ScreenerContainer>
+      <ScreenerContainer>
+        <TimePicker defaultValue="3333" />
+      </ScreenerContainer>
+      <ScreenerContainer>
+        <TimePicker defaultValue="1pm" defaultIsVisible />
+      </ScreenerContainer>
+      <ScreenerContainer>
+        <TimePicker defaultValue="1344" defaultIsVisible />
+      </ScreenerContainer>
+      <ScreenerContainer>
+        <TimePicker defaultValue="10....22pm2" defaultIsVisible />
+      </ScreenerContainer>
+      <ScreenerContainer>
+        <TimePicker defaultValue="03:33am" defaultIsVisible />
+      </ScreenerContainer>
+      <ScreenerContainer>
+        <TimePicker defaultValue="0000" defaultIsVisible />
+      </ScreenerContainer>
+    </>
+  ));
