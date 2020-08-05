4967599cdd06d9918832876654ccfc6c26484b5b
Allison Chen <allison_chen@acl.com>
2020-08-05 16:22:41 -0700

    UXD-27 Fix form element spacing

--- packages/FormElement/src/components/Content/Content.styles.js
+++ packages/FormElement/src/components/Content/Content.styles.js
@@ -2,5 +2,5 @@ import styled from "styled-components";
 import tokens from "@paprika/tokens";
 
 export const ContentContainer = styled.div`
-  margin: ${tokens.space} 0 ${tokens.spaceLg} 0;
+  margin-bottom: ${tokens.spaceLg};
 `;
