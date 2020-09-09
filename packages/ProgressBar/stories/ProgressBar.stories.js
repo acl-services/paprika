7aca106169ecb957d68b9fe2df6e8fb838adc441
kcumlat <karen.cumlat@wegalvanize.com>
2020-09-09 14:27:07 -0700

    UXD-188 add loading example

--- packages/ProgressBar/stories/ProgressBar.stories.js
+++ packages/ProgressBar/stories/ProgressBar.stories.js
@@ -6,6 +6,7 @@ import { getStoryName } from "storybook/storyTree";
 import Showcase from "./examples/Showcase";
 import ProgressBarModal from "./examples/ProgressBarModal";
 import Screener from "./examples/Screener";
+import Loading from "./examples/Loading";
 
 const storyName = getStoryName("ProgressBar");
 
@@ -13,6 +14,8 @@ storiesOf(storyName, module)
   .addDecorator(withKnobs)
   .add("Showcase", Showcase);
 
-storiesOf(`${storyName}/Examples`, module).add("ProgressBar in Modal", () => <ProgressBarModal />);
+storiesOf(`${storyName}/Examples`, module)
+  .add("ProgressBar in Modal", () => <ProgressBarModal />)
+  .add("Loading ProgressBar", () => <Loading />);
 
 storiesOf(`${storyName}/Backyard/Tests`, module).add("Screener", () => <Screener />);
---
+++ packages/ProgressBar/stories/examples/Loading.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Story } from "storybook/assets/styles/common.styles";
+import Modal from "@paprika/modal";
+import ProgressBar from "../../src";
+
+export default function Loading() {
+  const [value, setValue] = React.useState(0);
+  const [a11yText, setA11yText] = React.useState();
+
+  if (value < 100) {
+    setTimeout(() => {
+      setValue(val => val + 1);
+    }, 1000);
+  } else {
+    setValue(100);
+    setA11yText("Loading complete");
+  }
+
+  return (
+    <Story>
+      <Modal isOpen>
+        <Modal.Content>
+          <ProgressBar completed={value} a11yText={a11yText} />
+        </Modal.Content>
+      </Modal>
+    </Story>
+  );
+}
