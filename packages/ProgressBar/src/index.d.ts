a55b3997a1c3a8552447882a0efed95c8fee5e7a
nahumzs <nahum_zsilva@acl.com>
2020-09-14 13:58:00 -0700

    running npm lerna bootstrap

---
+++ packages/ProgressBar/src/index.d.ts
@@ -0,0 +1,13 @@
+export default ProgressBar;
+
+declare function ProgressBar(props: ProgressBarProps): JSX.Element;
+interface ProgressBarProps {
+  /** Descriptive a11y text for assistive technologies. */
+  a11yText?: string;
+  /** Description for the ProgressBar */
+  bodyText?: string;
+  /** Header text for the ProgressBar */
+  header?: string;
+  /** Specifies how much progress has been completed from 0-100 */
+  completed?: number;
+}
