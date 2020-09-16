ffe7cd3d28f6655577b940ee494ffff49e10e7fd
kcumlat <karen.cumlat@wegalvanize.com>
2020-09-16 13:22:39 -0700

    UXD-159 add types to package.json

--- packages/Tokens/src/index.d.ts
+++ packages/Tokens/src/index.d.ts
@@ -1,238 +1,253 @@
-declare namespace _default {
-  export namespace color {
-    const black: string;
-    const blackLighten10: string;
-    const blackLighten20: string;
-    const blackLighten30: string;
-    const blackLighten40: string;
-    const blackLighten50: string;
-    const blackLighten60: string;
-    const blackLighten70: string;
-    const blackLighten80: string;
-    const blackLighten90: string;
-    const blackDisabled: string;
-    const blackPure: string;
-    const blue: string;
-    const blueDarken20: string;
-    const blueDarken10: string;
-    const blueLighten10: string;
-    const blueLighten20: string;
-    const blueLighten30: string;
-    const blueLighten40: string;
-    const blueLighten50: string;
-    const creme: string;
-    const cremeDarken30: string;
-    const cremeDarken20: string;
-    const cremeDarken10: string;
-    const cremeLighten5: string;
-    const cremeLighten10: string;
-    const green: string;
-    const greenDarken10: string;
-    const greenDarken20: string;
-    const greenLighten10: string;
-    const greenLighten20: string;
-    const greenLighten30: string;
-    const greenLighten40: string;
-    const greenLighten50: string;
-    const offWhite: string;
-    const orange: string;
-    const orangeDarken10: string;
-    const orangeDarken20: string;
-    const orangeDarken30: string;
-    const orangeLighten10: string;
-    const orangeLighten20: string;
-    const orangeLighten30: string;
-    const orangeLighten40: string;
-    const orangeDisabled: string;
-    const orangeHighlight: string;
-    const purple: string;
-    const purpleDarken10: string;
-    const purpleDarken20: string;
-    const purpleLighten10: string;
-    const purpleLighten20: string;
-    const purpleLighten30: string;
-    const purpleLighten40: string;
-    const purpleLighten50: string;
-    const white: string;
-    const yellow: string;
-    const yellowDarken10: string;
-    const yellowDarken20: string;
-    const yellowDarken30: string;
-    const yellowDarken40: string;
-    const yellowLighten10: string;
-    const yellowLighten20: string;
-    const yellowLighten30: string;
-    const gold: string;
-    const silver: string;
-    const eggplant: string;
-    const eggplantLighten10: string;
-    const eggplantLighten20: string;
-    const eggplantLighten30: string;
-    const chartColor01: string;
-    const chartColor02: string;
-    const chartColor03: string;
-    const chartColor04: string;
-    const chartColor05: string;
-    const chartColor06: string;
-    const chartColor07: string;
-    const chartColor08: string;
-    const chartColor09: string;
-    const chartColor10: string;
-    const chartColor11: string;
-    const chartColor12: string;
-  }
-  export const space: string;
-  export const spaceSm: string;
-  export const spaceLg: string;
-  export namespace placeholder {
-    const color_1: string;
-    export { color_1 as color };
-    export const fontStyle: string;
-  }
-  export namespace highlight {
-    namespace active {
-      namespace noBorder {
-        const boxShadow: string;
-        const insetBoxShadow: string;
-        const borderColor: string;
-      }
-      namespace withBorder {
-        const boxShadow_1: string;
-        export { boxShadow_1 as boxShadow };
-        const insetBoxShadow_1: string;
-        export { insetBoxShadow_1 as insetBoxShadow };
-      }
-      namespace nonInteractive {
-        const outline: string;
-      }
+declare module "TokenSquare" {
+    function TokenSquare(props: any): JSX.Element;
+    namespace TokenSquare {
+        export { propTypes };
     }
-    namespace text {
-      export namespace green_1 {
-        const background: string;
-        const font: string;
-      }
-      export { green_1 as green };
-      export namespace orange_1 {
-        const background_1: string;
-        export { background_1 as background };
-        const font_1: string;
-        export { font_1 as font };
-      }
-      export { orange_1 as orange };
-      export namespace yellow_1 {
-        const background_2: string;
-        export { background_2 as background };
-        const font_2: string;
-        export { font_2 as font };
-      }
-      export { yellow_1 as yellow };
+    export default TokenSquare;
+    namespace propTypes {
+        const color: any;
     }
-  }
-  export namespace textColor {
-    const _default: string;
-    export { _default as default };
-    export const subtle: string;
-    export const link: string;
-    export const linkHover: string;
-    export const icon: string;
-  }
-  export namespace fontFamily {
-    const _default_1: string;
-    export { _default_1 as default };
-    export const jp: string;
-    export const kr: string;
-    export const zh: string;
-  }
-  export namespace table {
-    namespace header {
-      const backgroundColor: string;
-    }
-    namespace row {
-      const backgroundColor_1: string;
-      export { backgroundColor_1 as backgroundColor };
-    }
-    namespace rowEven {
-      const backgroundColor_2: string;
-      export { backgroundColor_2 as backgroundColor };
-    }
-  }
-  export namespace border {
-    export const radius: string;
-    const color_2: string;
-    export { color_2 as color };
-    export const hoverColor: string;
-    export const colorTransition: string;
-  }
-  export namespace button {
-    const borderRadius: string;
-  }
-  export namespace card {
-    const borderRadius_1: string;
-    export { borderRadius_1 as borderRadius };
-    export const shadow: string;
-  }
-  export namespace overlay {
-    const animationDuration: string;
-    namespace backdrop {
-      const backgroundColor_3: string;
-      export { backgroundColor_3 as backgroundColor };
-    }
-  }
-  export namespace modal {
-    const backgroundColor_4: string;
-    export { backgroundColor_4 as backgroundColor };
-    const borderRadius_2: string;
-    export { borderRadius_2 as borderRadius };
-    const shadow_1: string;
-    export { shadow_1 as shadow };
-    export namespace sizes {
-      const sm: string;
-      const md: string;
-      const lg: string;
-    }
-    export const margin: string;
-    export const top: string;
-    export namespace footer {
-      const backgroundColor_5: string;
-      export { backgroundColor_5 as backgroundColor };
-    }
-  }
-  export namespace checkbox {
-    const sizeSm: string;
-    const sizeMd: string;
-    const sizeLg: string;
-  }
-  export namespace radio {
-    const sizeSm_1: string;
-    export { sizeSm_1 as sizeSm };
-    const sizeMd_1: string;
-    export { sizeMd_1 as sizeMd };
-    const sizeLg_1: string;
-    export { sizeLg_1 as sizeLg };
-  }
-  export namespace popover {
-    const borderRadius_3: string;
-    export { borderRadius_3 as borderRadius };
-    const shadow_2: string;
-    export { shadow_2 as shadow };
-  }
-  export namespace counter {
-    const small: string;
-    const medium: string;
-  }
-  export namespace pill {
-    const smallRadius: string;
-    const mediumRadius: string;
-  }
-  export namespace backgroundColor_6 {
-    const level0: string;
-  }
-  export { backgroundColor_6 as backgroundColor };
-  export namespace fontSize {
-    const _default_2: string;
-    export { _default_2 as default };
-  }
-  const shadow_3: string;
-  export { shadow_3 as shadow };
 }
-export default _default;
+declare module "tokens" {
+    export default tokens;
+    namespace tokens {
+        export namespace color {
+            const black: string;
+            const blackLighten10: string;
+            const blackLighten20: string;
+            const blackLighten30: string;
+            const blackLighten40: string;
+            const blackLighten50: string;
+            const blackLighten60: string;
+            const blackLighten70: string;
+            const blackLighten80: string;
+            const blackLighten90: string;
+            const blackDisabled: string;
+            const blackPure: string;
+            const blue: string;
+            const blueDarken20: string;
+            const blueDarken10: string;
+            const blueLighten10: string;
+            const blueLighten20: string;
+            const blueLighten30: string;
+            const blueLighten40: string;
+            const blueLighten50: string;
+            const creme: string;
+            const cremeDarken30: string;
+            const cremeDarken20: string;
+            const cremeDarken10: string;
+            const cremeLighten5: string;
+            const cremeLighten10: string;
+            const green: string;
+            const greenDarken10: string;
+            const greenDarken20: string;
+            const greenLighten10: string;
+            const greenLighten20: string;
+            const greenLighten30: string;
+            const greenLighten40: string;
+            const greenLighten50: string;
+            const offWhite: string;
+            const orange: string;
+            const orangeDarken10: string;
+            const orangeDarken20: string;
+            const orangeDarken30: string;
+            const orangeLighten10: string;
+            const orangeLighten20: string;
+            const orangeLighten30: string;
+            const orangeLighten40: string;
+            const orangeDisabled: string;
+            const orangeHighlight: string;
+            const purple: string;
+            const purpleDarken10: string;
+            const purpleDarken20: string;
+            const purpleLighten10: string;
+            const purpleLighten20: string;
+            const purpleLighten30: string;
+            const purpleLighten40: string;
+            const purpleLighten50: string;
+            const white: string;
+            const yellow: string;
+            const yellowDarken10: string;
+            const yellowDarken20: string;
+            const yellowDarken30: string;
+            const yellowDarken40: string;
+            const yellowLighten10: string;
+            const yellowLighten20: string;
+            const yellowLighten30: string;
+            const gold: string;
+            const silver: string;
+            const eggplant: string;
+            const eggplantLighten10: string;
+            const eggplantLighten20: string;
+            const eggplantLighten30: string;
+            const chartColor01: string;
+            const chartColor02: string;
+            const chartColor03: string;
+            const chartColor04: string;
+            const chartColor05: string;
+            const chartColor06: string;
+            const chartColor07: string;
+            const chartColor08: string;
+            const chartColor09: string;
+            const chartColor10: string;
+            const chartColor11: string;
+            const chartColor12: string;
+        }
+        export const space: string;
+        export const spaceSm: string;
+        export const spaceLg: string;
+        export namespace placeholder {
+            const color_1: string;
+            export { color_1 as color };
+            export const fontStyle: string;
+        }
+        export namespace highlight {
+            namespace active {
+                namespace noBorder {
+                    const boxShadow: string;
+                    const insetBoxShadow: string;
+                    const borderColor: string;
+                }
+                namespace withBorder {
+                    const boxShadow_1: string;
+                    export { boxShadow_1 as boxShadow };
+                    const insetBoxShadow_1: string;
+                    export { insetBoxShadow_1 as insetBoxShadow };
+                }
+                namespace nonInteractive {
+                    const outline: string;
+                }
+            }
+            namespace text {
+                export namespace green_1 {
+                    const background: string;
+                    const font: string;
+                }
+                export { green_1 as green };
+                export namespace orange_1 {
+                    const background_1: string;
+                    export { background_1 as background };
+                    const font_1: string;
+                    export { font_1 as font };
+                }
+                export { orange_1 as orange };
+                export namespace yellow_1 {
+                    const background_2: string;
+                    export { background_2 as background };
+                    const font_2: string;
+                    export { font_2 as font };
+                }
+                export { yellow_1 as yellow };
+            }
+        }
+        export namespace textColor {
+            const _default: string;
+            export { _default as default };
+            export const subtle: string;
+            export const link: string;
+            export const linkHover: string;
+            export const icon: string;
+        }
+        export namespace fontFamily {
+            const _default_1: string;
+            export { _default_1 as default };
+            export const jp: string;
+            export const kr: string;
+            export const zh: string;
+        }
+        export namespace table {
+            namespace header {
+                const backgroundColor: string;
+            }
+            namespace row {
+                const backgroundColor_1: string;
+                export { backgroundColor_1 as backgroundColor };
+            }
+            namespace rowEven {
+                const backgroundColor_2: string;
+                export { backgroundColor_2 as backgroundColor };
+            }
+        }
+        export namespace border {
+            export const radius: string;
+            const color_2: string;
+            export { color_2 as color };
+            export const hoverColor: string;
+            export const colorTransition: string;
+        }
+        export namespace button {
+            const borderRadius: string;
+        }
+        export namespace card {
+            const borderRadius_1: string;
+            export { borderRadius_1 as borderRadius };
+            export const shadow: string;
+        }
+        export namespace overlay {
+            const animationDuration: string;
+            namespace backdrop {
+                const backgroundColor_3: string;
+                export { backgroundColor_3 as backgroundColor };
+            }
+        }
+        export namespace modal {
+            const backgroundColor_4: string;
+            export { backgroundColor_4 as backgroundColor };
+            const borderRadius_2: string;
+            export { borderRadius_2 as borderRadius };
+            const shadow_1: string;
+            export { shadow_1 as shadow };
+            export namespace sizes {
+                const sm: string;
+                const md: string;
+                const lg: string;
+            }
+            export const margin: string;
+            export const top: string;
+            export namespace footer {
+                const backgroundColor_5: string;
+                export { backgroundColor_5 as backgroundColor };
+            }
+        }
+        export namespace checkbox {
+            const sizeSm: string;
+            const sizeMd: string;
+            const sizeLg: string;
+        }
+        export namespace radio {
+            const sizeSm_1: string;
+            export { sizeSm_1 as sizeSm };
+            const sizeMd_1: string;
+            export { sizeMd_1 as sizeMd };
+            const sizeLg_1: string;
+            export { sizeLg_1 as sizeLg };
+        }
+        export namespace popover {
+            const borderRadius_3: string;
+            export { borderRadius_3 as borderRadius };
+            const shadow_2: string;
+            export { shadow_2 as shadow };
+        }
+        export namespace counter {
+            const small: string;
+            const medium: string;
+        }
+        export namespace pill {
+            const smallRadius: string;
+            const mediumRadius: string;
+        }
+        export namespace backgroundColor_6 {
+            const level0: string;
+        }
+        export { backgroundColor_6 as backgroundColor };
+        export namespace fontSize {
+            const _default_2: string;
+            export { _default_2 as default };
+        }
+        const shadow_3: string;
+        export { shadow_3 as shadow };
+    }
+}
+declare module "index" {
+    export { default } from "./tokens";
+}
