7bc8262d892cb52bacd462e862be74070904e36d
kcumlat <karen.cumlat@wegalvanize.com>
2020-09-18 14:11:16 -0700

    UXD-249 fix(types): tokens and constants, update package.json

---
+++ packages/Constants/src/Constants.d.ts
@@ -0,0 +1,63 @@
+export namespace align {
+    const TOP: string;
+    const RIGHT: string;
+    const BOTTOM: string;
+    const LEFT: string;
+}
+export namespace color {
+    const BLACK: string;
+    const BLUE: string;
+    const GREEN: string;
+    const GREY: string;
+    const ORANGE: string;
+    const LIGHT_BLUE: string;
+    const LIGHT_ORANGE: string;
+    const RED: string;
+}
+export namespace kind {
+    const DEFAULT: string;
+    const PRIMARY: string;
+    const SECONDARY: string;
+    const DESTRUCTIVE: string;
+    const FLAT: string;
+    const MINOR: string;
+    const LINK: string;
+}
+export namespace size {
+    const AUTO: string;
+    const XSMALL: string;
+    const SMALL: string;
+    const MEDIUM: string;
+    const LARGE: string;
+    const XLARGE: string;
+}
+export namespace autoSize {
+    const AUTO_1: string;
+    export { AUTO_1 as AUTO };
+    const SMALL_1: string;
+    export { SMALL_1 as SMALL };
+    const MEDIUM_1: string;
+    export { MEDIUM_1 as MEDIUM };
+    const LARGE_1: string;
+    export { LARGE_1 as LARGE };
+}
+export namespace defaultSize {
+    const SMALL_2: string;
+    export { SMALL_2 as SMALL };
+    const MEDIUM_2: string;
+    export { MEDIUM_2 as MEDIUM };
+    const LARGE_2: string;
+    export { LARGE_2 as LARGE };
+}
+export namespace limitedSize {
+    const SMALL_3: string;
+    export { SMALL_3 as SMALL };
+    const MEDIUM_3: string;
+    export { MEDIUM_3 as MEDIUM };
+}
+export namespace gridTypes {
+    const GRID: string;
+    const NONE: string;
+    const HORIZONTAL: string;
+    const VERTICAL: string;
+}
