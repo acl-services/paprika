2f4e478f20edc5a83f69143a6941aa1cd7c375f3
nahumzs <nahum_zsilva@acl.com>
2020-09-24 18:44:30 -0700

    UXD-257 feature(FieldTypes): Adding README support  / 🦄.

--- packages/FieldTypes/src/FieldTypes.js
+++ packages/FieldTypes/src/FieldTypes.js
@@ -1,10 +1,11 @@
+import React from "react";
 import PropTypes from "prop-types";
 import Numeric from "./components/Numeric";
 import Container from "./components/Container";
 import types from "./types";
 
 export default function FieldTypes(props) {
-  return props.children;
+  return <>{props.children}</>;
 }
 
 FieldTypes.types = types;
