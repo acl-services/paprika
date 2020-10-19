71a5943befc5b3982d9498e5fdcba266cc186a2b
Kaan Darcey <kaan.darcey@wegalvanize.com>
2020-10-19 14:06:29 -0700

    UXD-338 Docs: added mdx

--- packages/Popover/stories/examples/Variations.js
+++ packages/Popover/stories/examples/Variations.js
@@ -1,93 +1,84 @@
 import React from "react";
 import { CenteredStory, Gap, Tagline, Rule, Story } from "storybook/assets/styles/common.styles";
+import Button from "@paprika/button";
+import Popover from "@paprika/popover";
 import ButtonExample from "./ButtonExample";
-import InputExample from "./InputExample";
-import IconExample from "./IconExample";
-import LinkIsEagerExample from "./LinkIsEagerExample";
 import ButtonIsEagerExample from "./ButtonIsEagerExample";
-import IsDarkExample from "./IsDarkExample";
-import LinkExample from "./LinkExample";
-import NoCardExample from "./NoCardExample";
 import ShouldKeepFocusExample from "./ShouldKeepFocusExample";
-import ButtonStateExample from "./ButtonStateExample";
 
 const Variations = () => (
   <CenteredStory>
     <div>
-      <Tagline>
-        Render prop: <code>&lt;Button&gt;</code>
-      </Tagline>
+      <Tagline>Popover Button</Tagline>
       <Story>
         <ButtonExample />
       </Story>
       <Gap />
       <Rule />
-      <Tagline>
-        Render prop: <code>&lt;input&gt;</code> / <code>shouldKeepFocus</code>
-      </Tagline>
-      <Story>
-        <InputExample />
-      </Story>
-      <Gap />
-      <Rule />
-      <Tagline>
-        Render prop: <code>&lt;Icon&gt;</code> / <code>isEager</code>
-      </Tagline>
-      <Story>
-        <IconExample />
-      </Story>
-      &nbsp;More info
-      <Gap />
-      <Rule />
-      <Tagline>
-        Render prop: &lt;a&gt; link <code>isEager</code>
-      </Tagline>
-      <Story>
-        <LinkIsEagerExample />
-      </Story>
-      <Gap />
-      <Rule />
-      <Tagline>
-        Render prop: &lt;a&gt; Button <code>isEager</code>
-      </Tagline>
+      <Tagline>Popover isEager</Tagline>
       <Story>
         <ButtonIsEagerExample />
       </Story>
       <Gap />
       <Rule />
-      <Tagline>Node: &lt;a&gt; link</Tagline>
+      <Tagline>PopOver isDark</Tagline>
       <Story>
-        <LinkExample />
+        <Popover isEager isDark defaultIsOpen>
+          <Popover.Trigger data-test-attr="propagated">
+            <span role="img" aria-label="bulb light">
+              💡
+            </span>
+          </Popover.Trigger>
+          <Popover.Content>
+            <Popover.Card>Lorem ipsum single-origin kombucha.</Popover.Card>
+          </Popover.Content>
+        </Popover>
       </Story>
       <Gap />
       <Rule />
-      <Tagline>
-        Render prop: &lt;a&gt; link <code>isEager</code> without <code>Popover.Card</code> component
-      </Tagline>
-      <Story>
-        <NoCardExample />
-      </Story>
-      <Gap />
-      <Rule />
-      <Tagline>
-        Node: <code>isEager isDark</code>
-      </Tagline>
-      <Story>
-        <IsDarkExample />
-      </Story>
-      <Gap />
-      <Rule />
-      <Tagline>
-        Node: <code>shouldKeepFocus</code>
-      </Tagline>
+      <Tagline>Popover shouldKeepFocus</Tagline>
       <Story>
         <ShouldKeepFocusExample />
       </Story>
       <Gap />
       <Rule />
-      <Tagline>Render prop: &lt;a&gt; Button with different state</Tagline>
+      <Tagline>Popover defaultIsOpen</Tagline>
       <Story>
-        <ButtonStateExample />
+        <Popover defaultIsOpen>
+          <Popover.Trigger data-test-attr="propagated">
+            {handler => <Button onClick={handler}>defaultIsOpen</Button>}
+          </Popover.Trigger>
+          <Popover.Content data-test-attr="propagated">
+            <Popover.Card data-test-attr="propagated">
+              Lorem ipsum single-origin kombucha butcher gentrify foraged flannel.
+            </Popover.Card>
+          </Popover.Content>
+        </Popover>
+      </Story>
+      <Gap />
+      <Rule />
+      <Tagline>Popover edge</Tagline>
+      <Story>
+        <Popover edge={Popover.types.align.LEFT}>
+          <Popover.Trigger data-test-attr="propagated">
+            {handler => <Button onClick={handler}>edge left</Button>}
+          </Popover.Trigger>
+          <Popover.Content data-test-attr="propagated">
+            <Popover.Card data-test-attr="propagated">
+              Lorem ipsum single-origin kombucha butcher gentrify foraged flannel.
+            </Popover.Card>
+          </Popover.Content>
+        </Popover>
+        <Popover edge={Popover.types.align.RIGHT}>
+          <Popover.Trigger data-test-attr="propagated">
+            {handler => <Button onClick={handler}>edge right</Button>}
+          </Popover.Trigger>
+          <Popover.Content data-test-attr="propagated">
+            <Popover.Card data-test-attr="propagated">
+              Lorem ipsum single-origin kombucha butcher gentrify foraged flannel.
+            </Popover.Card>
+          </Popover.Content>
+        </Popover>
       </Story>
       <Gap />
     </div>
