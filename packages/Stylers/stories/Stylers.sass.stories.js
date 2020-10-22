d92f7a679ed3775b7d51a3d1ccee38767d0699e6
Mike Polowick <mike_polowick@acl.com>
2020-10-22 15:46:03 -0700

    Convert Stylers stories to CSF

--- packages/Stylers/stories/Stylers.sass.stories.js
+++ packages/Stylers/stories/Stylers.sass.stories.js
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
 
 import React from "react";
-import { storiesOf } from "@storybook/react";
 import { Story, Rule } from "storybook/assets/styles/common.styles";
 import { getStoryName } from "storybook/storyTree";
 import { Box } from "./Stylers.stories.styles";
@@ -9,181 +8,184 @@ import "./helpers.scss";
 
 const storyName = getStoryName("Stylers");
 
-storiesOf(`${storyName}/Sass`, module)
-  .add("Mixins", () => (
-    <Story className="story--stylers">
-      <h1>Mixin Examples</h1>
+export default {
+  title: `${storyName}/Sass`,
+};
 
-      <h4>
-        <code>@truncate-text</code>
-      </h4>
-      <Box className="truncated-box">
-        Lorem ipsum hexagon street art paleo selfies locavore ethical single-origin vaporware mixtape.
-      </Box>
-      <h4>
-        <code>@no-truncate-text</code>
-      </h4>
-      <Box className="truncated-box untruncated-box">
-        Lorem ipsum hexagon street art paleo selfies locavore ethical single-origin vaporware mixtape.
-      </Box>
+export const mixins = () => (
+  <Story className="story--stylers">
+    <h1>Mixin Examples</h1>
 
-      <Rule />
+    <h4>
+      <code>@truncate-text</code>
+    </h4>
+    <Box className="truncated-box">
+      Lorem ipsum hexagon street art paleo selfies locavore ethical single-origin vaporware mixtape.
+    </Box>
+    <h4>
+      <code>@no-truncate-text</code>
+    </h4>
+    <Box className="truncated-box untruncated-box">
+      Lorem ipsum hexagon street art paleo selfies locavore ethical single-origin vaporware mixtape.
+    </Box>
 
-      <h4>
-        <code>@is-visually-hidden</code>
-      </h4>
-      <Box className="invisible-box">
-        <span role="img" aria-label="ghost">
-          👻
-        </span>
-      </Box>
+    <Rule />
 
-      <Rule />
+    <h4>
+      <code>@is-visually-hidden</code>
+    </h4>
+    <Box className="invisible-box">
+      <span role="img" aria-label="ghost">
+        👻
+      </span>
+    </Box>
 
-      <h4>
-        <code>@placeholders</code>
-      </h4>
-      <Box>
-        <input placeholder="placeholder" />
-        <br />
-        <input placeholder="placeholder" disabled />
-      </Box>
+    <Rule />
 
-      <Rule />
+    <h4>
+      <code>@placeholders</code>
+    </h4>
+    <Box>
+      <input placeholder="placeholder" />
+      <br />
+      <input placeholder="placeholder" disabled />
+    </Box>
 
-      <h3>Focus Indicators</h3>
-      <h4>
-        <code>@focus-ring()</code>
-      </h4>
-      <div className="focus-box focus-box--default" tabIndex={0} />
-      <h4>
-        <code>@focus-ring(true)</code>
-      </h4>
-      <div className="focus-box focus-box--inset" tabIndex={0} />
-      <h4>
-        <code>@focus-ring--bordered()</code>
-      </h4>
-      <div className="focus-box focus-box--bordered" tabIndex={0} />
-      <h4>
-        <code>@focus-ring--bordered(true)</code>
-      </h4>
-      <div className="focus-box focus-box--bordered--inset" tabIndex={0} />
-      <h4>
-        <code>@focus-ring--subtle()</code>
-      </h4>
-      <div className="focus-box focus-box--subtle" tabIndex={0} />
-      <h4>
-        <code>@focus-ring--subtle(true)</code>
-      </h4>
-      <div className="focus-box focus-box--subtle--inset" tabIndex={0} />
-    </Story>
-  ))
+    <Rule />
 
-  .add("Functions", () => (
-    <Story className="story--stylers">
-      <h1>Function Examples</h1>
+    <h3>Focus Indicators</h3>
+    <h4>
+      <code>@focus-ring()</code>
+    </h4>
+    <div className="focus-box focus-box--default" tabIndex={0} />
+    <h4>
+      <code>@focus-ring(true)</code>
+    </h4>
+    <div className="focus-box focus-box--inset" tabIndex={0} />
+    <h4>
+      <code>@focus-ring--bordered()</code>
+    </h4>
+    <div className="focus-box focus-box--bordered" tabIndex={0} />
+    <h4>
+      <code>@focus-ring--bordered(true)</code>
+    </h4>
+    <div className="focus-box focus-box--bordered--inset" tabIndex={0} />
+    <h4>
+      <code>@focus-ring--subtle()</code>
+    </h4>
+    <div className="focus-box focus-box--subtle" tabIndex={0} />
+    <h4>
+      <code>@focus-ring--subtle(true)</code>
+    </h4>
+    <div className="focus-box focus-box--subtle--inset" tabIndex={0} />
+  </Story>
+);
 
-      <h4>
-        <code>font-scale()</code>
-      </h4>
-      <div className="font-scale">
-        <span className="font-scale--5">
-          <span>-5</span>
-          <span>9px</span>
-        </span>
-        <span className="font-scale--4">
-          <span>-4</span>
-          <span>10px</span>
-        </span>
-        <span className="font-scale--3">
-          <span>-3</span>
-          <span>11px</span>
-        </span>
-        <span className="font-scale--2">
-          <span>-2</span>
-          <span>13px</span>
-        </span>
-        <span className="font-scale--1">
-          <span>-1</span>
-          <span>14px</span>
-        </span>
-        <span className="font-scale-0">
-          <span>0</span>
-          <span>16px</span>
-        </span>
-        <span className="font-scale-1">
-          <span>1</span>
-          <span>18px</span>
-        </span>
-        <span className="font-scale-2">
-          <span>2</span>
-          <span>20px</span>
-        </span>
-        <span className="font-scale-3">
-          <span>3</span>
-          <span>23px</span>
-        </span>
-        <span className="font-scale-4">
-          <span>4</span>
-          <span>26px</span>
-        </span>
-        <span className="font-scale-5">
-          <span>5</span>
-          <span>29px</span>
-        </span>
-        <span className="font-scale-6">
-          <span>6</span>
-          <span>32px</span>
-        </span>
-        <span className="font-scale-7">
-          <span>7</span>
-          <span>36px</span>
-        </span>
-      </div>
+export const functions = () => (
+  <Story className="story--stylers">
+    <h1>Function Examples</h1>
 
-      <Rule />
+    <h4>
+      <code>font-scale()</code>
+    </h4>
+    <div className="font-scale">
+      <span className="font-scale--5">
+        <span>-5</span>
+        <span>9px</span>
+      </span>
+      <span className="font-scale--4">
+        <span>-4</span>
+        <span>10px</span>
+      </span>
+      <span className="font-scale--3">
+        <span>-3</span>
+        <span>11px</span>
+      </span>
+      <span className="font-scale--2">
+        <span>-2</span>
+        <span>13px</span>
+      </span>
+      <span className="font-scale--1">
+        <span>-1</span>
+        <span>14px</span>
+      </span>
+      <span className="font-scale-0">
+        <span>0</span>
+        <span>16px</span>
+      </span>
+      <span className="font-scale-1">
+        <span>1</span>
+        <span>18px</span>
+      </span>
+      <span className="font-scale-2">
+        <span>2</span>
+        <span>20px</span>
+      </span>
+      <span className="font-scale-3">
+        <span>3</span>
+        <span>23px</span>
+      </span>
+      <span className="font-scale-4">
+        <span>4</span>
+        <span>26px</span>
+      </span>
+      <span className="font-scale-5">
+        <span>5</span>
+        <span>29px</span>
+      </span>
+      <span className="font-scale-6">
+        <span>6</span>
+        <span>32px</span>
+      </span>
+      <span className="font-scale-7">
+        <span>7</span>
+        <span>36px</span>
+      </span>
+    </div>
 
-      <h4>
-        <code>line-height-scale()</code>
-      </h4>
-      <Box className="line-height--3">
-        <code>line-height(-3) === 0.98</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </Box>
-      <Box className="line-height--2">
-        <code>line-height(-2) === 1.11</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </Box>
-      <Box className="line-height--1">
-        <code>line-height(-1) === 1.24</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </Box>
-      <Box className="line-height-0">
-        <code>line-height() === 1.4</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </Box>
-      <Box className="line-height-1">
-        <code>line-height(1) === 1.58</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </Box>
-      <Box className="line-height-2">
-        <code>line-height(2) === 1.77</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </Box>
-      <Box className="line-height-3">
-        <code>line-height(3) === 1.99</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </Box>
+    <Rule />
 
-      <Rule />
+    <h4>
+      <code>line-height-scale()</code>
+    </h4>
+    <Box className="line-height--3">
+      <code>line-height(-3) === 0.98</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </Box>
+    <Box className="line-height--2">
+      <code>line-height(-2) === 1.11</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </Box>
+    <Box className="line-height--1">
+      <code>line-height(-1) === 1.24</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </Box>
+    <Box className="line-height-0">
+      <code>line-height() === 1.4</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </Box>
+    <Box className="line-height-1">
+      <code>line-height(1) === 1.58</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </Box>
+    <Box className="line-height-2">
+      <code>line-height(2) === 1.77</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </Box>
+    <Box className="line-height-3">
+      <code>line-height(3) === 1.99</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </Box>
 
-      <h4>
-        <code>z()</code>
-      </h4>
-      <Box className="z">
-        {[...Array(8).keys()].map(index => (
-          <span key={index}>{index}</span>
-        ))}
-      </Box>
-    </Story>
-  ));
+    <Rule />
+
+    <h4>
+      <code>z()</code>
+    </h4>
+    <Box className="z">
+      {[...Array(8).keys()].map(index => (
+        <span key={index}>{index}</span>
+      ))}
+    </Box>
+  </Story>
+);
--- packages/Stylers/stories/Stylers.stories.js
+++ packages/Stylers/stories/Stylers.stories.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { storiesOf } from "@storybook/react";
 import { Story, Rule } from "storybook/assets/styles/common.styles";
 import { getStoryName } from "storybook/storyTree";
 import {
@@ -18,181 +17,184 @@ import {
 
 const storyName = getStoryName("Stylers");
 
-storiesOf(storyName, module)
-  .add("Includes", () => (
-    <Story>
-      <h1>Include Examples</h1>
+export default {
+  title: storyName,
+};
 
-      <h4>
-        <code>stylers.truncateText()</code>
-      </h4>
-      <TruncatedBox>Lorem ipsum tumeric direct trade snackwave locavore taxidermy live-edge wolf mixtape.</TruncatedBox>
+export const includes = () => (
+  <Story>
+    <h1>Include Examples</h1>
 
-      <h4>
-        <code>stylers.noTruncateText()</code>
-      </h4>
-      <UntruncatedBox>
-        Lorem ipsum tumeric direct trade snackwave locavore taxidermy live-edge wolf mixtape.
-      </UntruncatedBox>
+    <h4>
+      <code>stylers.truncateText()</code>
+    </h4>
+    <TruncatedBox>Lorem ipsum tumeric direct trade snackwave locavore taxidermy live-edge wolf mixtape.</TruncatedBox>
 
-      <Rule />
+    <h4>
+      <code>stylers.noTruncateText()</code>
+    </h4>
+    <UntruncatedBox>
+      Lorem ipsum tumeric direct trade snackwave locavore taxidermy live-edge wolf mixtape.
+    </UntruncatedBox>
 
-      <h4>
-        <code>stylers.visuallyHidden</code>
-      </h4>
-      <InvisibleBox>
-        <span role="img" aria-label="ghost">
-          👻
-        </span>
-      </InvisibleBox>
+    <Rule />
 
-      <Rule />
+    <h4>
+      <code>stylers.visuallyHidden</code>
+    </h4>
+    <InvisibleBox>
+      <span role="img" aria-label="ghost">
+        👻
+      </span>
+    </InvisibleBox>
 
-      <h4>
-        <code>stylers.placeholders()</code>
-      </h4>
-      <Box>
-        <InputWithPlaceholder placeholder="placeholder" />
-        <br />
-        <InputWithPlaceholder placeholder="placeholder" disabled />
-      </Box>
+    <Rule />
 
-      <Rule />
+    <h4>
+      <code>stylers.placeholders()</code>
+    </h4>
+    <Box>
+      <InputWithPlaceholder placeholder="placeholder" />
+      <br />
+      <InputWithPlaceholder placeholder="placeholder" disabled />
+    </Box>
 
-      <h3>Focus Indicators</h3>
-      <h4>
-        <code>stylers.focusRing()</code>
-      </h4>
-      <FocusBox.Default tabIndex={0} />
-      <h4>
-        <code>stylers.focusRing(true)</code>
-      </h4>
-      <FocusBox.Inset tabIndex={0} />
-      <h4>
-        <code>stylers.focusRing.bordered()</code>
-      </h4>
-      <FocusBox.Bordered tabIndex={0} />
-      <h4>
-        <code>stylers.focusRing.bordered(true)</code>
-      </h4>
-      <FocusBox.Bordered.Inset tabIndex={0} />
-      <h4>
-        <code>stylers.focusRing.subtle()</code>
-      </h4>
-      <FocusBox.Subtle tabIndex={0} />
-      <h4>
-        <code>stylers.focusRing.subtle(true)</code>
-      </h4>
-      <FocusBox.Subtle.Inset tabIndex={0} />
-    </Story>
-  ))
+    <Rule />
 
-  .add("Helpers", () => (
-    <Story>
-      <h1>Helper Examples</h1>
+    <h3>Focus Indicators</h3>
+    <h4>
+      <code>stylers.focusRing()</code>
+    </h4>
+    <FocusBox.Default tabIndex={0} />
+    <h4>
+      <code>stylers.focusRing(true)</code>
+    </h4>
+    <FocusBox.Inset tabIndex={0} />
+    <h4>
+      <code>stylers.focusRing.bordered()</code>
+    </h4>
+    <FocusBox.Bordered tabIndex={0} />
+    <h4>
+      <code>stylers.focusRing.bordered(true)</code>
+    </h4>
+    <FocusBox.Bordered.Inset tabIndex={0} />
+    <h4>
+      <code>stylers.focusRing.subtle()</code>
+    </h4>
+    <FocusBox.Subtle tabIndex={0} />
+    <h4>
+      <code>stylers.focusRing.subtle(true)</code>
+    </h4>
+    <FocusBox.Subtle.Inset tabIndex={0} />
+  </Story>
+);
 
-      <h4>
-        <code>stylers.fontSize()</code>
-      </h4>
-      <FontScale>
-        <FontStep scale={-5}>
-          <span>-5</span>
-          <span>9px</span>
-        </FontStep>
-        <FontStep scale={-4}>
-          <span>-4</span>
-          <span>10px</span>
-        </FontStep>
-        <FontStep scale={-3}>
-          <span>-3</span>
-          <span>11px</span>
-        </FontStep>
-        <FontStep scale={-2}>
-          <span>-2</span>
-          <span>13px</span>
-        </FontStep>
-        <FontStep scale={-1}>
-          <span>-1</span>
-          <span>14px</span>
-        </FontStep>
-        <FontStep scale={0}>
-          <span>0</span>
-          <span>16px</span>
-        </FontStep>
-        <FontStep scale={1}>
-          <span>1</span>
-          <span>18px</span>
-        </FontStep>
-        <FontStep scale={2}>
-          <span>2</span>
-          <span>20px</span>
-        </FontStep>
-        <FontStep scale={3}>
-          <span>3</span>
-          <span>23px</span>
-        </FontStep>
-        <FontStep scale={4}>
-          <span>4</span>
-          <span>26px</span>
-        </FontStep>
-        <FontStep scale={5}>
-          <span>5</span>
-          <span>29px</span>
-        </FontStep>
-        <FontStep scale={6}>
-          <span>6</span>
-          <span>32px</span>
-        </FontStep>
-        <FontStep scale={7}>
-          <span>7</span>
-          <span>36px</span>
-        </FontStep>
-      </FontScale>
+export const helpers = () => (
+  <Story>
+    <h1>Helper Examples</h1>
 
-      <Rule />
+    <h4>
+      <code>stylers.fontSize()</code>
+    </h4>
+    <FontScale>
+      <FontStep scale={-5}>
+        <span>-5</span>
+        <span>9px</span>
+      </FontStep>
+      <FontStep scale={-4}>
+        <span>-4</span>
+        <span>10px</span>
+      </FontStep>
+      <FontStep scale={-3}>
+        <span>-3</span>
+        <span>11px</span>
+      </FontStep>
+      <FontStep scale={-2}>
+        <span>-2</span>
+        <span>13px</span>
+      </FontStep>
+      <FontStep scale={-1}>
+        <span>-1</span>
+        <span>14px</span>
+      </FontStep>
+      <FontStep scale={0}>
+        <span>0</span>
+        <span>16px</span>
+      </FontStep>
+      <FontStep scale={1}>
+        <span>1</span>
+        <span>18px</span>
+      </FontStep>
+      <FontStep scale={2}>
+        <span>2</span>
+        <span>20px</span>
+      </FontStep>
+      <FontStep scale={3}>
+        <span>3</span>
+        <span>23px</span>
+      </FontStep>
+      <FontStep scale={4}>
+        <span>4</span>
+        <span>26px</span>
+      </FontStep>
+      <FontStep scale={5}>
+        <span>5</span>
+        <span>29px</span>
+      </FontStep>
+      <FontStep scale={6}>
+        <span>6</span>
+        <span>32px</span>
+      </FontStep>
+      <FontStep scale={7}>
+        <span>7</span>
+        <span>36px</span>
+      </FontStep>
+    </FontScale>
 
-      <h4>
-        <code>stylers.lineHeight()</code>
-      </h4>
-      <LeadingStep scale={-3}>
-        <code>line-height(-3) === 0.98</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </LeadingStep>
-      <LeadingStep scale={-2}>
-        <code>line-height(-2) === 1.11</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </LeadingStep>
-      <LeadingStep scale={-1}>
-        <code>line-height(-1) === 1.24</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </LeadingStep>
-      <LeadingStep scale={0}>
-        <code>line-height() === 1.4</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </LeadingStep>
-      <LeadingStep scale={1}>
-        <code>line-height(1) === 1.58</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </LeadingStep>
-      <LeadingStep scale={2}>
-        <code>line-height(2) === 1.77</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </LeadingStep>
-      <LeadingStep scale={3}>
-        <code>line-height(3) === 1.99</code>
-        <br /> Hexagon street art selfies locavore ethical mixtape.
-      </LeadingStep>
-      <Rule />
+    <Rule />
 
-      <h4>
-        <code>stylers.z()</code>
-      </h4>
-      <ZBox>
-        {[...Array(8).keys()].map(index => (
-          <ZStep key={index} level={index}>
-            {index}
-          </ZStep>
-        ))}
-      </ZBox>
-    </Story>
-  ));
+    <h4>
+      <code>stylers.lineHeight()</code>
+    </h4>
+    <LeadingStep scale={-3}>
+      <code>line-height(-3) === 0.98</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </LeadingStep>
+    <LeadingStep scale={-2}>
+      <code>line-height(-2) === 1.11</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </LeadingStep>
+    <LeadingStep scale={-1}>
+      <code>line-height(-1) === 1.24</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </LeadingStep>
+    <LeadingStep scale={0}>
+      <code>line-height() === 1.4</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </LeadingStep>
+    <LeadingStep scale={1}>
+      <code>line-height(1) === 1.58</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </LeadingStep>
+    <LeadingStep scale={2}>
+      <code>line-height(2) === 1.77</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </LeadingStep>
+    <LeadingStep scale={3}>
+      <code>line-height(3) === 1.99</code>
+      <br /> Hexagon street art selfies locavore ethical mixtape.
+    </LeadingStep>
+    <Rule />
+
+    <h4>
+      <code>stylers.z()</code>
+    </h4>
+    <ZBox>
+      {[...Array(8).keys()].map(index => (
+        <ZStep key={index} level={index}>
+          {index}
+        </ZStep>
+      ))}
+    </ZBox>
+  </Story>
+);
