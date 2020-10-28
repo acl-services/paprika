12b07c84360329dc956cd89dd931caf8309b1619
Jamie Kupka <jamie_kupka@acl.com>
2020-10-28 12:12:57 -0700

    refactor(CommitValidator): undefined clean up the code. change order of validation that occurs

--- scripts/commit-validator.js
+++ scripts/commit-validator.js
@@ -2,65 +2,88 @@
 const fs = require("fs");
 const shell = require("shelljs");
 
-const PATH_TO_COMMIT_MESSAGE = ".git/COMMIT_EDITMSG";
-const commitMessage = fs.readFileSync(PATH_TO_COMMIT_MESSAGE, "utf8");
-const VALID_TYPES = ["feat", "fix", "style", "test", "docs", "build", "chore", "ci", "perf", "refactor", "revert"];
-
-function exitWithError(errorMessage) {
-  // Colour codes: https://stackoverflow.com/a/41407246/1532919
-  const bgRed = "\x1b[41m";
-  const fontWhite = "\x1b[37m";
-  const reset = "\x1b[0m";
-
-  console.error(bgRed, fontWhite, "COMMIT VALIDATOR ERROR:", errorMessage, reset);
-  process.exit(1);
-}
-
-/**
- * A valid format is like: "fix(MyComponent): this is what i did", and uses an approved "type"
- */
-function isValidCommitMessage(commitMessage) {
-  const type = commitMessage.substr(0, commitMessage.indexOf("("));
-  return VALID_TYPES.indexOf(type) > -1 && !!commitMessage.match(/^\w*\(\w+\): .+/g);
-}
-
-/**
- * @returns string    The ticket ID, e.g. "ABC-1234"
- */
-function getTicketIDFromBranchName() {
-  const branchName = shell.exec("git branch --show-current", { silent: true }).stdout.replace("\n", "");
-  const errorMessage = `Your branch name (${branchName}) must be formatted like: "abc-1234--some-description", where "abc-1234" is the JIRA ticket number.`;
-  const parts = branchName.split("-");
-
-  if (parts.length <= 1) {
-    exitWithError(errorMessage);
+class CommitValidator {
+  constructor() {
+    this.PATH_TO_COMMIT_MESSAGE = ".git/COMMIT_EDITMSG";
+    this.VALID_TYPES = ["feat", "fix", "style", "test", "docs", "build", "chore", "ci", "perf", "refactor", "revert"];
+    this.branchName = "";
+    this.brigadeAbbrev = "";
+    this.ticketCounter = "";
+    this.ticketId = "";
+    this.commitMessage = "";
   }
 
-  const brigadeAbbrev = parts[0];
-  if (brigadeAbbrev.length < 2 || brigadeAbbrev.length > 5) {
-    exitWithError(errorMessage);
+  exitWithError(errorMessage) {
+    // Colour codes: https://stackoverflow.com/a/41407246/1532919
+    const bgRed = "\x1b[41m";
+    const fontWhite = "\x1b[37m";
+    const reset = "\x1b[0m";
+
+    console.error(bgRed, fontWhite, "COMMIT VALIDATOR ERROR:", errorMessage, reset);
+    process.exit(1);
   }
 
-  const ticketCounter = parts[1];
-  if (ticketCounter.length < 2 || ticketCounter.length > 5) {
-    exitWithError(errorMessage);
+  validateAndGetBranchName() {
+    const branchName = shell.exec("git branch --show-current", { silent: true }).stdout.replace("\n", "");
+    if (branchName === "") {
+      this.exitWithError(
+        "Your branch name was not detected. Please ensure you have git version 2.22 or greater (git --version)."
+      );
+    }
+
+    return branchName;
   }
 
-  return `${brigadeAbbrev.toUpperCase()}-${ticketCounter}`;
+  getPillarAbbreviationFromBranchName() {
+    return this.branchName.split("-")[0];
+  }
+
+  validateAndGetTicketCounterFromBranchName() {
+    const ticketCounter = this.branchName.split("-")[1];
+
+    if (!ticketCounter) {
+      this.exitWithError(
+        `A valid JIRA ticket ID was not found in your branch name (${this.branchName}). Please name your branch following the pattern 'abc-1234--some-description' and try again.`
+      );
+    }
+
+    return ticketCounter;
+  }
+
+  getAndValidateCommitMessage() {
+    const commitMessage = fs.readFileSync(this.PATH_TO_COMMIT_MESSAGE, "utf8");
+    const type = commitMessage.substr(0, commitMessage.indexOf("("));
+
+    if (this.VALID_TYPES.indexOf(type) === -1) {
+      this.exitWithError(
+        `Your commit message has an invalid type (${type}). Please use one of the supported types: ${this.VALID_TYPES.toString()}`
+      );
+    }
+
+    if (!commitMessage.match(/^\w*\(\w+\): .+/g)) {
+      this.exitWithError(
+        `Your commit message must follow the "Conventional commits" format, eg: "fix(AffectedComponent): this is what i did"`
+      );
+    }
+
+    return commitMessage;
+  }
+
+  updateCommitMessage() {
+    const newCommitMessage = this.commitMessage.replace(/\):/i, `): ${this.ticketID}`);
+    fs.writeFileSync(this.PATH_TO_COMMIT_MESSAGE, newCommitMessage, { encoding: "utf8", flag: "w" });
+  }
+
+  run() {
+    this.branchName = this.validateAndGetBranchName();
+    this.brigadeAbbrev = this.getPillarAbbreviationFromBranchName();
+    this.ticketCounter = this.validateAndGetTicketCounterFromBranchName();
+    this.ticketId = `${this.brigadeAbbrev.toUpperCase()}-${this.ticketCounter}`;
+    this.commitMessage = this.getAndValidateCommitMessage();
+    this.updateCommitMessage();
+    process.exit(0);
+  }
 }
 
-function saveNewCommitMessage(newCommitMessage) {
-  fs.writeFileSync(PATH_TO_COMMIT_MESSAGE, newCommitMessage, { encoding: "utf8", flag: "w" });
-}
-
-if (!isValidCommitMessage(commitMessage)) {
-  exitWithError(
-    `Your commit message must follow the "Conventional commits" format, eg: "fix(MyComponent): this is what i did".\nValid types are: ${VALID_TYPES.toString()}`
-  );
-}
-
-const ticketID = getTicketIDFromBranchName();
-const newCommitMessage = commitMessage.replace(/\)\:/i, `): ${ticketID}`);
-saveNewCommitMessage(newCommitMessage);
-
-process.exit(0);
+const CV = new CommitValidator();
+CV.run();
