33363ecbe598e9c2679554d1081551008a27027e
nahumzs <nahum_zsilva@acl.com>
2020-11-02 14:10:17 -0800

    feature(ListBoxTags): UXD-374 cleaning

--- packages/ListBox/stories/examples/multi.js
+++ packages/ListBox/stories/examples/multi.js
@@ -98,17 +98,9 @@ export const WithGroupsAndHavePreselectedOptions = () => (
     <ListBox.Divider>Antiheroes</ListBox.Divider>
     <ListBox.Option isSelected>Michael Corleone</ListBox.Option>
     {characters.antiHeroes}
-    {Array(100)
-      .fill(null)
-      .map((a, index) => {
-        return <ListBox.Option>{index}</ListBox.Option>;
-      })}
-
     <ListBox.Option isSelected>Mad Max</ListBox.Option>
-
     <ListBox.Divider>Villians</ListBox.Divider>
     {characters.villians}
-
     <ListBox.Divider>Heroes</ListBox.Divider>
     <ListBox.Option isSelected>Aquaman</ListBox.Option>
     {characters.heroes}
--- packages/ListBoxTags/stories/ListBoxTags.uncontrolled-users.stories.js
+++ packages/ListBoxTags/stories/ListBoxTags.uncontrolled-users.stories.js
@@ -11,41 +11,8 @@ export default {
   title: storyName,
 };
 
-const data = [
-  { year: 1961, nationality: "(ITA)", name: "Federico Fellini", movie: "La Dolce Vita, Italian" },
-  { year: 1962, nationality: "(ITA)", name: "Pietro Germi", movie: "Divorce Italian Style, Italian" },
-  { year: 1963, nationality: "(ITA)", name: "Federico Fellini", movie: "8½, Italian" },
-  { year: 1964, nationality: "(GRE)", name: "Michael Cacoyannis", movie: "Zorba the Greek, Greek" },
-  { year: 1965, nationality: "(JPN)", name: "Hiroshi Teshigahara", movie: "Woman in the Dunes, Japanese" },
-  { year: 1966, nationality: "(ITA)", name: "Michelangelo Antonioni", movie: "Blowup, Italian" },
-  { year: 1966, nationality: "(FRA)", name: "Claude Lelouch", movie: "A Man and a Woman, French" },
-  { year: 1968, nationality: "(ITA)", name: "Gillo Pontecorvo", movie: "The Battle of Algiers, Arabic & French" },
-  { year: 1969, nationality: "(GRE)", name: "Costa-Gavras", movie: "Z, French †" },
-  { year: 1970, nationality: "(ITA)", name: "Federico Fellini", movie: "Fellini Satyricon, Italian" },
-  { year: 1972, nationality: "(SWE)", name: "Jan Troell", movie: "The Emigrants, Swedish †" },
-  { year: 1973, nationality: "(SWE)", name: "Ingmar Bergman", movie: "Cries and Whispers, Swedish †" },
-  { year: 1974, nationality: "(FRA)", name: "François Truffaut", movie: "Day Night, French" },
-  { year: 1975, nationality: "(ITA)", name: "Federico Fellini", movie: "Amarcord, Italian" },
-  { year: 1976, nationality: "(SWE)", name: "Ingmar Bergman", movie: "Face to Face, Swedish" },
-  { year: 1976, nationality: "(ITA)", name: "Lina Wertmüller", movie: "Seven Beauties, Italian" },
-  { year: 1979, nationality: "(FRA)", name: "Edouard Molinaro", movie: "La Cage aux Folles, French" },
-  { year: 1982, nationality: "(GER)", name: "Wolfgang Petersen", movie: "Das Boot, German" },
-  { year: 1983, nationality: "(SWE)", name: "Ingmar Bergman", movie: "Fanny and Alexander, Swedish" },
-  { year: 1985, nationality: "(JPN)", name: "Akira Kurosawa", movie: "Ran, Japanese" },
-  { year: 1987, nationality: "(SWE)", name: "Lasse Hallström", movie: "My Life as a Dog, Swedish" },
-  { year: 1994, nationality: "(POL)", name: "Krzysztof Kieślowski", movie: "Three Colours: Red, French" },
-  { year: 1995, nationality: "(GBR)", name: "Michael Radford", movie: "Il Postino: The Postman, Italian & Spanish †" },
-  { year: 1998, nationality: "(ITA)", name: "Roberto Benigni", movie: "Life Is Beautiful, Italian †" },
-  { year: 2000, nationality: "(TWN)", name: "Ang Lee", movie: "Crouching Tiger, Hidden Dragon, Mandarin Chinese †" },
-  { year: 2002, nationality: "(ESP)", name: "Pedro Almodóvar", movie: "Talk to Her, Spanish" },
-  { year: 2003, nationality: "(BRA)", name: "Fernando Meirelles", movie: "City of God, Portuguese" },
-  { year: 2007, nationality: "(USA)", name: "Julian Schnabel", movie: "The Diving Bell and the Butterfly, French" },
-  { year: 2011, nationality: "(FRA)", name: "Michel Hazanavicius", movie: "The Artist, French ‡" },
-  { year: 2012, nationality: "(AUT)", name: "Michael Haneke", movie: "Amour, French †" },
-  { year: 2018, nationality: "(MEX)", name: "Alfonso Cuarón", movie: "Roma, Spanish †" },
-  { year: 2018, nationality: "(POL)", name: "Paweł Pawlikowski", movie: "Cold War, Polish" },
-  { year: 2019, nationality: "(KOR)", name: "Bong Joon-ho", movie: "Parasite, Korean ‡" },
-];
+// prettier-ignore
+const data = [{ year: 1961, nationality: "(ITA)", name: "Federico Fellini", movie: "La Dolce Vita, Italian" },{ year: 1962, nationality: "(ITA)", name: "Pietro Germi", movie: "Divorce Italian Style, Italian" },{ year: 1963, nationality: "(ITA)", name: "Federico Fellini", movie: "8½, Italian" },{ year: 1964, nationality: "(GRE)", name: "Michael Cacoyannis", movie: "Zorba the Greek, Greek" },{ year: 1965, nationality: "(JPN)", name: "Hiroshi Teshigahara", movie: "Woman in the Dunes, Japanese" },{ year: 1966, nationality: "(ITA)", name: "Michelangelo Antonioni", movie: "Blowup, Italian" },{ year: 1966, nationality: "(FRA)", name: "Claude Lelouch", movie: "A Man and a Woman, French" },{ year: 1968, nationality: "(ITA)", name: "Gillo Pontecorvo", movie: "The Battle of Algiers, Arabic & French" },{ year: 1969, nationality: "(GRE)", name: "Costa-Gavras", movie: "Z, French †" },{ year: 1970, nationality: "(ITA)", name: "Federico Fellini", movie: "Fellini Satyricon, Italian" },{ year: 1972, nationality: "(SWE)", name: "Jan Troell", movie: "The Emigrants, Swedish †" },{ year: 1973, nationality: "(SWE)", name: "Ingmar Bergman", movie: "Cries and Whispers, Swedish †" },{ year: 1974, nationality: "(FRA)", name: "François Truffaut", movie: "Day Night, French" },{ year: 1975, nationality: "(ITA)", name: "Federico Fellini", movie: "Amarcord, Italian" },{ year: 1976, nationality: "(SWE)", name: "Ingmar Bergman", movie: "Face to Face, Swedish" },{ year: 1976, nationality: "(ITA)", name: "Lina Wertmüller", movie: "Seven Beauties, Italian" },{ year: 1979, nationality: "(FRA)", name: "Edouard Molinaro", movie: "La Cage aux Folles, French" },{ year: 1982, nationality: "(GER)", name: "Wolfgang Petersen", movie: "Das Boot, German" },{ year: 1983, nationality: "(SWE)", name: "Ingmar Bergman", movie: "Fanny and Alexander, Swedish" },{ year: 1985, nationality: "(JPN)", name: "Akira Kurosawa", movie: "Ran, Japanese" },{ year: 1987, nationality: "(SWE)", name: "Lasse Hallström", movie: "My Life as a Dog, Swedish" },{ year: 1994, nationality: "(POL)", name: "Krzysztof Kieślowski", movie: "Three Colours: Red, French" },{ year: 1995, nationality: "(GBR)", name: "Michael Radford", movie: "Il Postino: The Postman, Italian & Spanish †" },{ year: 1998, nationality: "(ITA)", name: "Roberto Benigni", movie: "Life Is Beautiful, Italian †" },{ year: 2000, nationality: "(TWN)", name: "Ang Lee", movie: "Crouching Tiger, Hidden Dragon, Mandarin Chinese †" },{ year: 2002, nationality: "(ESP)", name: "Pedro Almodóvar", movie: "Talk to Her, Spanish" },{ year: 2003, nationality: "(BRA)", name: "Fernando Meirelles", movie: "City of God, Portuguese" },{ year: 2007, nationality: "(USA)", name: "Julian Schnabel", movie: "The Diving Bell and the Butterfly, French" },{ year: 2011, nationality: "(FRA)", name: "Michel Hazanavicius", movie: "The Artist, French ‡" },{ year: 2012, nationality: "(AUT)", name: "Michael Haneke", movie: "Amour, French †" },{ year: 2018, nationality: "(MEX)", name: "Alfonso Cuarón", movie: "Roma, Spanish †" },{ year: 2018, nationality: "(POL)", name: "Paweł Pawlikowski", movie: "Cold War, Polish" },{ year: 2019, nationality: "(KOR)", name: "Bong Joon-ho", movie: "Parasite, Korean ‡" }];
 
 const processedData = data.map((d, index) => {
   const [name, lastName] = d.name.split(" ");
