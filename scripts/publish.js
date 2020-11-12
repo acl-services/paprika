63cdf47c20915b30e63321e2175936952c2d9471
Allison Chen <allison_chen@acl.com>
2020-11-12 11:14:01 -0800

    chore(Paprika): UXD-499 add publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,3 +1,28 @@
+const fs = require("fs");
 const shell = require("shelljs");
 
-shell.exec(`yarn lerna publish --force-publish`);
+const changedPackages = [];
+const ALPHA_TAG = "-alpha";
+
+(function findChanges() {
+  shell.ls("packages").forEach(folder => {
+    const path = `./packages/${folder}`;
+
+    try {
+      const { name, version } = JSON.parse(fs.readFileSync(`${path}/package.json`, "utf8"));
+
+      if (version.includes(ALPHA_TAG)) {
+        changedPackages.push(name);
+      }
+    } catch (e) {
+      console.warn(e);
+    }
+  });
+})();
+
+if (changedPackages.length === 0) {
+  console.warn("No changes to publish.");
+  return;
+}
+
+shell.exec(`yarn lerna publish --force-publish=${changedPackages.join(",")}`);
