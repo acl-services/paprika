2921b21554c01874bc3168cf3d5da5e8990ac7f9
Mike Polowick <mike_polowick@acl.com>
2020-11-12 15:56:55 -0800

    docs(Popover): clean up Variations story

--- packages/Popover/stories/examples/Variations.js
+++ packages/Popover/stories/examples/Variations.js
@@ -1,66 +1,120 @@
 import React from "react";
-import { CenteredStory, Gap, Tagline, Rule, Story } from "storybook/assets/styles/common.styles";
+import styled from "styled-components";
+import { Gap } from "storybook/assets/styles/common.styles";
+import StoryHeading from "storybook/components/StoryHeading";
 import Button from "@paprika/button";
 import Popover from "@paprika/popover";
-import ButtonExample from "./Button";
-import ButtonIsEagerExample from "./ButtonIsEager";
-import ShouldKeepFocusExample from "./ShouldKeepFocus";
 
-const Variations = () => (
-  <CenteredStory>
-    <div>
-      <Tagline>Popover Button</Tagline>
-      <Story>
-        <ButtonExample />
-      </Story>
-      <Gap />
-      <Rule />
-      <Tagline>Popover isEager</Tagline>
-      <Story>
-        <ButtonIsEagerExample />
-      </Story>
-      <Gap />
-      <Rule />
-      <Tagline>PopOver isDark</Tagline>
-      <Story>
-        <Popover isEager isDark defaultIsOpen>
-          <Popover.Trigger>
-            <span role="img" aria-label="bulb light">
-              💡
-            </span>
-          </Popover.Trigger>
-          <Popover.Content>
-            <Popover.Card>Lorem ipsum single-origin kombucha.</Popover.Card>
-          </Popover.Content>
-        </Popover>
-      </Story>
-      <Gap />
-      <Rule />
-      <Tagline>Popover shouldKeepFocus</Tagline>
-      <Story>
-        <ShouldKeepFocusExample />
-      </Story>
-      <Gap />
-      <Rule />
-      <Tagline>Popover edge</Tagline>
-      <Story>
-        <Popover defaultIsOpen edge={Popover.types.align.LEFT}>
-          <Popover.Trigger>{handler => <Button onClick={handler}>edge left</Button>}</Popover.Trigger>
-          <Popover.Content>
-            <Popover.Card>Lorem ipsum single-origin kombucha butcher gentrify foraged flannel.</Popover.Card>
-          </Popover.Content>
-        </Popover>
-        <Gap />
-        <Popover defaultIsOpen edge={Popover.types.align.RIGHT}>
-          <Popover.Trigger>{handler => <Button onClick={handler}>edge right</Button>}</Popover.Trigger>
-          <Popover.Content>
-            <Popover.Card>Lorem ipsum single-origin kombucha butcher gentrify foraged flannel.</Popover.Card>
-          </Popover.Content>
-        </Popover>
-      </Story>
-      <Gap />
-    </div>
-  </CenteredStory>
-);
+const Centered = styled.div`
+  text-align: center;
+`;
 
-export default Variations;
+export default function Variations() {
+  return (
+    <>
+      <Gap.Small />
+      <StoryHeading level={1}>Alignment</StoryHeading>
+      <code>align=Popover.types.align.BOTTOM</code> (default)
+      <Centered>
+        <Popover>
+          <Popover.Trigger>{handler => <Button onClick={handler}>Show Popover</Button>}</Popover.Trigger>
+          <Popover.Content>
+            <Popover.Card>Single-origin foraged kombucha.</Popover.Card>
+          </Popover.Content>
+          <Popover.Tip />
+        </Popover>
+      </Centered>
+      <Gap />
+      <code>align=Popover.types.align.RIGHT</code>
+      <Centered>
+        <Popover align={Popover.types.align.RIGHT}>
+          <Popover.Trigger>{handler => <Button onClick={handler}>Show Popover</Button>}</Popover.Trigger>
+          <Popover.Content>
+            <Popover.Card>Single-origin foraged kombucha.</Popover.Card>
+          </Popover.Content>
+          <Popover.Tip />
+        </Popover>
+      </Centered>
+      <Gap />
+      <code>align=Popover.types.align.TOP</code>
+      <Centered>
+        <Popover align={Popover.types.align.TOP}>
+          <Popover.Trigger>{handler => <Button onClick={handler}>Show Popover</Button>}</Popover.Trigger>
+          <Popover.Content>
+            <Popover.Card>Single-origin foraged kombucha.</Popover.Card>
+          </Popover.Content>
+          <Popover.Tip />
+        </Popover>
+      </Centered>
+      <Gap />
+      <code>align=Popover.types.align.LEFT</code>
+      <Centered>
+        <Popover align={Popover.types.align.LEFT}>
+          <Popover.Trigger>{handler => <Button onClick={handler}>Show Popover</Button>}</Popover.Trigger>
+          <Popover.Content>
+            <Popover.Card>Single-origin foraged kombucha.</Popover.Card>
+          </Popover.Content>
+          <Popover.Tip />
+        </Popover>
+      </Centered>
+      <Gap />
+      <code>edge=Popover.types.align.LEFT</code>
+      <Centered>
+        <Popover edge={Popover.types.align.LEFT}>
+          <Popover.Trigger>{handler => <Button onClick={handler}>Show Popover</Button>}</Popover.Trigger>
+          <Popover.Content>
+            <Popover.Card>Single-origin foraged kombucha.</Popover.Card>
+          </Popover.Content>
+          <Popover.Tip />
+        </Popover>
+      </Centered>
+      <Gap />
+      <code>edge=Popover.types.align.RIGHT</code>
+      <Centered>
+        <Popover edge={Popover.types.align.RIGHT}>
+          <Popover.Trigger>{handler => <Button onClick={handler}>Show Popover</Button>}</Popover.Trigger>
+          <Popover.Content>
+            <Popover.Card>Single-origin foraged kombucha.</Popover.Card>
+          </Popover.Content>
+          <Popover.Tip />
+        </Popover>
+      </Centered>
+      <Gap />
+      <StoryHeading level={1}>Width</StoryHeading>
+      <code>maxWidth=120</code>
+      <Centered>
+        <Popover maxWidth={120}>
+          <Popover.Trigger>{handler => <Button onClick={handler}>Show Popover</Button>}</Popover.Trigger>
+          <Popover.Content>
+            <Popover.Card>Single-origin foraged kombucha.</Popover.Card>
+          </Popover.Content>
+          <Popover.Tip />
+        </Popover>
+      </Centered>
+      <Gap />
+      <code>minWidth=320</code>
+      <Centered>
+        <Popover minWidth={320}>
+          <Popover.Trigger>{handler => <Button onClick={handler}>Show Popover</Button>}</Popover.Trigger>
+          <Popover.Content>
+            <Popover.Card>Single-origin foraged kombucha.</Popover.Card>
+          </Popover.Content>
+          <Popover.Tip />
+        </Popover>
+      </Centered>
+      <Gap />
+      <StoryHeading level={1}>Dark</StoryHeading>
+      <code>isDark=true</code>
+      <Centered>
+        <Popover isDark>
+          <Popover.Trigger>{handler => <Button onClick={handler}>Show Popover</Button>}</Popover.Trigger>
+          <Popover.Content>
+            <Popover.Card>Single-origin foraged kombucha.</Popover.Card>
+          </Popover.Content>
+          <Popover.Tip />
+        </Popover>
+      </Centered>
+      <Gap.Large />
+    </>
+  );
+}
