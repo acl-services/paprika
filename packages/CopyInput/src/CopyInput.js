d53ae34df27b2df3dae8ca82242829c61f8c66db
Robyn Choi <robyn.choi@wegalvanize.com>
2020-11-18 12:39:37 -0800

    feat(CopyInput): UXD-531 replaced wasabicon with placeholder paprika icon, generated readme and new types

--- packages/CopyInput/src/CopyInput.js
+++ packages/CopyInput/src/CopyInput.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import CopyIcon from "@acl-services/wasabicons/lib/Clipboard";
 import Input from "@paprika/input";
 import Popover from "@paprika/popover";
 import Button from "@paprika/button";
 import useI18n from "@paprika/l10n/lib/useI18n";
 import extractChildrenProps from "@paprika/helpers/lib/extractChildrenProps";
+import CopyIcon from "@paprika/icon/lib/ArrowDown"; // TODO: Replace with actual icon when ready
 import CopyInputInputPropsCollector from "./CopyInputInputPropsCollector";
 import * as sc from "./CopyInput.styles";
 
--- packages/CopyInput/src/index.d.ts
+++ packages/CopyInput/src/index.d.ts
@@ -3,6 +3,8 @@ export default CopyInput;
 declare function CopyInput(props: CopyInputProps): JSX.Element;
 interface CopyInputProps {
   [x: string]: any;
+  /** Used for CopyInput.Input */
+  children?: React.ReactNode;
   /** Default value for the input. */
   defaultValue?: string;
 }
