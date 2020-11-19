3c3e3d3499af30807c779c65629c40a167aad3c6
Jamie Kupka <jamie_kupka@acl.com>
2020-11-19 12:18:08 -0800

    chore(Panel): UXD-546 Demonstrate how to adjust size of panel from bottom.

--- packages/Panel/stories/examples/Examples.js
+++ packages/Panel/stories/examples/Examples.js
@@ -54,31 +54,42 @@ export const FocusLockPanel = () => {
 };
 
 export const FromBottom = () => {
-  const [isOpen, setIsOpen] = React.useState(true);
-  const toggle = () => {
-    setIsOpen(state => !state);
+  const [isFooterOpen, setIsFooterOpen] = React.useState(true);
+  const [isNavOpen, setIsNavOpen] = React.useState(true);
+  const navWidth = { expanded: 300, collapsed: 30 };
+
+  const toggleFooter = () => {
+    setIsFooterOpen(state => !state);
   };
+  const toggleNav = () => {
+    setIsNavOpen(state => !state);
+  };
+
+  function getNavWidth() {
+    return isNavOpen ? navWidth.expanded : navWidth.collapsed;
+  }
 
   return (
     <React.Fragment>
       <div style={{ display: "flex", height: "100vh" }}>
-        <nav style={{ flex: "0 0 300px", background: "red" }}>Sample Navigation</nav>
-        <div style={{ flex: 1 }}>
-          Body goes here
-          <br />
-          Change this example so the side nav is collapsible - and as its width changes, the width of the panel adjusts
-          <br />
-          <button type="button" onClick={toggle}>
-            {isOpen ? "close" : "open"} the panel
+        <nav style={{ flex: `0 0 ${getNavWidth()}px`, background: "red" }}>
+          <button type="button" onClick={toggleNav}>
+            {isNavOpen ? <span>&laquo;</span> : <span>&raquo;</span>}
+          </button>
+        </nav>
+        <div style={{ flex: 1, padding: "10px" }}>
+          <p>Try toggling the div on the left and notice the Panel&apos;s width adjusts (controlled by consumer).</p>
+          <button type="button" onClick={toggleFooter}>
+            {isFooterOpen ? "close" : "open"} the bottom Panel
           </button>
         </div>
       </div>
 
       <Panel
         height={150}
-        isOpen={isOpen}
-        offset={{ left: 300 }}
-        onClose={toggle}
+        isOpen={isFooterOpen}
+        offset={{ left: getNavWidth() }}
+        onClose={toggleFooter}
         slideFrom={Panel.slideFromDirections.BOTTOM}
       >
         <Panel.Content>
