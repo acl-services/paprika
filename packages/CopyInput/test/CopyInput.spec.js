e0ffb1e340a125b1f1121b0136bd6ad04e536069
Robyn Choi <robyn.choi@wegalvanize.com>
2020-11-19 13:40:30 -0800

    feat(CopyInput): UXD-531 fixed test

--- packages/CopyInput/test/CopyInput.spec.js
+++ packages/CopyInput/test/CopyInput.spec.js
@@ -7,15 +7,15 @@ describe("CopyInput", () => {
     document.execCommand = jest.fn();
 
     const props = { value: "this will be copied" };
-    const { getByTestId, getByText } = render(<CopyInput {...props} />);
+    const { getAllByText, getByTestId, getByText, queryAllByText } = render(<CopyInput {...props} />);
 
     fireEvent.mouseOver(getByTestId("button"));
     await waitFor(() => expect(getByText("Copy to clipboard")).toBeVisible());
 
     fireEvent.click(getByTestId("button"));
     expect(document.execCommand).toHaveBeenCalledWith("copy");
-    await waitFor(() => expect(getByText("Copied")).toBeVisible());
+    await waitFor(() => expect(getAllByText("Copied")).toBeTruthy());
 
-    setTimeout(() => expect(getByText("Copied")).not.toBeVisible(), 5000);
+    setTimeout(() => expect(queryAllByText("Copied")).toBeNull(), 5000);
   });
 });
