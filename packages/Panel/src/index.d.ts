dd014cc717a2f9df5210a970b4e1ca38dacb410f
Jamie Kupka <jamie_kupka@acl.com>
2020-11-19 12:44:35 -0800

    docs(Panel): UXD-546 Updated changed props.

--- packages/Panel/src/index.d.ts
+++ packages/Panel/src/index.d.ts
@@ -11,27 +11,29 @@ interface PanelProps {
   getPushContentElement?: (...args: any[]) => any;
   /** Y offset that is passed down from <Panel.Group> */
   groupOffsetY?: number;
-  /** Control the compactness of the side panel */
+  /** The height of the open Panel (when slide in from bottom) */
+  height?: string | number;
+  /** Control the compactness of the Panel */
   isCompact?: boolean;
   /** Render the panel inline */
   isInline?: boolean;
-  /** Control the visibility of the side panel. This prop makes the side panel appear */
+  /** Control the visibility of the Panel. This prop makes the Panel appear. */
   isOpen: boolean;
   /** Modify the look of the Panel */
   kind?: Panel.types.kind.DEFAULT | Panel.types.kind.CHILD | Panel.types.kind.PRIMARY;
-  /** Control y offset of the panel */
-  offsetY?: number;
-  /** Callback once the panel has been closed event */
+  /** Control offset of the Panel. Only use 'top' when sliding in from the left or right. Only use 'left' or 'right' when sliding in from the bottom. */
+  offset?: shape;
+  /** Callback once the Panel has been closed event */
   onAfterClose?: (...args: any[]) => any;
-  /** Callback once the panel has been opened event */
+  /** Callback once the Panel has been opened event */
   onAfterOpen?: (...args: any[]) => any;
-  /** Callback triggered when the side panel needs to be close */
+  /** Callback triggered when the Panel needs to be close */
   onClose?: (...args: any[]) => any;
-  /** Control which side the panel slides in from */
+  /** Control where the Panel slides in from */
   slideFrom?: Panel.slideFromDirections.RIGHT | Panel.slideFromDirections.LEFT | Panel.slideFromDirections.BOTTOM;
-  /** The width of the open panel. */
+  /** The width of the open Panel (when slide in from left or right) */
   width?: string | number;
-  /** Control the z position of the panel */
+  /** Control the z-index of the Panel */
   zIndex?: number;
 }
 
