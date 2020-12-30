daea6b3cc448856f4ab3cb83690685e9987b121e
Mike Polowick <mike_polowick@acl.com>
2020-12-30 14:26:59 -0800

    refactor(helpers): UXD-572 Use a barrel index.js + update package.json

---
+++ packages/helpers/src/index.js
@@ -0,0 +1,18 @@
+export * from "./customPropTypes";
+export { default as extractChildren } from "./extractChildren";
+export { default as extractChildrenProps } from "./extractChildrenProps";
+export { default as isDevEnv } from "./isDevEnv";
+
+// components
+export { default as LockBodyScroll } from "./components/LockBodyScroll";
+export { default as Portal } from "./components/Portal";
+
+// dom
+export { default as elementScrollToPolyfill } from "./dom/elementScrollToPolyfill";
+export { default as isElementContainsFocus } from "./dom/isElementContainsFocus";
+
+// hooks
+export { default as useDebounce } from "./hooks/useDebounce";
+export { default as useForwardDomRef } from "./hooks/useForwardDomRef";
+export { default as useMountedRef } from "./hooks/useMountedRef";
+export { default as usePrevious } from "./hooks/usePrevious";
