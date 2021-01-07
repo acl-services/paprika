0a74ea1d56acbfd80a3a8b5a88a9f8bc6fd56e6d
Jamie Kupka <jamie_kupka@acl.com>
2021-01-07 12:50:12 -0800

    feat(DynamicHyperlink): Re-run the scan when the DOM changes. Mark links as processed to avoid infinite loop.

--- packages/DynamicHyperlink/src/DynamicHyperlink.js
+++ packages/DynamicHyperlink/src/DynamicHyperlink.js
@@ -4,13 +4,11 @@ import PropTypes from "prop-types";
 import useI18n from "../../L10n/lib/useI18n";
 import "./DynamicHyperlink.scss";
 
-// what if they switch tabs, or change page with react-router?
-//   the DOM gets updated so this needs to run again, right? MutationObserver
-//   https://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
 // TODO: ignore those that are inside a ckEditor?
 // TODO: nahum suggested looking at a webworker
 
 function fetchFromApi(url) {
+  // Fetch from sessionStorage or API
   return new Promise(resolve => {
     switch (url) {
       case "www.google1.ca":
@@ -32,28 +30,41 @@ export default function DynamicHyperlink({ terms }) {
   const I18n = useI18n();
 
   React.useEffect(() => {
-    const dynamicHyperlinks = document.querySelectorAll("[data-dynamic-hyperlink]");
+    function updateDynamicHyperlinks() {
+      const dynamicHyperlinks = Array.from(document.querySelectorAll("[data-dynamic-hyperlink]"));
 
-    dynamicHyperlinks.forEach(dynamicHyperlink => {
-      const originalLinkUrl = dynamicHyperlink.innerHTML;
-      dynamicHyperlink.innerHTML = I18n.t("dynamicHyperlink.loading"); // eslint-disable-line no-param-reassign
+      dynamicHyperlinks
+        .filter(dynamicHyperlink => !dynamicHyperlink.hasAttribute("data-dynamic-hyperlink--processed"))
+        .forEach(dynamicHyperlink => {
+          const originalLinkUrl = dynamicHyperlink.innerHTML;
+          dynamicHyperlink.innerHTML = I18n.t("dynamicHyperlink.loading"); // eslint-disable-line no-param-reassign
+          dynamicHyperlink.setAttribute("data-dynamic-hyperlink--processed", "true");
 
-      // THIS IS JUST A PLACEHOLDER TO SIMULATE API
-      fetchFromApi(originalLinkUrl).then(({ error, id, name, type }) => {
-        const className = error ? "invalid" : "valid";
-        const linkText = error ? originalLinkUrl : `${id}: ${name}`;
-        const errorText = error ? I18n.t(`dynamicHyperlink.${error}`) : "";
+          // THIS IS JUST A PLACEHOLDER TO SIMULATE API
+          fetchFromApi(originalLinkUrl).then(({ error, id, name, type }) => {
+            const className = error ? "invalid" : "valid";
+            const linkText = error ? originalLinkUrl : `${id}: ${name}`;
+            const errorText = error ? I18n.t(`dynamicHyperlink.${error}`) : "";
 
-        dynamicHyperlink.innerHTML = linkText; // eslint-disable-line no-param-reassign
-        const typeOrErrorSpan = document.createElement("span");
-        typeOrErrorSpan.innerHTML = error ? `- ${errorText}` : terms[type];
-        typeOrErrorSpan.className = className;
-        dynamicHyperlink.appendChild(typeOrErrorSpan);
-      });
-    });
+            dynamicHyperlink.innerHTML = linkText; // eslint-disable-line no-param-reassign
+            const typeOrErrorSpan = document.createElement("span");
+            typeOrErrorSpan.innerHTML = error ? `- ${errorText}` : terms[type];
+            typeOrErrorSpan.className = className;
+            dynamicHyperlink.appendChild(typeOrErrorSpan);
+          });
+        });
+    }
+
+    const observer = new MutationObserver(updateDynamicHyperlinks);
+    const config = { childList: true, subtree: true };
+    observer.observe(document.querySelector("body"), config);
+
+    updateDynamicHyperlinks();
+
+    return () => observer.disconnect();
   }, []);
 
   return null;
 }
 
-DynamicHyperlink.propTypes = { terms: PropTypes.object.isRequired };
+DynamicHyperlink.propTypes = { terms: PropTypes.object.isRequired }; // eslint-disable-line react/forbid-prop-types
--- packages/DynamicHyperlink/stories/examples/Showcase.js
+++ packages/DynamicHyperlink/stories/examples/Showcase.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Story, Rule, Tagline } from "storybook/assets/styles/common.styles";
 import Heading from "@paprika/heading";
 import L10n from "@paprika/l10n";
+import Tabs from "@paprika/tabs";
 import DynamicHyperlink from "../../src";
 
 const terms = {
@@ -12,8 +13,6 @@ const terms = {
   walkthrough: "WALKTHROUGH",
   testPlan: "TEST PLAN",
 };
-
-// TODO: use tabs to see if it works on links on a hidden tab
 
 const ExampleStory = () => (
   <Story>
@@ -27,25 +26,44 @@ const ExampleStory = () => (
     <Rule />
     <L10n locale="en">
       <DynamicHyperlink terms={terms} />
-      <p>
-        A functioning dynamic hyperlink{" "}
-        <a href="www.google1.ca" data-dynamic-hyperlink>
-          www.google1.ca
-        </a>{" "}
-        in action. Here is one the viewer does not have access to{" "}
-        <a href="www.google2.ca" data-dynamic-hyperlink>
-          www.google2.ca
-        </a>
-        and here is one where the URL doesnt really exist{" "}
-        <a href="www.google3.ca" data-dynamic-hyperlink>
-          www.google3.ca
-        </a>
-        . This one is still loading{" "}
-        <a href="www.google4.ca" data-dynamic-hyperlink>
-          www.google4.ca
-        </a>{" "}
-        and this last one is a normal boring link <a href="www.google.ca">http://google.ca</a>. The end.
-      </p>
+      <Tabs>
+        <Tabs.List>
+          <Tabs.Tab>Page 1</Tabs.Tab>
+          <Tabs.Tab>Page 2</Tabs.Tab>
+        </Tabs.List>
+        <Tabs.Panels>
+          <Tabs.Panel>
+            <p>
+              A functioning dynamic hyperlink{" "}
+              <a href="www.google1.ca" data-dynamic-hyperlink>
+                www.google1.ca
+              </a>{" "}
+              in action. Here is one the viewer does not have access to{" "}
+              <a href="www.google2.ca" data-dynamic-hyperlink>
+                www.google2.ca
+              </a>
+              and here is one where the URL doesnt really exist{" "}
+              <a href="www.google3.ca" data-dynamic-hyperlink>
+                www.google3.ca
+              </a>
+              . This one is still loading{" "}
+              <a href="www.google4.ca" data-dynamic-hyperlink>
+                www.google4.ca
+              </a>{" "}
+              and this last one is a normal boring link <a href="www.google.ca">http://google.ca</a>. The end.
+            </p>
+          </Tabs.Panel>
+          <Tabs.Panel>
+            <p>
+              This panel is not rendered on load, but once it is activated the links should still be processed. Here is
+              a dynamic hyperlink:{" "}
+              <a href="www.google1.ca" data-dynamic-hyperlink>
+                www.google1.ca
+              </a>
+            </p>
+          </Tabs.Panel>
+        </Tabs.Panels>
+      </Tabs>
     </L10n>
   </Story>
 );
