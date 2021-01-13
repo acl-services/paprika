5f9dac4caf92cf8143d69a7c6f0f3df56236f268
Allison Chen <allison_chen@acl.com>
2021-01-13 13:54:02 -0800

    feat(ActionBar): UXD-684 use throw error

--- packages/ActionBar/src/components/filter.js
+++ packages/ActionBar/src/components/filter.js
@@ -1,13 +1,11 @@
 export const Filter = () => {
-  console.error(
+  throw new Error(
     "The <Filter /> component in @paprika/action-bar has been deprecated. Please use the package @paprika/filter directly."
   );
-
-  return null;
 };
 
 export const useFilter = () => {
-  console.error(
+  throw new Error(
     "The hook useFilter and the <Filter /> component in @paprika/action-bar are deprecated. Please use the package @paprika/filter directly."
   );
 };
