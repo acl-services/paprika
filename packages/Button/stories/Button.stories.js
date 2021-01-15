143b2cd24f67b877c69693f5608aa7caf9150c37
Mike Polowick <mike_polowick@acl.com>
2021-01-14 23:05:07 -0800

    docs(Button): UXD-745 fussing with Button stories

--- packages/Button/stories/Button.stories.js
+++ packages/Button/stories/Button.stories.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { withKnobs } from "@storybook/addon-knobs";
 import { getStoryName } from "storybook/storyTree";
-import { Gap } from "storybook/assets/styles/common.styles";
 import { showcaseStoryParameters, variationsStoryParameters } from "storybook/assets/storyParameters";
 import { ButtonStory } from "./Button.stories.styles";
 import Showcase from "./examples/Showcase";
 import Variations from "./variations/Variations";
-import Button from "../src";
 
 const storyName = getStoryName("Button");
 
 export default {
   title: storyName,
-  component: Button,
 };
 
 export const showcase = () => (
@@ -26,20 +23,8 @@ showcase.story = {
   parameters: showcaseStoryParameters,
 };
 
-const variationsTagline = (
-  <code>
-    &lt;Button&gt;
-    <Gap.Inline />
-    &lt;Button.Link&gt;
-    <Gap.Inline />
-    &lt;Button.Icon&gt;
-    <Gap.Inline />
-    &lt;Button.Close&gt;
-  </code>
-);
-
 export const variations = () => (
-  <ButtonStory storyName="Variations" tagline={variationsTagline}>
+  <ButtonStory storyName="Button" tagline={ButtonStory.defaultTaglines.variations}>
     <Variations />
   </ButtonStory>
 );
