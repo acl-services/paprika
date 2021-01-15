5781529bebc85ef69698f735df9c48dd2dc1683e
Mike Polowick <mike_polowick@acl.com>
2021-01-14 23:10:59 -0800

    docs(ResizeDetector): UXD-745 standardize stories

--- packages/ResizeDetector/stories/ResizeDetector.stories.js
+++ packages/ResizeDetector/stories/ResizeDetector.stories.js
@@ -1,17 +1,21 @@
+import React from "react";
 import { withKnobs } from "@storybook/addon-knobs";
 import { getStoryName } from "storybook/storyTree";
 import { showcaseStoryParameters } from "storybook/assets/storyParameters";
-import ShowcaseStory from "./examples/Showcase";
-import ResizeDetector from "../src";
+import ExampleStory from "storybook/components/ExampleStory";
+import Showcase from "./examples/Showcase";
 
 const storyName = getStoryName("ResizeDetector");
 
 export default {
   title: storyName,
-  component: ResizeDetector,
 };
 
-export const showcase = ShowcaseStory;
+export const showcase = () => (
+  <ExampleStory storyName="ResizeDetector" tagline={ExampleStory.defaultTaglines.showcase}>
+    <Showcase />
+  </ExampleStory>
+);
 showcase.story = {
   decorators: [withKnobs],
   parameters: {
