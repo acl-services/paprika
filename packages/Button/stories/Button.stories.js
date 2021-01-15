cd051bb119d4bb785582edfafc978a29b2d3689b
Mike Polowick <mike_polowick@acl.com>
2021-01-14 17:13:19 -0800

    docs(Button): UXD-745 update Button Showcase/Variations

--- packages/Button/stories/Button.stories.js
+++ packages/Button/stories/Button.stories.js
@@ -1,7 +1,10 @@
+import React from "react";
 import { withKnobs } from "@storybook/addon-knobs";
 import { getStoryName } from "storybook/storyTree";
+import { Gap } from "storybook/assets/styles/common.styles";
 import { showcaseStoryParameters, variationsStoryParameters } from "storybook/assets/storyParameters";
-import ShowcaseStory from "./examples/Showcase";
+import { ButtonStory } from "./Button.stories.styles";
+import Showcase from "./examples/Showcase";
 import Variations from "./variations/Variations";
 import Button from "../src";
 
@@ -12,14 +15,34 @@ export default {
   component: Button,
 };
 
-export const showcase = ShowcaseStory;
+export const showcase = () => (
+  <ButtonStory storyName="Button" tagline={ButtonStory.defaultTaglines.showcase}>
+    <Showcase />
+  </ButtonStory>
+);
 showcase.story = {
   name: "Showcase",
   decorators: [withKnobs],
   parameters: showcaseStoryParameters,
 };
 
-export const variations = Variations;
+const variationsTagline = (
+  <code>
+    &lt;Button&gt;
+    <Gap.Inline />
+    &lt;Button.Link&gt;
+    <Gap.Inline />
+    &lt;Button.Icon&gt;
+    <Gap.Inline />
+    &lt;Button.Close&gt;
+  </code>
+);
+
+export const variations = () => (
+  <ButtonStory storyName="Variations" tagline={variationsTagline}>
+    <Variations />
+  </ButtonStory>
+);
 variations.story = {
   name: "Variations",
   parameters: variationsStoryParameters,
