bf96d8536c28e9fd7c04e81c96bb1da0a1ef1208
Jamie Kupka <jamie_kupka@acl.com>
2021-01-19 11:14:23 -0800

    feat(DynamicHyperlink): PDTVT-753 capitalize term returned from server

--- packages/DynamicHyperlink/stories/examples/Tab3Content.js
+++ packages/DynamicHyperlink/stories/examples/Tab3Content.js
@@ -10,7 +10,7 @@ export default function Tab4Content() {
       config={{
         extraAllowedContent: "a[*]",
         contentsCss:
-          "a[data-dynamic-hyperlink] { background-color: #fff; border-radius: 4px; box-shadow: 0 0 2px #3f3d3c; color: #0063c5; font-size: 14px; padding: 2px 4px; text-decoration: none; }a[data-dynamic-hyperlink]:hover {text-decoration: underline;}a[data-dynamic-hyperlink] span {border-radius: 4px;font-size: 13px;font-weight: bold;margin-left: 4px;}a[data-dynamic-hyperlink] span.valid {background-color: #cce5fd;color: #0063c5;padding: 1px 8px;text-transform: capitalize;}a[data-dynamic-hyperlink] span.invalid {color: #717171;}",
+          "a[data-dynamic-hyperlink] { background-color: #fff; border-radius: 4px; box-shadow: 0 0 2px #3f3d3c; color: #0063c5; font-size: 14px; padding: 2px 4px; text-decoration: none; }a[data-dynamic-hyperlink]:hover {text-decoration: underline;}a[data-dynamic-hyperlink] span {border-radius: 4px;font-size: 13px;font-weight: bold;margin-left: 4px;}a[data-dynamic-hyperlink] span.dynamic-hyperlink--valid {background-color: #cce5fd;color: #0063c5;padding: 1px 8px;text-transform: uppercase;}a[data-dynamic-hyperlink] span.dynamic-hyperlink--invalid {color: #717171;}",
       }}
       data={contents}
     />
