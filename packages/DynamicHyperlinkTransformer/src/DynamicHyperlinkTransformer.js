bc69835282a94deb4e25d1a64c5a8d15948e1ab1
Jamie Kupka <jamie_kupka@acl.com>
2021-01-20 12:58:43 -0800

    refactor(DynamicHyperlinkTransform): PDTVT-753 Improve how get links from the page and from within ckeditor instances

--- packages/DynamicHyperlinkTransformer/src/DynamicHyperlinkTransformer.js
+++ packages/DynamicHyperlinkTransformer/src/DynamicHyperlinkTransformer.js
@@ -9,59 +9,16 @@ function isPromise(obj) {
   return obj && obj.then && typeof obj.then === "function";
 }
 
-function onElementReady(getElement) {
-  return new Promise(resolve => {
-    function waitForElement() {
-      if (getElement()) {
-        resolve(getElement());
-      } else {
-        window.requestAnimationFrame(waitForElement);
-      }
-    }
-    waitForElement();
-  });
-}
-
-async function getDynamicHyperlinksInCkEditor(iframe) {
-  console.log(26, "getting iframe body...");
-  const iframeBody = await onElementReady(() => iframe.contentWindow.document.body);
-  console.log(28, "...done getting the iframe body");
-  const dynamicHyperlinks = [];
-
-  iframeBody.querySelectorAll(dynamicHyperlinkSelector).forEach(dynamicHyperlink => {
-    dynamicHyperlinks.push(dynamicHyperlink);
-  });
-
-  return dynamicHyperlinks;
-}
-
-async function getDynamicHyperlinks() {
-  console.log(40, "getting all the links in all the iframes...");
-  const dynamicHyperlinksInCkEditors = [];
-  const dynamicHyperlinksInPage = Array.from(document.querySelectorAll(dynamicHyperlinkSelector));
-
-  await document.querySelectorAll("iframe.cke_wysiwyg_frame").forEach(async iframe => {
-    console.log(42, "getting all the links in an iframe...");
-    const dynamicHyperlinksInCkEditor = await getDynamicHyperlinksInCkEditor(iframe);
-    console.log(46, "...got all the links in an iframe");
-    dynamicHyperlinksInCkEditors.push(...dynamicHyperlinksInCkEditor);
-  });
-
-  console.log(40, "...got all the links in all the iframes");
-
-  return [...dynamicHyperlinksInCkEditors, ...dynamicHyperlinksInPage];
+function getDynamicHyperlinks(document) {
+  return Array.from(document.querySelectorAll(dynamicHyperlinkSelector));
 }
 
 export default function DynamicHyperlinkTransformer({ onFetch }) {
   const I18n = useI18n();
 
   React.useEffect(() => {
-    async function updateDynamicHyperlinks() {
-      console.log("-------START-------");
-      const dynamicHyperlinks = await getDynamicHyperlinks();
-
-      console.log(61, "...got all the links !!!this should be the very last message!!!");
-      // it is getting here before the onIframeReady() gets called, so there is no link
+    function updateDynamicHyperlinks(document) {
+      const dynamicHyperlinks = getDynamicHyperlinks(document);
 
       dynamicHyperlinks
         .filter(dynamicHyperlink => !dynamicHyperlink.hasAttribute("data-dynamic-hyperlink--processed"))
@@ -88,7 +45,6 @@ export default function DynamicHyperlinkTransformer({ onFetch }) {
                 typeOrErrorSpan.innerHTML = error ? `- ${errorText}` : term;
                 typeOrErrorSpan.className = className;
                 dynamicHyperlink.appendChild(typeOrErrorSpan);
-                console.log(89, typeOrErrorSpan);
               });
           } else {
             throw new Error("In DynamicHyperlinkTransformer component, the onFetch prop must return a Promise");
@@ -96,11 +52,30 @@ export default function DynamicHyperlinkTransformer({ onFetch }) {
         });
     }
 
-    const observer = new MutationObserver(updateDynamicHyperlinks);
+    function getCkEditorInstances() {
+      const ckEditorInstances = window.CKEDITOR && window.CKEDITOR.instances;
+      return ckEditorInstances && Object.keys(ckEditorInstances).length ? ckEditorInstances : null;
+    }
+
+    function updateDynamicHyperlinksWhenReady() {
+      const ckEditorInstances = getCkEditorInstances();
+
+      if (ckEditorInstances) {
+        Object.entries(ckEditorInstances).forEach(([, ckEditorInstance]) => {
+          ckEditorInstance.on("instanceReady", () => {
+            updateDynamicHyperlinks(ckEditorInstance.document.$);
+          });
+        });
+      } else {
+        updateDynamicHyperlinks(document);
+      }
+    }
+
+    const observer = new MutationObserver(updateDynamicHyperlinksWhenReady);
     const config = { childList: true, subtree: true };
     observer.observe(document.querySelector("body"), config);
 
-    updateDynamicHyperlinks();
+    updateDynamicHyperlinksWhenReady();
 
     return () => observer.disconnect();
   }, [I18n, onFetch]);
