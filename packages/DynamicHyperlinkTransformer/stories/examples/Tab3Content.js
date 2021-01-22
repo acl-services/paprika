d0cb7b2161e8507c69ff1970e2850e4f1188562e
Jamie Kupka <jamie_kupka@acl.com>
2021-01-22 15:44:49 -0800

    chore(DynamicHyperlinkTransformer): PDTVT-745 Added minor note

--- packages/DynamicHyperlinkTransformer/stories/examples/Tab3Content.js
+++ packages/DynamicHyperlinkTransformer/stories/examples/Tab3Content.js
@@ -12,12 +12,18 @@ export default function Tab4Content() {
     "a[data-dynamic-hyperlink] { background-color: #fff; border-radius: 4px; box-shadow: 0 0 2px #3f3d3c; color: #0063c5; font-size: 14px; padding: 2px 2px 2px 4px; text-decoration: none; }a[data-dynamic-hyperlink]:hover {text-decoration: underline;}a[data-dynamic-hyperlink] span {border-radius: 4px;font-size: 13px;font-weight: bold;margin-left: 4px;}a[data-dynamic-hyperlink] span.dynamic-hyperlink--valid {background-color: #cce5fd;color: #0063c5;padding: 1px 8px 0;text-transform: uppercase;}a[data-dynamic-hyperlink] span.dynamic-hyperlink--invalid {color: #717171;}";
 
   return (
-    <CKEditor
-      config={{
-        extraAllowedContent: "a[*]",
-        contentsCss: `${ckeditorStyles} ${dynamicHyperlinkStyles}`,
-      }}
-      data={contents}
-    />
+    <>
+      <p>
+        The transformer will find and replace links within a CKEditor iframe, but the custom style must be passed into
+        the CKEditor config.
+      </p>
+      <CKEditor
+        config={{
+          extraAllowedContent: "a[*]",
+          contentsCss: `${ckeditorStyles} ${dynamicHyperlinkStyles}`,
+        }}
+        data={contents}
+      />
+    </>
   );
 }
