4aefb4e270e2531bcdf46a920b98364940b45477
Jamie Kupka <jamie_kupka@acl.com>
2021-01-22 15:52:23 -0800

    chore(DynamicHyperlinkTransformer): PDTVT-745 Minor tweaks to style

--- packages/DynamicHyperlinkTransformer/stories/examples/Tab3Content.js
+++ packages/DynamicHyperlinkTransformer/stories/examples/Tab3Content.js
@@ -9,7 +9,7 @@ export default function Tab4Content() {
     '@import url("https://fonts.googleapis.com/css2?family=Lato&display=swap"); body {font-family: "Lato"; font-size: 16px; color: #3f3d3c;} a { cursor: pointer; } ';
 
   const dynamicHyperlinkStyles =
-    "a[data-dynamic-hyperlink] { background-color: #fff; border-radius: 4px; box-shadow: 0 0 2px #3f3d3c; color: #0063c5; font-size: 14px; padding: 2px 2px 2px 4px; text-decoration: none; }a[data-dynamic-hyperlink]:hover {text-decoration: underline;}a[data-dynamic-hyperlink] span {border-radius: 4px;font-size: 13px;font-weight: bold;margin-left: 4px;}a[data-dynamic-hyperlink] span.dynamic-hyperlink--valid {background-color: #cce5fd;color: #0063c5;padding: 1px 8px 0;text-transform: uppercase;}a[data-dynamic-hyperlink] span.dynamic-hyperlink--invalid {color: #717171;}";
+    "a[data-dynamic-hyperlink] { background-color: white; border-radius: 4px; box-shadow: 0 0 2px #3f3d3c; color: #0063c5; font-size: 14px; padding: 2px 2px 2px 4px; text-decoration: none; } a[data-dynamic-hyperlink]:hover { text-decoration: underline; } a[data-dynamic-hyperlink] span { border-radius: 4px; font-size: 13px; font-weight: bold; margin-left: 4px; } a[data-dynamic-hyperlink] span.dynamic-hyperlink--valid { background-color: #cce5fd; color: #0063c5; padding: 1px 8px 0; text-transform: uppercase; } a[data-dynamic-hyperlink] span.dynamic-hyperlink--invalid { color: #717171; padding-right: 4px;}";
 
   return (
     <>
