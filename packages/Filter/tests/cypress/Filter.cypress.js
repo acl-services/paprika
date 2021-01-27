5230901e5f6d3a0a6ecac353f0549934a35c50f7
Allison Chen <allison_chen@acl.com>
2021-01-27 10:51:55 -0800

    test(Filter): UXD-707 update tests for cypress upgrade

--- packages/Filter/tests/cypress/Filter.cypress.js
+++ packages/Filter/tests/cypress/Filter.cypress.js
@@ -5,7 +5,7 @@ describe("Filter", () => {
   it("Should display the different filtering options", () => {
     cy.clock();
     cy.visitStorybook(`${getStoryUrlPrefix("Filter")}--showcase`);
-    cy.getByText("1 filtered").click();
+    cy.findByText("1 filtered").click();
 
     clickListBoxTrigger("filter.item.columnSelector");
     cy.findByText("Goals").click();
@@ -33,9 +33,9 @@ describe("Filter", () => {
 
   it("should add filter and delete filter", () => {
     cy.visitStorybook(`${getStoryUrlPrefix("Filter")}--showcase`);
-    cy.getByText("1 filtered").click();
-    cy.getByText("Add filter").click();
-    cy.getByTestId("filter.item").should("have.length", 2);
+    cy.findByText("1 filtered").click();
+    cy.findByText("Add filter").click();
+    cy.findAllByTestId("filter.item").should("have.length", 2);
 
     cy.findAllByTestId("filter.deleteFilterButton")
       .eq(0)
@@ -45,22 +45,22 @@ describe("Filter", () => {
 
   it("Should switch between and or", () => {
     cy.visitStorybook(`${getStoryUrlPrefix("Filter")}--showcase`);
-    cy.getByText("1 filtered").click();
-    cy.getByText("Add filter").click();
-    cy.getAllByRole("radio")
+    cy.findByText("1 filtered").click();
+    cy.findByText("Add filter").click();
+    cy.findAllByRole("radio")
       .eq(0)
       .should("be.checked");
     cy.findAllByTestId("filter.item")
       .eq(1)
       .within(() => {
-        cy.getByTestId("filter.item.valueInput").type("1");
+        cy.findByTestId("filter.item.valueInput").type("1");
       });
     cy.findByText("Apply").click();
 
-    cy.getAllByRole("row").should("have.length", 1);
-    cy.getByText("2 filtered").click();
-    cy.getByText("Or").click();
-    cy.getAllByRole("radio")
+    cy.findAllByRole("row").should("have.length", 1);
+    cy.findByText("2 filtered").click();
+    cy.findByText("Or").click();
+    cy.findAllByRole("radio")
       .eq(1)
       .should("be.checked");
     cy.findByText("Apply").click();
@@ -68,12 +68,12 @@ describe("Filter", () => {
   });
 
   it("Should cache changes", () => {
-    cy.getByText("2 filtered").click();
-    cy.getByText("Add filter").click();
-    cy.getByText("Cancel").click();
+    cy.findByText("2 filtered").click();
+    cy.findByText("Add filter").click();
+    cy.findByText("Cancel").click();
 
-    cy.getByText("2 filtered").click();
-    cy.getByTestId("filter.item").should("have.length", 3);
+    cy.findByText("2 filtered").click();
+    cy.findAllByTestId("filter.item").should("have.length", 3);
   });
 
   it("Should clear", () => {
--- packages/Filter/tests/cypress/FilterWithServer.cypress.js
+++ packages/Filter/tests/cypress/FilterWithServer.cypress.js
@@ -5,8 +5,8 @@ describe("Filter with server", () => {
   it("Should display the different filtering options", () => {
     cy.clock();
     cy.visitStorybook(`${getStoryUrlPrefix("Filter")}--server-side-filter-example`);
-    cy.getByText("Filter").click();
-    cy.getByText("Add filter").click();
+    cy.findByText("Filter").click();
+    cy.findByText("Add filter").click();
 
     clickListBoxTrigger("filter.item.columnSelector");
     cy.findByText("Name").click();
@@ -50,26 +50,26 @@ describe("Filter with server", () => {
     cy.findAllByTestId("filter.item")
       .eq(1)
       .within(() => {
-        cy.getByTestId("filter.item.valueInput").type("1");
+        cy.findByTestId("filter.item.valueInput").type("1");
       });
     cy.findByText("Apply").click();
 
-    cy.getAllByRole("row").should("have.length", 2);
-    cy.getByText("2 filtered").click();
-    cy.getByText("Or").click();
-    cy.getAllByRole("radio")
+    cy.findAllByRole("row").should("have.length", 2);
+    cy.findByText("2 filtered").click();
+    cy.findByText("Or").click();
+    cy.findAllByRole("radio")
       .eq(1)
       .should("be.checked");
     cy.findByText("Apply").click();
   });
 
   it("Should cache changes", () => {
-    cy.getByText("2 filtered").click();
-    cy.getByText("Add filter").click();
-    cy.getByText("Cancel").click();
+    cy.findByText("2 filtered").click();
+    cy.findByText("Add filter").click();
+    cy.findByText("Cancel").click();
 
-    cy.getByText("2 filtered").click();
-    cy.getByTestId("filter.item").should("have.length", 3);
+    cy.findByText("2 filtered").click();
+    cy.findAllByTestId("filter.item").should("have.length", 3);
   });
 
   it("Should clear", () => {
