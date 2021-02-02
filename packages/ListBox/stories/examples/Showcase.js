b9619a8469d085a4070e19674e3e8c9e25ae69b0
Oscar <oscar_kwan@acl.com>
2021-02-01 20:52:15 -0800

    UXD-803 docs(ListBox): remove non relevant stories

--- packages/ListBox/stories/examples/Showcase.js
+++ packages/ListBox/stories/examples/Showcase.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { action } from "@storybook/addon-actions";
 import { boolean, text } from "@storybook/addon-knobs";
 import { Rule } from "storybook/assets/styles/common.styles";
 import * as characters from "../fixtures/characters";
@@ -15,20 +16,41 @@ const getKnobs = () => ({
 export default function Showcase() {
   const { showFilter, showFooter, ...listBoxProps } = getKnobs();
 
+  const ListBoxChildren = (
+    <>
+      {showFilter && <ListBox.Filter />}
+      {characters.villians}
+    </>
+  );
+
   return (
     <>
       <h3>Single</h3>
       <ListBox {...listBoxProps}>
-        {showFilter && <ListBox.Filter />}
-        {characters.villians}
-        {showFooter && <ListBox.Footer />}
+        {ListBoxChildren}
+        {showFooter && (
+          <ListBox.Footer
+            onClickAccept={(index, options) => {
+              if (index) {
+                action(`Option ${options[index].label} was clicked`)(options[index]);
+              }
+            }}
+          />
+        )}
       </ListBox>
       <Rule />
       <h3>Multi</h3>
       <ListBox {...listBoxProps} isMulti>
-        {showFilter && <ListBox.Filter />}
-        {characters.villians}
-        {showFooter && <ListBox.Footer />}
+        {ListBoxChildren}
+        {showFooter && (
+          <ListBox.Footer
+            onClickAccept={(indexes, options) => {
+              for (const i of indexes) {
+                action(`Option was selected`)(options[i]);
+              }
+            }}
+          />
+        )}
       </ListBox>
     </>
   );
