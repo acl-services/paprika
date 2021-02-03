f890da55ea0ae71513b9c6cee000f2f769b7d694
Oscar <oscar_kwan@acl.com>
2021-02-02 16:44:58 -0800

    UXD-803 chore(ListBox): fix up showcase story

--- packages/ListBox/stories/examples/Showcase.js
+++ packages/ListBox/stories/examples/Showcase.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
-import { boolean, text } from "@storybook/addon-knobs";
-import { Rule } from "storybook/assets/styles/common.styles";
+import { boolean, number, text } from "@storybook/addon-knobs";
+import CodeViewer from "storybook/components/CodeViewer";
+import StoryHeading from "storybook/components/StoryHeading";
 import * as characters from "../fixtures/characters";
 import ListBox from "../../src";
 
 const getKnobs = () => ({
-  hasImplicitAll: boolean("hasImplicitAll", false),
+  height: number("height", 200),
   isDisabled: boolean("isDisabled", false),
   placeholder: text("placeholder", "Select..."),
   showFilter: boolean("Show Filter", false),
@@ -16,42 +17,40 @@ const getKnobs = () => ({
 export default function Showcase() {
   const { showFilter, showFooter, ...listBoxProps } = getKnobs();
 
-  const ListBoxChildren = (
-    <>
-      {showFilter && <ListBox.Filter />}
-      {characters.villians}
-    </>
-  );
-
   return (
     <>
-      <h3>Single</h3>
-      <ListBox {...listBoxProps}>
-        {ListBoxChildren}
-        {showFooter && (
-          <ListBox.Footer
-            onClickAccept={(index, options) => {
-              if (index) {
-                action(`Option ${options[index].label} was clicked`)(options[index]);
-              }
-            }}
-          />
-        )}
-      </ListBox>
-      <Rule />
-      <h3>Multi</h3>
-      <ListBox {...listBoxProps} isMulti>
-        {ListBoxChildren}
-        {showFooter && (
-          <ListBox.Footer
-            onClickAccept={(indexes, options) => {
-              for (const i of indexes) {
-                action(`Option was selected`)(options[i]);
-              }
-            }}
-          />
-        )}
-      </ListBox>
+      <StoryHeading level={2}>Single-select – default</StoryHeading>
+      <CodeViewer>
+        <ListBox {...listBoxProps}>
+          {showFilter && <ListBox.Filter />}
+          {characters.villians}
+          {showFooter && (
+            <ListBox.Footer
+              onClickAccept={(index, options) => {
+                if (index) {
+                  action(`Option ${options[index].label} was clicked`)(options[index]);
+                }
+              }}
+            />
+          )}
+        </ListBox>
+      </CodeViewer>
+      <StoryHeading level={2}>Multi-select – isMulti</StoryHeading>
+      <CodeViewer>
+        <ListBox {...listBoxProps} isMulti>
+          {showFilter && <ListBox.Filter />}
+          {characters.villians}
+          {showFooter && (
+            <ListBox.Footer
+              onClickAccept={(indexes, options) => {
+                for (const i of indexes) {
+                  action(`Option was selected`)(options[i]);
+                }
+              }}
+            />
+          )}
+        </ListBox>
+      </CodeViewer>
     </>
   );
 }
