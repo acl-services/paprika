fc26fa98e3e35ad63cf2bbfbeaefa3462bb32800
nahumzs <nahum_zsilva@acl.com>
2021-02-08 11:47:41 -0800

    feature(seducer): UXD-630 remoing useless var

--- .storybook/main.js
+++ .storybook/main.js
@@ -26,7 +26,7 @@ module.exports = {
     "@storybook/addon-a11y",
     "@storybook/addon-cssresources",
   ],
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async config => {
     config.module.rules = [
       ...config.module.rules,
       {
