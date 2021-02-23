063843dc4fd7c0d718942f3d514092e2dbc0aea0
Jamie Kupka <jamie_kupka@acl.com>
2021-02-22 16:52:27 -0800

    feat(CollapsibleCard): UXD-717 Make sub components show up in docs

--- packages/CollapsibleCard/src/index.d.ts
+++ packages/CollapsibleCard/src/index.d.ts
@@ -8,13 +8,57 @@ interface CollapsibleCardProps {
 
   initialIsCollapsed?: boolean;
 
+  isCollapsed?: boolean;
+
   isEditing?: boolean;
 
-  isFirstRow?: boolean;
-
-  isMiddleRow?: boolean;
-
-  isLastRow?: boolean;
-
   onToggleIsCollapsed?: (...args: any[]) => any;
+
+  position?: string;
+}
+
+declare namespace CollapsibleCard {
+  function Body(props: BodyProps): JSX.Element;
+  interface BodyProps {
+    [x: string]: any;
+
+    children?: React.ReactNode;
+  }
+}
+declare function Group(props: GroupProps): JSX.Element;
+interface GroupProps {
+  [x: string]: any;
+
+  children?: React.ReactNode;
+}
+
+declare namespace CollapsibleCard {
+  function Header(props: HeaderProps): JSX.Element;
+  interface HeaderProps {
+    [x: string]: any;
+
+    breakpoint?: number;
+
+    children?: React.ReactNode;
+
+    onChangeIsBroken?: (...args: any[]) => any;
+  }
+}
+declare namespace CollapsibleCard {
+  function HeaderLayout(props: HeaderLayoutProps): JSX.Element;
+  interface HeaderLayoutProps {
+    [x: string]: any;
+
+    children?: React.ReactNode;
+  }
+}
+declare namespace CollapsibleCard {
+  function Segment(props: SegmentProps): JSX.Element;
+  interface SegmentProps {
+    [x: string]: any;
+
+    children?: React.ReactNode;
+
+    width?: number;
+  }
 }
