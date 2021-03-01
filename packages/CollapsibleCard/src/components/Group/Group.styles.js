3bb2d345af62e6d50fd53f80ce663e7e3ab703b0
Jamie Kupka <jamie_kupka@acl.com>
2021-03-01 09:37:11 -0800

    fix(CollapsibleCard): UXD-851 Minor border radius fix

--- packages/CollapsibleCard/src/components/Group/Group.styles.js
+++ packages/CollapsibleCard/src/components/Group/Group.styles.js
@@ -1,11 +1,12 @@
 import styled, { css } from "styled-components";
 import tokens from "@paprika/tokens";
+import getBorderRadius from "../../helper.styles";
 
 export const Group = styled.div(
   () => css`
     background-color: ${tokens.color.white};
     border: 1px solid ${tokens.color.blackLighten60};
-    border-radius: ${tokens.card.borderRadius};
+    border-radius: ${getBorderRadius(null, false, false)};
     box-shadow: ${tokens.card.shadow};
     overflow: hidden;
   `
