8dca56c9f7d4e55b146972f7369e78760b32eb27
nahum zsilva <nahumzs@users.noreply.github.com>
2021-04-06 13:25:09 -0700

    Update packages/TextEditor/stories/examples/controlled.js
    Co-authored-by: Jamie K <23224777+jamiek-acl@users.noreply.github.com>

--- packages/TextEditor/stories/examples/controlled.js
+++ packages/TextEditor/stories/examples/controlled.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TextEditor from "../../src";
 
-export default function TextEditorSimple() {
+export default function TextEditorControlled() {
   const API_KEY = "t9jqh1aieshznn654ruvg53914pbzyqcni0bhzbcl1iznj8v";
   const [value, setValue] = React.useState("Type something...");
 
