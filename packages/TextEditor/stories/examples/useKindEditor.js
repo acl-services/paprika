d9b17b64bc38134ab14f6d9304a67f166b7366e0
nahumzs <nahum_zsilva@acl.com>
2021-04-07 15:23:52 -0700

    feature(TextEditor): UXD-1031 support for advance editor

--- packages/TextEditor/stories/examples/useKindEditor.js
+++ packages/TextEditor/stories/examples/useKindEditor.js
@@ -1,14 +1,15 @@
 import React from "react";
-import SwitchPaprika from "../../../Switch/src";
 
 export function Switch({ isChecked, onChange }) {
   return (
     <>
       <div style={{ display: "flex", alignItems: "center" }}>
-        <SwitchPaprika size="small" onChange={onChange} isChecked={isChecked} />
-        <span style={{ display: "inline-block", paddingLeft: "4px" }}>
-          Editor kind: {isChecked ? "advanced" : "simple"}
-        </span>
+        <label htmlFor="checkbox">
+          <input id="checkbox" type="checkbox" onChange={onChange} isChecked={isChecked} />
+          <span style={{ display: "inline-block", paddingLeft: "4px" }}>
+            Editor kind: {isChecked ? "advanced" : "simple"}
+          </span>
+        </label>
       </div>
       <hr />
     </>
