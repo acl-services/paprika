3874d8c76026532fa6f7b3062dc60a9d0cef0843
Mike Polowick <mike_polowick@acl.com>
2021-04-21 14:27:48 -0700

    feat(ProgressBar): RAPRM-1705 Add isCompact prop / styling

--- packages/ProgressBar/src/ProgressBar.js
+++ packages/ProgressBar/src/ProgressBar.js
@@ -7,39 +7,46 @@ import * as sc from "./ProgressBar.styles";
 const propTypes = {
   /** Descriptive a11y text for assistive technologies. */
   a11yText: PropTypes.string,
+
   /** Description for the ProgressBar */
   bodyText: PropTypes.string,
-  /** Header text for the ProgressBar */
-  header: PropTypes.string,
+
   /** Specifies how much progress has been completed from 0-100 */
   completed: PropTypes.number,
+
+  /** Header text for the ProgressBar */
+  header: PropTypes.string,
+
+  /** If ProgressBar is displayed in a compact style */
+  isCompact: PropTypes.bool,
 };
 
 const defaultProps = {
   a11yText: null,
   bodyText: null,
-  header: null,
   completed: 0,
+  header: null,
+  isCompact: false,
 };
 
 function ProgressBar(props) {
-  const { a11yText, header, bodyText, completed, ...moreProps } = props;
-  const bestAria = a11yText || "Loading";
+  const { a11yText, bodyText, completed, header, isCompact, ...moreProps } = props;
+
   return (
     <sc.ProgressBar {...moreProps}>
-      <Heading level={3}>{header}</Heading>
-      <sc.Bar>
+      {header ? <Heading level={3}>{header}</Heading> : null}
+      <sc.Bar isCompact={isCompact}>
         <sc.BarFiller
-          data-pka-anchor="progress-bar"
-          role="progressbar"
           aria-valuemin="0"
           aria-valuemax="100"
           aria-valuenow={completed}
           completed={completed}
+          data-pka-anchor="progress-bar"
+          role="progressbar"
         />
       </sc.Bar>
-      <sc.Body>{bodyText}</sc.Body>
-      <sc.BarAria aria-live="polite">{bestAria}</sc.BarAria>
+      {bodyText ? <sc.Body>{bodyText}</sc.Body> : null}
+      {a11yText ? <sc.A11yText aria-live="polite">{a11yText}</sc.A11yText> : null}
     </sc.ProgressBar>
   );
 }
--- packages/ProgressBar/src/ProgressBar.styles.js
+++ packages/ProgressBar/src/ProgressBar.styles.js
@@ -12,6 +12,17 @@ const progress = keyframes`
   100% { background-position: left bottom; }
 `;
 
+const completeStyles = css`
+  animation: none;
+  background-image: linear-gradient(
+    270deg,
+    ${tokens.color.green} 30%,
+    ${tokens.color.greenLighten20} 80%,
+    ${tokens.color.green} 100%
+  );
+  transition: none;
+`;
+
 export const BarFiller = styled.div(
   ({ completed }) => css`
     animation: ${progress} ease 1.5s infinite;
@@ -22,25 +33,38 @@ export const BarFiller = styled.div(
       ${tokens.color.purple} 100%
     );
     background-size: 200% 200%;
-    transition: width 0.2s ease;
-    width: ${completed}%;
+    transform: scaleX(${completed / 100});
+    transform-origin: center left;
+    transition: transform 0.2s ease-in-out;
+    width: 100%;
+
+    ${completed === 100 ? completeStyles : ""}
   `
 );
 
-export const Bar = styled.div`
-  background-color: ${tokens.color.blackLighten80};
-  border: 1px solid ${tokens.border.color};
-  border-radius: ${tokens.border.radius};
-  display: flex;
-  height: ${stylers.spacer(2)};
-  overflow: hidden;
-  width: 100%;
+const compactStyles = css`
+  border: none;
+  border-radius: 0;
+  height: ${tokens.space};
 `;
+
+export const Bar = styled.div(
+  ({ isCompact }) => css`
+    background-color: ${tokens.color.blackLighten80};
+    border: 1px solid ${tokens.border.color};
+    border-radius: ${tokens.border.radius};
+    display: flex;
+    height: ${stylers.spacer(2)};
+    overflow: hidden;
+
+    ${isCompact ? compactStyles : ""}
+  `
+);
 
 export const Body = styled.p`
   ${stylers.fontSize()};
 `;
 
-export const BarAria = styled.div`
+export const A11yText = styled.div`
   ${stylers.visuallyHidden}
 `;
