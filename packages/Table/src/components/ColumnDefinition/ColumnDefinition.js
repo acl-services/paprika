37c610a23fb918ea703ecef5b6d9553e8c000b51
nahumzs <nahum_zsilva@acl.com>
2021-05-11 16:22:37 -0700

    feature(Table): UXD-1141 generate readme and d.ts

--- packages/Table/src/components/ColumnDefinition/ColumnDefinition.js
+++ packages/Table/src/components/ColumnDefinition/ColumnDefinition.js
@@ -5,9 +5,9 @@ import PropTypes from "prop-types";
 const propTypes = {
   /** Each time a cell is renderer this prop will be call either to read a string value or to execute a cell function */
   cell: PropTypes.oneOfType([PropTypes.string, PropTypes.func]).isRequired,
-  /** Represent the header for the column can either be a string or a function */
+  /** Give you access to return an object (styles, className, data-qa, etc) to render on top of each <td /> element */
   cellProps: PropTypes.func,
-  cellPropsResetCSS: PropTypes.bool,
+  /** Represent the header for the column can either be a string or a function */
   header: PropTypes.oneOfType([PropTypes.string, PropTypes.func]).isRequired,
   /** Determine if a column should behave as a sticky column or not, received a number representing the space between the left side and the column pixels
    *  internally the default value is zero
@@ -18,7 +18,6 @@ const propTypes = {
 const defaultProps = {
   sticky: undefined,
   cellProps: undefined,
-  cellPropsResetCSS: undefined,
 };
 
 export default function ColumnDefinition() {
