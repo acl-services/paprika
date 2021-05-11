b473e345903e30798f423f2bd6ba87ddf1a7c980
nahumzs <nahum_zsilva@acl.com>
2021-05-11 16:11:53 -0700

    feature(Table): UXD-1141 adding support for cssProps prop in Columndefinitions

--- packages/Table/src/components/ColumnDefinition/ColumnDefinition.js
+++ packages/Table/src/components/ColumnDefinition/ColumnDefinition.js
@@ -6,6 +6,8 @@ const propTypes = {
   /** Each time a cell is renderer this prop will be call either to read a string value or to execute a cell function */
   cell: PropTypes.oneOfType([PropTypes.string, PropTypes.func]).isRequired,
   /** Represent the header for the column can either be a string or a function */
+  cellProps: PropTypes.func,
+  cellPropsResetCSS: PropTypes.bool,
   header: PropTypes.oneOfType([PropTypes.string, PropTypes.func]).isRequired,
   /** Determine if a column should behave as a sticky column or not, received a number representing the space between the left side and the column pixels
    *  internally the default value is zero
@@ -15,6 +17,8 @@ const propTypes = {
 
 const defaultProps = {
   sticky: undefined,
+  cellProps: undefined,
+  cellPropsResetCSS: undefined,
 };
 
 export default function ColumnDefinition() {
