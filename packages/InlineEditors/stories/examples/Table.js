678eadd566a5dfc94fea3e2c3e4e9394d4fd8997
nahumzs <nahum_zsilva@acl.com>
2021-05-12 15:41:26 -0700

    feature(Table): UXD-1130 improving inline editor example

--- packages/InlineEditors/stories/examples/Table.js
+++ packages/InlineEditors/stories/examples/Table.js
@@ -7,6 +7,18 @@ const users = getUsers(20);
 const cellTypes = ["an", "name", "status", "role", "subscription", "reportsRole", "activations", "signed"];
 
 const cellRenders = {
+  name: () => ({ row }) => {
+    return (
+      <div>
+        <div>{row.name}</div>
+        <div>
+          <a href="https://wegalvanize.com" rel="noreferrer" target="_blank">
+            Some link
+          </a>
+        </div>
+      </div>
+    );
+  },
   an: () => ({ row }) => {
     return <input type="checkbox" defaultChecked={row.an} />;
   },
@@ -49,10 +61,51 @@ const cellRenders = {
       </ListBox>
     );
   },
+  role: ({ setData }) => props => {
+    const { setStatus, setOptimisticValue, statusType } = useTable({
+      rowIndex: props.rowIndex,
+      columnIndex: props.columnIndex,
+    });
+    const roleTypes = ["Admin", "User", "Super Admin", "Guest", "None"];
+
+    return (
+      <ListBox
+        {...props}
+        value={props.row.role}
+        onChange={value => {
+          console.log("onChange", value);
+        }}
+        onSubmit={(index, options, _, { rowIndex }) => {
+          const nextValue = options[index].value;
+          if (nextValue !== props.row.role) {
+            setOptimisticValue(nextValue);
+            setStatus(statusType.LOADING);
+
+            setTimeout(() => {
+              setData(prevData => {
+                const nextData = prevData.slice(0);
+                nextData[rowIndex].role = nextValue;
+                setStatus(statusType.SUCCEED);
+                return nextData;
+              });
+            }, 1200);
+          }
+        }}
+      >
+        {roleTypes.map(role => (
+          <ListBox.Option value={role} isSelected={role === props.row.role} key={role}>
+            {role}
+          </ListBox.Option>
+        ))}
+      </ListBox>
+    );
+  },
 };
 
 const columnDefinitionProps = {
   subscription: { width: 140 },
+  name: { width: 170 },
+  role: { width: 110 },
 };
 
 export default function TableStory() {
