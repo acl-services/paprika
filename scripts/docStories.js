2e03fb11abbfad556b191fc0cd86a07e6a02e13c
trtri2 <leontrieu88@hotmail.com>
2021-06-11 13:59:45 -0700

    feat(scripts): UXD-1239 add script to generate MDX stories from readmes.

---
+++ scripts/docStories.js
@@ -0,0 +1,55 @@
+
+/**
+ * This script runs and generates the corresponding documentation {Component}.stories.mdx files for each package if it doesn't already exist.
+ */
+
+const fs = require("fs");
+const shell = require("shelljs");
+
+const skipPackages = [
+  "BuildTranslations",
+  "Calendar",
+  "Constants",
+  "helpers",
+  "Stylers",
+  "Overlay",
+];
+
+const renderMDXFileTemplate = ({
+  displayName = "",
+}) => {
+  return `
+import { Meta, Description } from "@storybook/addon-docs/blocks";
+import { getStoryName } from "storybook/storyTree";
+import { docsStoryParameters } from "storybook/assets/storyParameters";
+import Readme from "../README.md";
+import ${displayName} from "../src";
+
+<Meta title={\`\${getStoryName("${displayName}")}/ Docs\`} component={${displayName}} parameters={{ ...docsStoryParameters }} />
+
+<Description markdown={Readme} />
+`;
+};
+
+shell.ls("packages").forEach(folder => {
+  if (!skipPackages.includes(folder)) {
+    const path = `./packages/${folder}`;
+    const destinationFileName = `${path}/stories/${folder}.stories.mdx`;
+
+    try {
+      if (!fs.existsSync(destinationFileName)) {
+        const template = renderMDXFileTemplate({
+          displayName: folder
+        });
+
+        fs.writeFileSync(`${destinationFileName}`, template, {
+          encoding: "utf8",
+          flag: "w",
+        })
+      }
+    } catch(err) {
+      console.warn(folder, err);
+    }
+  }
+});
+
