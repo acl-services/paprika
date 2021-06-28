d853f1941e0a3defe6d06baf46f4bcda5dffe3ee
nahum zsilva <nahum_zsilva@acl.com>
2021-06-28 12:40:52 -0700

    fix(ListBox): UXD-1280 removing Boolean

--- packages/ListBox/src/components/Popover/Popover.js
+++ packages/ListBox/src/components/Popover/Popover.js
@@ -21,13 +21,7 @@ export default function Popover(props) {
   const { children, zIndex } = props;
 
   return (
-    <sc.PopoverWrapper
-      {...props}
-      isOpen={Boolean(state.isOpen)}
-      maxWidth={state.triggerWidth}
-      offset={0}
-      zIndex={zIndex}
-    >
+    <sc.PopoverWrapper {...props} isOpen={state.isOpen} maxWidth={state.triggerWidth} offset={0} zIndex={zIndex}>
       {children}
     </sc.PopoverWrapper>
   );
