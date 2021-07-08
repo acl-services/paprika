e71fe0344a38629cd5c8b76e053a5a4a74316409
Joshua Luong <josh.luong@wegalvanize.com>
2021-07-08 12:36:25 -0700

    feat(Paprika): [UXD-1260] fixing script

--- scripts/paprika-status.js
+++ scripts/paprika-status.js
@@ -1,65 +1,76 @@
-#!/usr/bin/env node --max-old-space-size=8192
-/* eslint-disable import/no-extraneous-dependencies */
+#!/usr/bin/env node
+// --max-old-space-size=8192
 const fs = require("fs");
-const exec = require("child_process").execSync;
+const child_process = require('child_process');
 const tree = require("../.storybook/storyTree")
 
 class PaprikaStatus {
-  constructor() {
+  constructor(statusOutputPath) {
     this.PATH_TO_COMPONENTS = "./packages/";
-    this.VALID_TYPES = ["feat", "fix", "style", "test", "docs", "build", "chore", "ci", "perf", "refactor", "revert"];
+    this.LOCAL_HOST = "http://localhost:9009/";
 
     this.gitOutput = "output_git.xml";
     this.lineCount = "line_count.json";
     this.bugCountOutput = "bug_count.json";
     this.choreCount = "chore_count.json";
     this.featCount = "feat_count.json";
-    this.a11yReport = "a11y_report.txt";
-    this.statuses = {};
+    this.a11yReport = "a11y_report.md";
+    this.countCommits = "count_commits.json";
+    this.statuses = JSON.parse(fs.existsSync(statusOutputPath) && fs.readFileSync(statusOutputPath, "utf8")) || {};
   }
 
+  getStorybookA11yReport(subDir) {
+    try {
+      child_process.spawnSync(`yarn`, [`storybook-a11y-report`, `--storybookUrl ${this.LOCAL_HOST}`, `--include '${tree.getStoryName(subDir)}/**'`, `--outDir ${this.PATH_TO_COMPONENTS + subDir}`], {shell: true, timeout: 60000});
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-  getCommits() {
-    // https://stackoverflow.com/questions/6327276/parsing-git-log-output-preferably-as-xml
+  getCommitPrefixCounts(subDir) {
+    child_process.execSync(`git rev-list --count master --grep="fix(" --count -- ${this.PATH_TO_COMPONENTS + subDir} > ${this.PATH_TO_COMPONENTS + subDir}/${this.bugCountOutput}`);
+    child_process.execSync(`git rev-list --count master --grep="chore(" --count -- ${this.PATH_TO_COMPONENTS + subDir} > ${this.PATH_TO_COMPONENTS + subDir}/${this.choreCount}`);
+    child_process.execSync(`git rev-list --count master --grep="feat(" --count -- ${this.PATH_TO_COMPONENTS + subDir} > ${this.PATH_TO_COMPONENTS + subDir}/${this.featCount}`);
+  }
+
+  getPaprikaStatuses() {
     const log = `git log --invert-grep --author='Paprika Semaphore 2.0' -n 1 -- . > ${this.gitOutput}`;
     const lineCount = `git ls-files | xargs cat | wc -l > ${this.lineCount}`;
-    exec(`for d in ${this.PATH_TO_COMPONENTS}* ; do (cd "$d" && ${log} && ${lineCount}); done`);
     fs.readdirSync(this.PATH_TO_COMPONENTS).forEach(subDir => {
-        console.log(subDir)
-        exec(`git rev-list --count master -- ${this.PATH_TO_COMPONENTS + subDir} > ${this.PATH_TO_COMPONENTS + subDir}/count_commits.json`);
-        exec(`git rev-list --all --grep="fix(" --count -- ${this.PATH_TO_COMPONENTS + subDir} > ${this.PATH_TO_COMPONENTS + subDir}/${this.bugCountOutput}`);
-        exec(`git rev-list --all --grep="chore(" --count -- ${this.PATH_TO_COMPONENTS + subDir} > ${this.PATH_TO_COMPONENTS + subDir}/${this.choreCount}`);
-        exec(`git rev-list --all --grep="feat(" --count -- ${this.PATH_TO_COMPONENTS + subDir} > ${this.PATH_TO_COMPONENTS + subDir}/${this.featCount}`);
-        try {
-          exec(`yarn storybook-a11y-report --storybookUrl http://localhost:9009/ --include '${tree.getStoryName(subDir)}/**' > ${this.PATH_TO_COMPONENTS + subDir}/${this.a11yReport}`);
-        } catch (e) {
-          console.log(e);
-        }
-        const commits = {};
-        try {
-            commits.logs = fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.gitOutput}`, 'utf8');
-            commits.commitCount =  parseInt(fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/count_commits.json`, 'utf8').toString(), 10);
-            commits.bugCount = parseInt(fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.bugCountOutput}`, 'utf8').toString(), 10);
-            commits.choreCount = parseInt(fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.choreCount}`, 'utf8').toString(), 10);
-            commits.featCount = parseInt(fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.featCount}`, 'utf8').toString(), 10);
-            commits.lineCount = parseInt(fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.lineCount}`, 'utf8').toString(), 10);
-            commits.a11yReport= this.parseA11y(`${this.PATH_TO_COMPONENTS + subDir}/${this.a11yReport}`);
-            this.statuses[subDir] = commits;
+      child_process.execSync(`cd ${this.PATH_TO_COMPONENTS + subDir} && ${log} && ${lineCount}`);
+      child_process.execSync(`git rev-list --count master -- ${this.PATH_TO_COMPONENTS + subDir} > ${this.PATH_TO_COMPONENTS + subDir}/${this.countCommits}`);
+      this.getCommitPrefixCounts(subDir);
+      this.getStorybookA11yReport(subDir);
+      const commits = {};
+      try {
+        // clone the repo then run yarn in sonar
+        // get number task by ticket
+          commits.logs = fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.gitOutput}`, 'utf8');
+          commits.commitCount =  parseInt(fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.countCommits}`, 'utf8').toString(), 10);
+          commits.bugCount = parseInt(fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.bugCountOutput}`, 'utf8').toString(), 10);
+          commits.choreCount = parseInt(fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.choreCount}`, 'utf8').toString(), 10);
+          commits.featCount = parseInt(fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.featCount}`, 'utf8').toString(), 10);
+          commits.lineCount = parseInt(fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.lineCount}`, 'utf8').toString(), 10);
+          commits.a11yReport= this.parseA11y(`${this.PATH_TO_COMPONENTS + subDir}/${this.a11yReport}`);
+          this.statuses[subDir] = commits;
+          console.log(this.statuses[subDir]);
 
-            
-        } catch (e) {
-            this.statuses[subDir] = "Error parsing commit messages"
-        }
-        fs.writeFileSync("./status.json", JSON.stringify(this.statuses));
+      } catch (e) {
+          this.statuses[subDir] = e;
+      }
+      fs.writeFileSync("./status.json", JSON.stringify(this.statuses));
+      child_process.execSync(`cd ${this.PATH_TO_COMPONENTS + subDir} && rm -f ${this.gitOutput} && rm -f ${this.countCommits} && rm -f ${this.bugCountOutput} && rm -f ${this.choreCount} && rm -f ${this.featCount} && rm -f ${this.lineCount} && rm -f ${this.a11yReport}`);
       });
-
-     // exec(`for d in ${this.PATH_TO_COMPONENTS}* ; do (cd "$d" && rm ${this.gitOutput} && rm count_commits.json && rm ${this.bugCountOutput} && rm ${this.choreCount} && rm ${this.featCount} && rm ${this.lineCount} && rm ${this.a11yReport}); done`);
-    
   }
 
   parseA11y(path){
-    const report = (fs.readFileSync(path, "utf8"));
-    console.log(report)
+    let report;
+    try {
+      report = fs.readFileSync(path, "utf8");
+    }
+    catch(e) {
+      return [];
+    }
     const output = [];
     const cur = {};
     report.split("\n").forEach((line) => {
@@ -70,18 +81,21 @@ class PaprikaStatus {
         if (line.includes("description:")) {
             cur.description = line;
         }
-        console.log(cur);
     });
-    console.log(output)
     return output;
   }
 
   run() {
-    this.getCommits()
-    console.log(JSON.parse(JSON.stringify(this.statuses)));
+    if (typeof process.argv[2] === 'undefined') {
+      console.log("😱 Please provide a path to an exisiting file used to house the results");
+      process.exit(0);
+    }
+    this.getPaprikaStatuses()
+    // JSON.stringify what's in the status.json; then JSON.parse
+    // console.log(this.statuses);
     process.exit(0);
   }
 }
 
-const CV = new PaprikaStatus();
-CV.run();
+const PS = new PaprikaStatus(process.argv[2]);
+PS.run();
