dedad0a191adc27b0a5a7b3670c313456c68024e
trtri2 <leontrieu88@hotmail.com>
2021-07-30 15:23:45 -0700

    feat(Pestle): UXD-1264 use helper folder

---
+++ scripts/pestle/helpers/createFile.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+
+// Helper function to create files and directories using fs.
+function createFile(filePath, template = "", overwriteFile = false) {
+  try {
+    // Create the directory if it doesn't already exist
+    const dirPath = path.dirname(filePath);
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath, { recursive: true }, err => {
+        if (err) throw err;
+      });
+    }
+
+    // Create the file
+    if (!fs.existsSync(filePath) || overwriteFile) {
+      fs.writeFileSync(filePath, template, {
+        encoding: "utf8",
+        flag: "w",
+      });
+      console.log("added:", filePath);
+    } else {
+      console.log("file already exists:", filePath);
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+module.exports = {
+  createFile,
+}
