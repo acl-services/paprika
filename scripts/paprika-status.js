dd8bbd0e62c33e809cd742872033bba26afcf1a4
Allison Chen <allison_chen@acl.com>
2021-08-26 14:16:07 -0700

    chore(Paprika): UXD-1260 try fix semaphore

--- scripts/paprika-status.js
+++ scripts/paprika-status.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 /* eslint no-param-reassign: ["error", { "props": true, "ignorePropertyModificationsFor": ["commits"] }] */
 const fs = require("fs");
-const child_process = require('child_process');
-const tree = require("../.storybook/storyTree")
+const child_process = require("child_process");
+const tree = require("../.storybook/storyTree");
 
 class PaprikaStatus {
   constructor() {
@@ -23,8 +23,17 @@ class PaprikaStatus {
 
   getStorybookA11yReport(subDir, commits) {
     try {
-      child_process.spawnSync(`yarn storybook && wait-on http://localhost:9009/ && yarn`, [`storybook-a11y-report`, `--storybookUrl ${this.LOCAL_HOST}`, `--include '${tree.getStoryName(subDir)}/**'`, `--outDir ${this.PATH_TO_COMPONENTS + subDir}`], {shell: true, timeout: 60000});
-      commits.a11yReport= this.parseA11y(`${this.PATH_TO_COMPONENTS + subDir}/${this.a11yReport}`);
+      child_process.spawnSync(
+        `yarn`,
+        [
+          `storybook-a11y-report`,
+          `--storybookUrl ${this.LOCAL_HOST}`,
+          `--include '${tree.getStoryName(subDir)}/**'`,
+          `--outDir ${this.PATH_TO_COMPONENTS + subDir}`,
+        ],
+        { shell: true, timeout: 60000 }
+      );
+      commits.a11yReport = this.parseA11y(`${this.PATH_TO_COMPONENTS + subDir}/${this.a11yReport}`);
     } catch (e) {
       console.log(e);
     }
@@ -34,14 +43,18 @@ class PaprikaStatus {
     const path = this.PATH_TO_COMPONENTS + subDir;
     try {
       child_process.execSync(`git rev-list --count HEAD -- ${path} > ${path}/${this.countCommits}`);
-      child_process.execSync(`git rev-list --count HEAD --grep="fix(" --count -- ${path} > ${path}/${this.bugCountOutput}`);
-      child_process.execSync(`git rev-list --count HEAD --grep="chore(" --count -- ${path} > ${path}/${this.choreCount}`);
+      child_process.execSync(
+        `git rev-list --count HEAD --grep="fix(" --count -- ${path} > ${path}/${this.bugCountOutput}`
+      );
+      child_process.execSync(
+        `git rev-list --count HEAD --grep="chore(" --count -- ${path} > ${path}/${this.choreCount}`
+      );
       child_process.execSync(`git rev-list --count HEAD --grep="feat(" --count -- ${path} > ${path}/${this.featCount}`);
-  
-      commits.commitCount =  parseInt(fs.readFileSync(`${path}/${this.countCommits}`, 'utf8').toString(), 10);
-      commits.bugCount = parseInt(fs.readFileSync(`${path}/${this.bugCountOutput}`, 'utf8').toString(), 10);
-      commits.choreCount = parseInt(fs.readFileSync(`${path}/${this.choreCount}`, 'utf8').toString(), 10);
-      commits.featCount = parseInt(fs.readFileSync(`${path}/${this.featCount}`, 'utf8').toString(), 10);
+
+      commits.commitCount = parseInt(fs.readFileSync(`${path}/${this.countCommits}`, "utf8").toString(), 10);
+      commits.bugCount = parseInt(fs.readFileSync(`${path}/${this.bugCountOutput}`, "utf8").toString(), 10);
+      commits.choreCount = parseInt(fs.readFileSync(`${path}/${this.choreCount}`, "utf8").toString(), 10);
+      commits.featCount = parseInt(fs.readFileSync(`${path}/${this.featCount}`, "utf8").toString(), 10);
     } catch (e) {
       console.log(e);
     }
@@ -50,12 +63,17 @@ class PaprikaStatus {
   getTestStatistics(subDir, commits) {
     try {
       const jestSummary = [];
-      const output = child_process.execSync(`yarn jest --passWithNoTests --testPathPattern=${this.PATH_TO_COMPONENTS + subDir} --coverage --coverageReporters="json-summary" --forceExit`).toString();
-      if (output.toUpperCase().includes('NO TESTS FOUND')) return;
+      const output = child_process
+        .execSync(
+          `yarn jest --passWithNoTests --testPathPattern=${this.PATH_TO_COMPONENTS +
+            subDir} --coverage --coverageReporters="json-summary" --forceExit`
+        )
+        .toString();
+      if (output.toUpperCase().includes("NO TESTS FOUND")) return;
 
-      for (const [key, value] of Object.entries(JSON.parse(fs.readFileSync(`${this.jestSummary}`, 'utf8')))) {
-        const endsWithRegex = new RegExp('^.*[js-ts-tsx]$');
-        if (key.includes(`${subDir  }/src`) && key.match(endsWithRegex)) {
+      for (const [key, value] of Object.entries(JSON.parse(fs.readFileSync(`${this.jestSummary}`, "utf8")))) {
+        const endsWithRegex = new RegExp("^.*[js-ts-tsx]$");
+        if (key.includes(`${subDir}/src`) && key.match(endsWithRegex)) {
           const testStatistic = {};
           testStatistic.testPath = key.substring(key.indexOf(subDir), key.length);
           testStatistic.coverage = value;
@@ -79,46 +97,54 @@ class PaprikaStatus {
       this.getStorybookA11yReport(subDir, commits);
       this.getTestStatistics(subDir, commits);
       try {
-          commits.version = JSON.parse(fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.version}`, 'utf8')).version;
-          commits.logs = fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.gitOutput}`, 'utf8');
-          commits.lineCount = parseInt(fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.lineCount}`, 'utf8').toString(), 10);
-          this.statuses[subDir] = commits;
-          console.log(subDir);
-          console.log(this.statuses[subDir]);
+        commits.version = JSON.parse(
+          fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.version}`, "utf8")
+        ).version;
+        commits.logs = fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.gitOutput}`, "utf8");
+        commits.lineCount = parseInt(
+          fs.readFileSync(`${this.PATH_TO_COMPONENTS + subDir}/${this.lineCount}`, "utf8").toString(),
+          10
+        );
+        this.statuses[subDir] = commits;
+        console.log(subDir);
+        console.log(this.statuses[subDir]);
       } catch (e) {
-          this.statuses[subDir] = {error: e};
+        this.statuses[subDir] = { error: e };
       }
-      child_process.execSync(`cd ${this.PATH_TO_COMPONENTS + subDir} && rm -f ${this.gitOutput} && rm -f ${this.countCommits} && rm -f ${this.bugCountOutput} && rm -f ${this.choreCount} && rm -f ${this.featCount} && rm -f ${this.lineCount} && rm -f ${this.a11yReport}`);
+      child_process.execSync(
+        `cd ${this.PATH_TO_COMPONENTS + subDir} && rm -f ${this.gitOutput} && rm -f ${this.countCommits} && rm -f ${
+          this.bugCountOutput
+        } && rm -f ${this.choreCount} && rm -f ${this.featCount} && rm -f ${this.lineCount} && rm -f ${this.a11yReport}`
+      );
       child_process.execSync(`rm -f ${this.jestSummary}`);
-      });
-      fs.writeFileSync("./status.json", JSON.stringify(this.statuses));
+    });
+    fs.writeFileSync("./status.json", JSON.stringify(this.statuses));
   }
 
-  parseA11y(path){
+  parseA11y(path) {
     let report;
     try {
       report = fs.readFileSync(path, "utf8");
-    }
-    catch(e) {
+    } catch (e) {
       return [];
     }
     const output = [];
     const cur = {};
-    report.split("\n").forEach((line) => {
-        if (line.includes("A11y ID: ")) {
-            if (Object.keys(cur).length > 0) output.push(Object.assign({}, cur));
-            cur.A11yID = line.split("A11y ID: ")[1];
-        }
-        if (line.includes("description: ")) {
-            cur.description = line.split("description: ")[1];
-        }
+    report.split("\n").forEach(line => {
+      if (line.includes("A11y ID: ")) {
+        if (Object.keys(cur).length > 0) output.push(Object.assign({}, cur));
+        cur.A11yID = line.split("A11y ID: ")[1];
+      }
+      if (line.includes("description: ")) {
+        cur.description = line.split("description: ")[1];
+      }
     });
     return output;
   }
 
   run() {
     // TODO: add statistics on Screener tests
-    this.getPaprikaStatuses()
+    this.getPaprikaStatuses();
     process.exit(0);
   }
 }
