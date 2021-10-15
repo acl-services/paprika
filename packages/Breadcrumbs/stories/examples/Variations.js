7d0b027112c7777aa75f6595bf5cd2e909e4a60b
Allison Chen <allison_chen@acl.com>
2021-10-15 16:27:01 -0700

    fix(Breadcrumbs): UXD-1328 fix storybook example

--- packages/Breadcrumbs/stories/examples/Variations.js
+++ packages/Breadcrumbs/stories/examples/Variations.js
@@ -81,22 +81,10 @@ const AllVariations = () => (
         <Breadcrumbs.Link href={URL}>Breadcrumb 3</Breadcrumbs.Link>
         <Breadcrumbs.Link href={URL}>Breadcrumb 4</Breadcrumbs.Link>
       </Breadcrumbs>
-      <Gap />
-
-      <StoryHeading level={3}>Dark version</StoryHeading>
-      <CodeHeading>{"isDark={true}"}</CodeHeading>
-      <div style={{ padding: "16px", background: "#3f3d3c" }}>
-        <Breadcrumbs isDark>
-          <Breadcrumbs.Link href={URL}>Breadcrumb 1</Breadcrumbs.Link>
-          <Breadcrumbs.Link href={URL}>Breadcrumb 2</Breadcrumbs.Link>
-          <Breadcrumbs.Link href={URL}>Breadcrumb 3</Breadcrumbs.Link>
-          <Breadcrumbs.Link href={URL}>Breadcrumb 4</Breadcrumbs.Link>
-        </Breadcrumbs>
-        <Gap.Small />
-        <Breadcrumbs isDark>
-          <Breadcrumbs.Link href={URL}>Breadcrumb 1</Breadcrumbs.Link>
-        </Breadcrumbs>
-      </div>
+      <Gap.Small />
+      <Breadcrumbs isDark>
+        <Breadcrumbs.Link href={URL}>Breadcrumb 1</Breadcrumbs.Link>
+      </Breadcrumbs>
     </div>
   </>
 );
