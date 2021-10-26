84db045f1bc3cc940db9ce52adf4a732f3a23584
Mike Polowick <mike_polowick@acl.com>
2021-10-25 17:12:55 -0700

    Add HackHeading component

---
+++ packages/HackHeading/CHANGELOG.md
@@ -0,0 +1,58 @@
+# Changelog
+
+## 1.0.9
+
+### Patch Changes
+
+- Updated dependencies [3385c1f]
+- Updated dependencies [fd94ddb]
+  - @paprika/tokens@1.1.0
+  - @paprika/stylers@1.1.1
+
+## 1.0.9-next.0
+
+### Patch Changes
+
+- Updated dependencies [3385c1f]
+  - @paprika/tokens@1.1.0-next.0
+  - @paprika/stylers@1.1.1-next.0
+
+## 1.0.8
+
+### Patch Changes
+
+- Updated dependencies [ca237ea]
+  - @paprika/stylers@1.1.0
+
+## 1.0.8-next.0
+
+### Patch Changes
+
+- Updated dependencies [ca237ea]
+  - @paprika/stylers@1.1.0-next.0
+
+## 1.0.7
+
+### Patch Changes
+
+- Updated dependencies [ff8eae9]
+  - @paprika/stylers@1.0.1
+
+## 1.0.7-next.0
+
+### Patch Changes
+
+- Updated dependencies [ff8eae9]
+  - @paprika/stylers@1.0.1-next.0
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
+
+## [Unreleased]
+
+## [0.4.0] - 2020-07-09
+
+### Removed
+
+- Removed `domRef` prop. The primary use case for this prop was to focus on the DOM element, which can now be accomplished with the `focus()` method available as part of the imperative API of the component. [@mikrotron](http://github.com/mikrotron).
---
+++ packages/HackHeading/stories/examples/Variations.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { Gap } from "storybook/assets/styles/common.styles";
+import Heading, { atlasHeading } from "../../src";
+
+export default function Variations() {
+  return (
+    <>
+      <ThemeProvider theme={atlasHeading}>
+        <Heading level={1} isLight>
+          Title - Display!
+        </Heading>
+        <Heading level={2} isLight>
+          Title - Large
+        </Heading>
+        <Heading level={3} isLight>
+          Title - Medium
+        </Heading>
+        <Heading level={4} isLight>
+          Title - Small
+        </Heading>
+
+        <Gap />
+
+        <Heading level={2}>Title - Large</Heading>
+        <Heading level={3}>Title - Medium</Heading>
+        <Heading level={4}>Title - Small</Heading>
+
+        <Gap />
+
+        <Heading level={2} isLight>
+          <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank" rel="noreferrer">
+            Title - Large
+          </a>
+        </Heading>
+        <Heading level={3} isLight>
+          <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank" rel="noreferrer">
+            Title - Medium
+          </a>
+        </Heading>
+        <Heading level={4} isLight>
+          <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank" rel="noreferrer">
+            Title - Small
+          </a>
+        </Heading>
+      </ThemeProvider>
+    </>
+  );
+}
