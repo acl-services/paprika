8b19fbaaf65eb2362edeb0a2a72dd038f456b6ce
Michelle_Temp <michelle.huang@wegalvanize.com>
2021-10-26 12:55:06 -0700

    panel style minor update

--- packages/HackPanel/src/themes/atlas.js
+++ packages/HackPanel/src/themes/atlas.js
@@ -3,7 +3,12 @@ import { css } from "styled-components";
 export const Panel = {
   Panel: () => css`
     [data-pka-anchor="panel.header"] {
+      height: auto;
       width: auto;
+    }
+
+    [data-pka-anchor="heading"] {
+      font-weight: 400;
     }
   `,
 };
