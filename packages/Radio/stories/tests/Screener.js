51d8687a9cac72c27d7a10a97aaecc908fc1dc39
Yushan Chang <yushan.chang@wegalvanize.com>
2021-11-12 10:27:32 -0800

    fix(radio): UXD-1825 add story wip

---
+++ packages/Radio/stories/tests/Screener.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Story } from "storybook/assets/styles/common.styles";
+import Radio from "../../src";
+
+const ScreenerStory = () => (
+  <Story>
+    <Radio />
+  </Story>
+);
+export default ScreenerStory;
