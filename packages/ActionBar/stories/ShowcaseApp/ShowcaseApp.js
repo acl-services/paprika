80361b22f7c8c81f716131966979a6673f0d49ea
Andrei Chernykh <andrei_chernykh@acl.com>
2021-11-30 17:01:53 -0800

    UXD-1877 upddate @action-bar SeachInput api (#1192)
    * UXD-1877 upddate @action-bar SeachInput api
    
    * UXD-1877 update SearchInput
    
    * UXD-1877 add useCallbackDebouncer
    
    * UXD-1877 update changeset

--- packages/ActionBar/stories/ShowcaseApp/ShowcaseApp.js
+++ packages/ActionBar/stories/ShowcaseApp/ShowcaseApp.js
@@ -69,6 +69,7 @@ export default function App() {
 
       <ActionBar>
         <SearchInput
+          initialValue={searchTerm}
           onChange={term => {
             setSearchTerm(term);
           }}
