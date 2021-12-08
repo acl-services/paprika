a7656c12ac75091a96e6f22daca3cbafffaf1990
Alice Lee <45611276+slee1130@users.noreply.github.com>
2021-12-08 13:25:52 -0800

    style(modal): UXD-1699 use spacer instead of calc

--- packages/Modal/src/components/Header/Header.styles.js
+++ packages/Modal/src/components/Header/Header.styles.js
@@ -2,13 +2,12 @@ import styled, { css } from "styled-components";
 import { spacer } from "@paprika/stylers/lib/helpers";
 import stylers from "@paprika/stylers";
 import Heading from "@paprika/heading";
-import tokens from "@paprika/tokens";
 
 export const Header = styled.div`
   align-items: center;
   display: flex;
   justify-content: space-between;
-  min-height: calc(${tokens.space} * 7);
+  min-height: ${spacer(7)};
   padding: 0 ${spacer(2)};
 `;
 
