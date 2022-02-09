d7cb49761dcb356b1d2d7d2f2e8d3179dd9376e1
Allison Chen <38733362+allison-c@users.noreply.github.com>
2022-02-09 14:43:08 -0800

    refactor(DataTable): TW-2556 initial size refactor (#1228)
    * refactor(DataTable): TW-2556 initial size refactor
    
    * refactor(DataTable): TW-2556 cleanup
    
    * refactor(DataTable): TW-2556 address comments and add changelog

---
+++ packages/DataTable/src/hooks/useBestTableDimensions.ts
@@ -0,0 +1,76 @@
+import React from "react";
+import debounce from "lodash.debounce";
+import convertSizeStringToNumber, { Direction } from "../helpers/convertSizeStringToNumber";
+
+const DEBOUNCE_DELAY = 300;
+
+type Dimensions = {
+  width: number;
+  height: number;
+  shouldHaveHorizontalScroll: boolean;
+};
+
+export default function useBestTableDimensions({
+  tableRef,
+  componentRef,
+  maxHeight,
+  maxWidth,
+  shouldResizeWithViewport,
+}: {
+  tableRef: React.RefObject<HTMLDivElement>;
+  componentRef: React.RefObject<unknown>;
+  maxHeight: string;
+  maxWidth: string;
+  shouldResizeWithViewport: boolean;
+}): Dimensions {
+  const [dimensions, setDimensions] = React.useState<Dimensions>(() => ({
+    width: convertSizeStringToNumber(maxWidth, Direction.width),
+    height: convertSizeStringToNumber(maxHeight, Direction.height),
+    shouldHaveHorizontalScroll: false,
+  }));
+
+  const resetDimension = React.useCallback(() => {
+    if (!tableRef.current) return;
+
+    const theadEl = tableRef.current.querySelector('[data-pka-anchor="dataTable.thead"]');
+    const tbodyEl = tableRef.current.querySelector('[data-pka-anchor="dataTable.tbody"]');
+
+    if (!theadEl || !tbodyEl) {
+      return;
+    }
+
+    setDimensions(() => {
+      const realWidth = theadEl.clientWidth;
+      const realHeight = theadEl.clientHeight + tbodyEl.clientHeight;
+      const maxWidthInNumber = convertSizeStringToNumber(maxWidth, Direction.width);
+      const maxHeightInNumber = convertSizeStringToNumber(maxHeight, Direction.height);
+
+      return {
+        width: Math.min(maxWidthInNumber, realWidth),
+        height: Math.min(maxHeightInNumber, realHeight),
+        shouldHaveHorizontalScroll: maxWidthInNumber < realWidth,
+      };
+    });
+  }, [maxHeight, maxWidth, tableRef]);
+
+  React.useLayoutEffect(() => {
+    resetDimension();
+    const debouncedReset = debounce(resetDimension, DEBOUNCE_DELAY);
+
+    if (shouldResizeWithViewport) {
+      window.addEventListener("resize", debouncedReset);
+    }
+
+    return () => {
+      if (shouldResizeWithViewport) {
+        window.removeEventListener("resize", debouncedReset);
+      }
+    };
+  }, [resetDimension, shouldResizeWithViewport]);
+
+  React.useImperativeHandle(componentRef, () => ({
+    resize: resetDimension,
+  }));
+
+  return dimensions;
+}
---
+++ packages/DataTable/stories/examples/WithCollapsible.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import CollapsibleCard from "@paprika/collapsible-card";
+import StoryHeading from "storybook/components/StoryHeading";
+import { Story, Tagline } from "storybook/assets/styles/common.styles";
+import DataTable, { DataTableRef } from "../../src";
+import makeData from "../helpers/makeData";
+
+export const WithCollapsibleStory: () => JSX.Element = () => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "First Name",
+        accessor: "firstName",
+        width: 100,
+        isSticky: true,
+      },
+      {
+        Header: "Last Name",
+        accessor: "lastName",
+        width: 100,
+        isSticky: true,
+      },
+      {
+        Header: "Age",
+        accessor: "age",
+        width: 50,
+      },
+      {
+        Header: "Visits",
+        accessor: "visits",
+        width: 60,
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+      },
+    ],
+    []
+  );
+
+  const [items] = React.useState(() => makeData(5));
+  const tableRef = React.useRef<DataTableRef>(null);
+
+  return (
+    <DataTable a11yText="Data table for showcase." columns={columns} maxHeight="400px" data={items} ref={tableRef}>
+      <DataTable.InfiniteLoader
+        itemCount={items.length}
+        isItemLoaded={(index: number) => items[index] !== undefined}
+        isNextPageLoading={false}
+        loadMoreItems={async () => {
+          // null
+        }}
+      />
+    </DataTable>
+  );
+};
+
+export default () => {
+  const [isCollapsed, setCollapsed] = React.useState(true);
+
+  return (
+    <Story>
+      <StoryHeading level={1}>DataTable</StoryHeading>
+      <Tagline>
+        with <code>CollapsibleCard</code>
+      </Tagline>
+      <CollapsibleCard
+        onToggleIsCollapsed={isCollapsed => {
+          setCollapsed(isCollapsed);
+        }}
+        isCollapsed={isCollapsed}
+      >
+        <CollapsibleCard.Header>
+          <CollapsibleCard.Segment>Collapsed Table</CollapsibleCard.Segment>
+        </CollapsibleCard.Header>
+        <CollapsibleCard.Body>{!isCollapsed && <WithCollapsibleStory />}</CollapsibleCard.Body>
+      </CollapsibleCard>
+    </Story>
+  );
+};
