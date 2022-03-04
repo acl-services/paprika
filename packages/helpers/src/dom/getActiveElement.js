2f7778d7956b5ffb475190f5748c8ef020ce593a
Mike Polowick <mike_polowick@acl.com>
2022-03-04 23:01:43 +0000

    fix(helpers): RNG-271 return null when no activeElement found

--- packages/helpers/src/dom/getActiveElement.js
+++ packages/helpers/src/dom/getActiveElement.js
@@ -1,5 +1,7 @@
 export default function getActiveElement(rootElement = document) {
   const activeElement = rootElement.activeElement;
+  if (!activeElement) return null;
+
   const shadowRoot = activeElement.shadowRoot;
 
   return shadowRoot ? getActiveElement(shadowRoot) : activeElement;
