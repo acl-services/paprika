d98af59ee50356750f144911c57373516f6a7a29
Alice Lee <45611276+slee1130@users.noreply.github.com>
2022-03-04 17:05:27 -0800

    fix(OverflowMenu): UXD-309 revert cypress

--- packages/OverflowMenu/test/overflowMenu.cypress.js
+++ packages/OverflowMenu/test/overflowMenu.cypress.js
@@ -1,24 +1,22 @@
-import { getStoryUrlPrefix } from "../../../.storybook/storyTree";
-
 describe("<OverflowMenu />", () => {
   beforeEach(() => {
-    cy.visitStorybook(`${getStoryUrlPrefix("OverflowMenu")}-backyard-tests--cypress`);
+    cy.visitStorybook(`overflowmenu-tests--cypres`);
   });
 
   it("should show hide overflow content when when clicking trigger", () => {
-    cy.findByTestId('[data-pka-anchor="overflow-menu__trigger"]').click();
+    cy.findByTestId("overflow-menu__trigger").click();
     cy.findByTestId("popover.content").should("be.visible");
     cy.wait(250).then(() => {
-      cy.findByTestId('[data-pka-anchor="overflow-menu__trigger"]').click();
+      cy.findByTestId("overflow-menu__trigger").click();
       cy.findByTestId("popover.content").should("not.exist");
     });
   });
 
   it("should retain trigger focus when the overflow is closed", () => {
-    cy.findByTestId('[data-pka-anchor="overflow-menu__trigger"]').click();
+    cy.findByTestId("overflow-menu__trigger").click();
     cy.wait(250).then(() => {
-      cy.findByTestId('[data-pka-anchor="overflow-menu__trigger"]').click();
-      cy.findByTestId('[data-pka-anchor="overflow-menu__trigger"]').then(triggerElement => {
+      cy.findByTestId("overflow-menu__trigger").click();
+      cy.findByTestId("overflow-menu__trigger").then(triggerElement => {
         cy.focused().then(e => {
           expect(triggerElement.get(0) === e.get(0)).equal(true);
         });
