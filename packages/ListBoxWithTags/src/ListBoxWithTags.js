90cd180b9a00f8e465400a54f66f6d71307dd603
Danil Agafonov <danil.agafonov@flatstack.com>
2022-05-16 15:50:17 +0200

    fix(ListBoxWithTags): FS-3112 Fix focus after select

--- packages/ListBoxWithTags/src/ListBoxWithTags.js
+++ packages/ListBoxWithTags/src/ListBoxWithTags.js
@@ -125,7 +125,7 @@ export default function ListBoxWithTags(props) {
         refListBox.current.setFocusOptionByIndex(lastSelectedOption);
       }
     }
-  });
+  }, [lastSelectedOption]);
 
   return (
     <div ref={refDivRoot}>
