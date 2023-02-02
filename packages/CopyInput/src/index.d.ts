02853012fc2dfdf1b4d8aa936b9c271d673b2eaf
Paprika Semaphore 2.0 <allison.chen@wegalvanize.com>
2023-02-02 03:06:30 +0000

    [Bot] Paprika Pre-Release

--- packages/CopyInput/src/index.d.ts
+++ packages/CopyInput/src/index.d.ts
@@ -3,8 +3,14 @@ export default CopyInput;
 declare function CopyInput(props: CopyInputProps): JSX.Element;
 interface CopyInputProps {
   [x: string]: any;
+  /** Called after the button is clicked, and the copied value is passed in */
+  alterCopiedText?: (...args: any[]) => any;
   /** Used for CopyInput.Input */
   children?: React.ReactNode;
+  /** The text to show in the tooltip when the user presses the "copy" button */
+  clickedText?: string;
+  /** The text to show in the tooltip when the user hovers over the "copy" button */
+  hoverText?: string;
   /** Is the input read-only. */
   isReadOnly?: boolean;
   /** If the value will be rendered in an Input component or hidden */
