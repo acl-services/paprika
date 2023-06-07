8b1bc68d5dd29c6e8c223c64ae009f17e1d8382f
Michał Malec <39166047+MichalMalec@users.noreply.github.com>
2023-06-07 14:10:48 +0200

    Revert "FS-4985: Fix DataGrid scroll"

--- packages/DataGrid/src/DataGrid.js
+++ packages/DataGrid/src/DataGrid.js
@@ -215,7 +215,7 @@ const DataGrid = React.forwardRef((props, ref) => {
     refScrollHeader.current = refContainer.current.querySelector(`.${gridId}-header`);
     refScrollStickyColumns.current = refContainer.current.querySelector(`.${gridId}-sticky-columns`);
     refScrollGrid.current = refContainer.current.querySelector(`.grid-${gridId}`);
-  }, [refContainer.current, gridId]);
+  }, [gridId]);
 
   React.useLayoutEffect(() => {
     const scrollContainer =
@@ -328,26 +328,21 @@ const DataGrid = React.forwardRef((props, ref) => {
     import("mouse-wheel").then(module => {
       if (Array.isArray(data) && data.length) {
         const { default: mouseWheel } = module;
+        mouseWheel(refScrollGrid.current, (dx, dy, dz, event) => {
+          event.preventDefault();
+          refScrollGrid.current.scrollTo(refScrollGrid.current.scrollLeft + dx, refScrollGrid.current.scrollTop + dy);
+        });
 
-        if (refScrollGrid.current !== null) {
-          mouseWheel(refScrollGrid.current, (dx, dy, dz, event) => {
-            event.preventDefault();
-            refScrollGrid.current.scrollTo(refScrollGrid.current.scrollLeft + dx, refScrollGrid.current.scrollTop + dy);
-          });
-        }
-
-        if (refScrollStickyColumns.current !== null) {
-          mouseWheel(refScrollStickyColumns.current, (dx, dy, dz, event) => {
-            event.preventDefault();
-            refScrollStickyColumns.current.scrollTo(
-              refScrollStickyColumns.current.scrollLeft + dx,
-              refScrollStickyColumns.current.scrollTop + dy
-            );
-          });
-        }
+        mouseWheel(refScrollStickyColumns.current, (dx, dy, dz, event) => {
+          event.preventDefault();
+          refScrollStickyColumns.current.scrollTo(
+            refScrollStickyColumns.current.scrollLeft + dx,
+            refScrollStickyColumns.current.scrollTop + dy
+          );
+        });
       }
     });
-  }, [refScrollGrid.current, refScrollStickyColumns.current, gridId, data]);
+  }, [gridId, data]);
 
   React.useEffect(() => {
     if (!refContainer.current) return;
