cb5e9fd018bac5e438758698aca9fc336741948d
Paprika Semaphore 2.0 <allison.chen@wegalvanize.com>
2023-12-20 03:06:13 +0000

    [Bot] Paprika Pre-Release

--- packages/L10n/src/locales-js/it.js
+++ packages/L10n/src/locales-js/it.js
@@ -2,18 +2,257 @@
   const locales = {
   "it": {
     "translation": {
-      "formElement": {
-        "optional": "(facoltativo)",
-        "required": "(obbligatorio)"
+      "timePicker": {
+        "am": "am",
+        "custom": "Personalizzato",
+        "hours": "Ore",
+        "minutes": "Minuti",
+        "period": "Periodo",
+        "pm": "pm"
+      },
+      "back": "Indietro",
+      "remove": "rimuovi",
+      "remove_a11y": "Rimuovi {{value}}",
+      "moreInformation": "maggiori informazioni",
+      "close": "Chiudi",
+      "dataGrid": {
+        "a11yTextMessage": "{{value}}. Ti trovi in: riga {{rowIndex}}, colonna {{columnIndex}}. Ignora le seguenti informazioni:",
+        "rows": "Righe: {{rowCount}}",
+        "columns": "Colonne: {{columnCount}}"
+      },
+      "uploader": {
+        "drop_files": "Rilascia i file per il caricamento",
+        "drop_files_here_or": "Rilascia i file qui per il caricamento oppure",
+        "choose_from_computer": "seleziona i file dal tuo computer",
+        "choose_from_computer_a11y": "Seleziona i file dal tuo computer",
+        "cancel_upload": "Annulla caricamento per {{name}}",
+        "restart_upload": "Seleziona nuovamente {{name}} per riprovare.",
+        "file_list": "Elenco dei file",
+        "errors": {
+          "size": "Il file deve essere più piccolo di {{maxFileSize}}.",
+          "type": "Il tipo di file deve essere uno dei seguenti: {{- supportedTypes}}"
+        },
+        "status": {
+          "idle": "Inattivo",
+          "processing": "In elaborazione",
+          "uploading": "Caricamento in corso",
+          "success": "Tentativo riuscito",
+          "cancel": "Annulla",
+          "error": "Errore"
+        },
+        "progress": {
+          "cancelled": "Annullato",
+          "complete": "Completo",
+          "error": "Si è verificato un errore per {{name}}: {{error}}",
+          "idle": "Inattivo",
+          "file_progress": "{{name}} è {{progress}}",
+          "uploading_percent": "Caricamento in corso: {{a11yProgress}}%",
+          "uploading": "Caricamento di {{progressWithUnits}} di {{sizeWithUnits}}"
+        },
+        "size_abbreviations": {
+          "byte": "B",
+          "kibibyte": "KiB",
+          "mebibyte": "MiB",
+          "gibibyte": "GiB",
+          "tebibyte": "TiB"
+        }
+      },
+      "actions": {
+        "delete": "Elimina",
+        "apply": "Applica",
+        "cancel": "Annulla",
+        "confirm": "Salva",
+        "decline": "Rifiuta"
+      },
+      "datePicker": {
+        "confirmation_format": "D MMMM YYYY"
       },
       "calendar": {
         "header_format": "MMMM YYYY",
         "month": "Mese",
         "year": "Anno"
       },
-      "actions": {
-        "apply": "Applica",
-        "cancel": "Cancella"
+      "daterange": {
+        "last_7_days": "Ultimi 7 Giorni",
+        "last_30_days": "Ultimi 30 Giorni",
+        "this_month": "Questo mese",
+        "last_month": "Ultimo mese",
+        "start_date": "Data di inizio",
+        "end_date": "Data di fine"
+      },
+      "dropdownMenu": {
+        "isExternal": "{{link}} si apre in una nuova scheda."
+      },
+      "comment": {
+        "delete": "Elimina il commento"
+      },
+      "pagination": {
+        "intro": "Numerazione delle pagine, attualmente a pagina {{currentPage}}",
+        "page": "Pagina {{pageNumber}}",
+        "ellipsis": "Più pagine",
+        "prev": "Pagina precedente",
+        "next": "Pagina successiva"
+      },
+      "formElement": {
+        "optional": "(facoltativo)",
+        "required": "(obbligatorio)",
+        "aria_info_circle": "Informazioni"
+      },
+      "prevNextArrowButton": {
+        "arrowup": "su",
+        "arrowdown": "giù",
+        "escape": "esci"
+      },
+      "collapsible": {
+        "ariaText": "Sezione a scomparsa",
+        "collapse": "Comprimi",
+        "expand": "Espandi"
+      },
+      "sortable": {
+        "aria_description": "Elemento trascinabile. Premi la barra spaziatrice per sollevare.",
+        "aria_lift": "Hai sollevato un elemento in posizione {{source}}. Utilizza i tasti freccia per spostar, la barra spaziatrice per rilasciare e il tasto esc per annullare.",
+        "aria_moving": "L'elemento è stato spostato dalla posizione {{source}} alla posizione {{destination}}.",
+        "aria_moving_outside": "Non stai trascinando su un'area di rilascio.",
+        "aria_cancelled": "Movimento annullato. L'elemento è tornato alla posizione iniziale: {{source}}.",
+        "aria_dropped": "Hai rilasciato l'elemento. È stato spostato dalla posizione {{source}} a {{destination}}.",
+        "aria_dropped_outside": "L'elemento non è stato rilasciato sopra l'elenco. L'elemento è tornato alla posizione iniziale: {{source}}.",
+        "aria_remove": "Rimuovi"
+      },
+      "progressAccordion": {
+        "active": "attivo/a",
+        "no_response": "Nessuno"
+      },
+      "collapsibleText": {
+        "more": "Mostra di più",
+        "less": "Mostra meno",
+        "more_about": "mostra di più su {{topic}}",
+        "less_about": "mostra di meno su {{topic}}"
+      },
+      "listBox": {
+        "trigger": {
+          "clear_selection": "Cancella la selezione",
+          "placeholder": "Seleziona..."
+        },
+        "filter": {
+          "a11y_text": "Cerca",
+          "no_results_message": "Nessun risultato trovato",
+          "placeholder": "Cerca..."
+        },
+        "footer": {
+          "accept": "Applica",
+          "cancel": "Annulla",
+          "clear": "Cancella"
+        }
+      },
+      "listBoxBrowser": {
+        "explore": "Esplora altre opzioni",
+        "selected": "opzioni selezionate",
+        "selectOne": "Seleziona uno o più elementi..."
+      },
+      "listBoxWithTags": {
+        "placeholder": "Seleziona...",
+        "no_results_found": "Nessun risultato trovato",
+        "all_items_have_been_selected": "Tutti gli elementi sono stati selezionati",
+        "a11y_text_trigger": "Opzione di selezione multipla: {{options}}"
+      },
+      "search": {
+        "searchTerm": "Cerca termine..."
+      },
+      "actionBar": {
+        "columns_arrangement": {
+          "hide_all": "Nascondi tutto",
+          "show_all": "Mostra tutto",
+          "label": "Disponi",
+          "singular_label": "1 colonna nascosta",
+          "plural_label": "{{numberOfHiddenColumn}} colonne nascoste",
+          "search_placeholder": "Cerca..."
+        },
+        "no_results": "Nessun risultato",
+        "search_placeholder": "Cerca...",
+        "search_a11y_text": "Cerca i dati",
+        "sort": {
+          "no_sorts_applied": "Nessun ordine applicato alla vista",
+          "label": "Ordina",
+          "singular_label": "1 colonna ordinata",
+          "plural_label": "{{numberOfFields}} colonne ordinate",
+          "add_field": "Aggiungi un campo per ordinare per",
+          "sort_by": "Ordina per",
+          "then_by": "quindi per",
+          "rules": {
+            "ascending": {
+              "boolean": "Ordina per crescente (vero → falso)",
+              "number": "Ordina per crescente (1 → 9)",
+              "text": "Ordina per crescente (A → Z)",
+              "date": "Ordina per crescente (1 → 9)",
+              "single_select": "Ordina per crescente (A → Z)"
+            },
+            "descending": {
+              "boolean": "Ordina per decrescente (vero → falso)",
+              "number": "Ordina per decrescente (1 → 9)",
+              "text": "Ordina per decrescente (Z → A)",
+              "date": "Ordina per decrescente (1 → 9)",
+              "single_select": "Ordina per decrescente (Z → A)"
+            }
+          }
+        }
+      },
+      "breadcrumbs": {
+        "aria_label": "Percorsi di navigazione",
+        "aria_expand": "Mostra tutti gli elementi del percorso di navigazione.",
+        "aria_back_to": "Torna a"
+      },
+      "sideNavigation": {
+        "aria_label": "Navigazione laterale",
+        "trigger_aria_label": "Apri navigazione"
+      },
+      "copyInput": {
+        "clicked_tooltip": "Copiato",
+        "hover_tooltip": "Copia negli appunti"
+      },
+      "dynamicHyperlinkTransformer": {
+        "access_denied": "Accesso negato",
+        "invalid_url": "URL non valido",
+        "exceeded_max_links_rendered": "Impossibile eseguire il rendering",
+        "loading": "Caricamento in corso"
+      },
+      "filter": {
+        "actions": {
+          "add": "Aggiungi filtro",
+          "clear": "Cancella",
+          "delete": "Elimina"
+        },
+        "no_filters_applied": "Nessun filtro applicato alla vista",
+        "label": "Filtro",
+        "label_with_count": "{{count}} filtrati",
+        "and": "E",
+        "or": "Oppure",
+        "rules": {
+          "is": "è",
+          "is_not": "non è",
+          "contains": "contiene",
+          "does_not_contain": "non contiene",
+          "is_blank": "è vuoto",
+          "is_not_blank": "non è vuoto",
+          "equals": "è uguale a",
+          "not_equal_to": "non è uguale a",
+          "greater_than": ">",
+          "less_than": "<",
+          "greater_than_or_equal_to": "≥",
+          "less_than_or_equal_to": "≤",
+          "is_empty": "è vuoto",
+          "is_not_empty": "non è vuoto",
+          "is_before": "è prima di",
+          "is_after": "è dopo",
+          "is_one_of": "è uno di",
+          "is_not_one_of": "non è uno di",
+          "true": "vero",
+          "false": "falso"
+        }
+      },
+      "input": {
+        "clear_button": {
+          "aria_label": "Cancella immissione"
+        }
       }
     }
   }
